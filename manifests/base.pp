f12f709afeda16c70cd8ad2fc369c9b457cd5ce9
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-19 20:46:12 +0200

    Add user/group management

--- manifests/base.pp
+++ manifests/base.pp
@@ -16,4 +16,31 @@ class profile::base {
   package { $packages:
     ensure => present,
   }
+
+  $users = merge(
+    hiera('users::base_users'),
+    hiera('users::extra_users')
+  )
+
+  $users.each |$name, $data| {
+    user { $name:
+      ensure  => 'present',
+      uid     => $data['uid'],
+      comment => $data['full_name'],
+      shell   => $data['shell'],
+    }
+  }
+
+  $groups = merge(
+    hiera('groups::base_groups'),
+    hiers('groups::extra_groups')
+  )
+
+  $groups.each |$name, $data| {
+    group { $name:
+      ensure  => 'present',
+      gid     => $data['gid'],
+      members => $data['members'],
+    }
+  }
 }
