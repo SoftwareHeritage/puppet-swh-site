493950a227a8e68448f85bd77add89c09040bec6
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-19 20:17:50 +0200

    Add package installation to profile::base

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,4 +7,13 @@ class profile::base {
     default_locale => hiera('locales::default_locale'),
     locales        => hiera('locales::installed_locales'),
   }
+
+  $packages = union(
+    hiera('packages::base_packages'),
+    hiera('packages::extra_packages'),
+  )
+
+  package { $packages:
+    ensure => present,
+  }
 }
