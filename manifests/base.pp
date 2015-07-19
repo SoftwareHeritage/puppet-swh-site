a7e27287b9bf0f77f4fdeddf13de806aa669b828
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-19 18:45:17 +0200

    Add locales module

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,4 +2,9 @@ class profile::base {
   class { '::ntp':
     servers => hiera('ntp::servers'),
   }
+
+  class { '::locales':
+    default_locale => hiera('locales::default_locale'),
+    locales        => hiera('locales::installed_locales'),
+  }
 }
