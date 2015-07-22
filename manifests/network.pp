ab7c3b12c5cc14a2134886d5a97b4abeedfe75e5
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-22 22:22:50 +0200

    Add profile::network

---
+++ manifests/network.pp
@@ -0,0 +1,38 @@
+class profile::network {
+  debnet::iface::loopback { 'lo': }
+
+  $interfaces = hiera('networks')
+  each($interfaces) |$label, $data| {
+
+    if $label == 'private' {
+      file_line {'private route table':
+        ensure => 'present',
+        line   => '42 private',
+        path   => '/etc/iproute2/rt_tables',
+      }
+      $ups = [
+              "ip rule add from ${data['address']} table private",
+              "ip route add default via ${data['gateway']} dev ${data['interface']} table private",
+              "ip route flush table",
+      ]
+      $downs = [
+              "ip route del default via ${data['gateway']} dev ${data['interface']} table private",
+              "ip rule del from ${data['address']} table private",
+              "ip route flush table",
+      ]
+    } else {
+      $ups = []
+      $downs = []
+    }
+
+
+    debnet::iface { $data['interface']:
+      method  => 'static',
+      address => $data['address'],
+      netmask => $data['netmask'],
+      gateway => $data['gateway'],
+      ups     => $ups,
+      downs   => $downs,
+    }
+  }
+}
