bfb9415a24d1d2193261f67d2dd957d4f50f555c
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-27 16:48:18 +0200

    Properly classify softwareheritage nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,27 @@
+node 'louvre.softwareheritage.org' {
+  include role::swh_hypervisor
+}
+
+node 'pergamon.softwareheritage.org' {
+  include role::swh_sysadmin
+}
+
+node 'tate.softwareheritage.org' {
+  include role::swh_forge
+}
+
+node 'moma.softwareheritage.org' {
+  include role::swh_api
+}
+
+node 'prado.softwareheritage.org' {
+  include role::swh_database
+}
+
+node 'uffizi.softwareheritage.org' {
+  include role::swh_storage
+}
+
 node /worker\d+\.softwareheritage\.org/ {
   include role::swh_worker
 }
