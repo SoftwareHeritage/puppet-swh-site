2062b34891f45b62d8e4cde41e9d57ce1bbca7ff
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-30 15:06:01 +0200

    add munin master vhost definition

--- manifests/munin/master.pp
+++ manifests/munin/master.pp
@@ -1,4 +1,42 @@
 # Munin master class
 class profile::munin::master {
   include ::munin::master
+
+  $master_hostname = hiera('munin::master::hostname')
+
+  include ::apache::mod::rewrite
+  include ::apache::mod::fcgid
+
+  apache::vhost { $master_hostname:
+    port        => 80,
+    docroot     => '/var/www/html',
+    rewrites    => [
+      {
+        comment      => 'static resources',
+        rewrite_rule => [
+          '^/favicon.ico /etc/munin/static/favicon.ico [L]',
+          '^/static/(.*) /etc/munin/static/$1          [L]',
+        ],
+      },
+      {
+        comment      => 'HTML',
+        rewrite_cond => [
+          '%{REQUEST_URI} .html$ [or]',
+          '%{REQUEST_URI} =/',
+        ],
+        rewrite_rule => [
+          '^/(.*)          /usr/lib/munin/cgi/munin-cgi-html/$1 [L]',
+        ],
+      },
+      {
+        comment      => 'Images',
+        rewrite_rule => ['^/(.*) /usr/lib/munin/cgi/munin-cgi-graph/$1 [L]'],
+      },
+    ],
+    directories => [
+      { 'path'       => '/usr/lib/munin/cgi',
+        'options'    => '+ExecCGI',
+        'sethandler' => 'fcgid-script' },
+    ],
+  }
 }
