8d7c9020488c307b3dd4407c19195616115734c2
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-30 13:44:02 +0200

    Add ip_for_network function for munin

--- manifests/munin/node.pp
+++ manifests/munin/node.pp
@@ -3,6 +3,7 @@ class profile::munin::node {
   $munin_node_allow = hiera('munin::node::allow')
 
   class { '::munin::node':
-    allow => $munin_node_allow,
+    allow   => $munin_node_allow,
+    address => ip_for_network('192.168.100.0/24')
   }
 }
