34cb57b966f191e5587b9f21bb39f44740a18adf
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-30 22:33:15 +0200

    Add puppet master profile to sysadmin role

--- manifests/swh_sysadmin.pp
+++ manifests/swh_sysadmin.pp
@@ -3,5 +3,6 @@ class role::swh_sysadmin {
   include profile::ssh::server
   include profile::munin::node
   include profile::munin::master
+  include profile::puppet::master
   include profile::apache::simple_server
 }
