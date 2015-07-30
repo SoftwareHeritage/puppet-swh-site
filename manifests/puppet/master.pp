357219cd8d28207e8fa0e13407f9d445c1056a87
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-07-30 22:32:18 +0200

    Add puppet master profile

---
+++ manifests/puppet/master.pp
@@ -0,0 +1,19 @@
+# Puppet master profile
+class profile::puppet::master {
+  $puppetmaster = hiera('puppet::master::hostname')
+
+  class { '::puppet':
+    server               => true,
+    server_parser        => 'future',
+    server_foreman       => false,
+    server_environments  => [],
+    server_passenger     => true,
+
+    storeconfigs         => true,
+    storeconfigs_backend => 'active_record',
+
+    runmode              => 'none',
+    pluginsync           => true,
+    puppetmaster         => $puppetmaster,
+  }
+}
