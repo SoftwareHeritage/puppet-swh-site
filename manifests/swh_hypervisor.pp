14f40d4163c8d13d9704f4381a9713e36e15f9db
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-02 14:13:39 +0200

    Add dar client profile to swh_server and swh_hypervisor

--- manifests/swh_hypervisor.pp
+++ manifests/swh_hypervisor.pp
@@ -2,4 +2,5 @@ class role::swh_hypervisor {
   include profile::base
   include profile::ssh::server
   include profile::munin::node
+  include profile::dar::client
 }
--- manifests/swh_server.pp
+++ manifests/swh_server.pp
@@ -2,4 +2,5 @@ class role::swh_server {
   include profile::base
   include profile::ssh::server
   include profile::munin::node
+  include profile::dar::client
 }
