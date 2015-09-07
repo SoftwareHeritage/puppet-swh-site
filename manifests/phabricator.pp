c7862bb4992dcc5f8209c72e02e831ab9d04f914
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-07 14:31:28 +0200

    Add phabricator virtual hosts

--- manifests/phabricator.pp
+++ manifests/phabricator.pp
@@ -9,6 +9,8 @@ class profile::phabricator {
   $phabricator_db_user = hiera('phabricator::mysql::username')
   $phabricator_db_password = hiera('phabricator::mysql::password')
   $phabricator_fpm_listen = hiera('phabricator::php::fpm_listen')
+  $phabricator_vhost_name = hiera('phabricator::vhost::name')
+  $phabricator_vhost_docroot = hiera('phabricator::vhost::docroot')
 
   ::mysql::db {$phabricator_db_name:
     user     => $phabricator_db_user,
@@ -39,4 +41,20 @@ class profile::phabricator {
     'apcu',
   ]:
   }
+
+  ::apache::vhost {"$phabricator_vhost_name non-ssl":
+    servername      => $phabricator_vhost_name,
+    port            => '80',
+    docroot         => $phabricator_vhost_docroot,
+    redirect_status => 'permanent',
+    redirect_dest   => "https://${phabricator_vhost_name}/",
+  }
+
+  ::apache::vhost {"$phabricator_vhost_name ssl":
+    servername => $phabricator_vhost_name,
+    port       => '443',
+    ssl        => true,
+    docroot    => $phabricator_vhost_docroot,
+  }
+
 }
