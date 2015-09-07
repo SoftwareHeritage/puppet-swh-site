273b7201085d4a99e8fc50e84798befe02a81b74
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-07 12:22:42 +0200

    Allow several deploy users

--- manifests/worker/deploy_key.pp
+++ manifests/worker/deploy_key.pp
@@ -1,28 +1,30 @@
 class profile::worker::deploy_key {
 
-  $worker_user = hiera('worker::deploy::user')
+  $worker_users = hiera_array('worker::deploy::users')
 
-  file {"/home/${worker_user}/.ssh/id_rsa":
-    ensure  => present,
-    content => hiera('worker::deploy::private_key'),
-    owner   => $worker_user,
-    group   => $worker_user,
-    mode    => '0400',
-    require => [
-      User[$worker_user],
-      File["/home/${worker_user}/.ssh"],
-    ],
-  }
+  each ($worker_users) |$worker_user| {
+    file {"/home/${worker_user}/.ssh/id_rsa":
+      ensure  => present,
+      content => hiera('worker::deploy::private_key'),
+      owner   => $worker_user,
+      group   => $worker_user,
+      mode    => '0400',
+      require => [
+        User[$worker_user],
+        File["/home/${worker_user}/.ssh"],
+      ],
+    }
 
-  file {"/home/${worker_user}/.ssh/id_rsa.pub":
-    ensure  => present,
-    content => hiera('worker::deploy::public_key'),
-    owner   => $worker_user,
-    group   => $worker_user,
-    mode    => '0400',
-    require => [
-      User[$worker_user],
-      File["/home/${worker_user}/.ssh"],
-    ],
+    file {"/home/${worker_user}/.ssh/id_rsa.pub":
+      ensure  => present,
+      content => hiera('worker::deploy::public_key'),
+      owner   => $worker_user,
+      group   => $worker_user,
+      mode    => '0400',
+      require => [
+        User[$worker_user],
+        File["/home/${worker_user}/.ssh"],
+      ],
+    }
   }
 }
