dfb27e64023b2f8c016655e308fb9842503cf6a9
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-07 13:53:35 +0200

    Add php configuration to phabricator profile

--- manifests/phabricator.pp
+++ manifests/phabricator.pp
@@ -8,11 +8,28 @@ class profile::phabricator {
   $phabricator_db_name = hiera('phabricator::mysql::database')
   $phabricator_db_user = hiera('phabricator::mysql::username')
   $phabricator_db_password = hiera('phabricator::mysql::password')
+  $phabricator_fpm_listen = hiera('phabricator::php::fpm_listen')
 
   ::mysql::db {$phabricator_db_name:
     user     => $phabricator_db_user,
     password => $phabricator_db_password,
     host     => 'localhost',
     grant    => ['ALL'],
+  }
+
+  include ::php::cli
+  include ::php::fpm::daemon
+
+  ::php::fpm::conf {'phabricator':
+    listen => $phabricator_fpm_listen,
+    user   => 'www-data',
+  }
+
+  ::php::module {[
+    'mysql',
+    'curl',
+    'gd',
+    'apcu',
+  ]:
   }
 }
