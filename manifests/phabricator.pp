7c6209700343784a214d72f95d7544440dcbdff9
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-07 12:33:31 +0200

    Add database creation to phabricator manifest

--- manifests/phabricator.pp
+++ manifests/phabricator.pp
@@ -1,4 +1,19 @@
+# Setup an instance of phabricator
 class profile::phabricator {
   include ::apache
+
   include ::mysql::server
+  include ::mysql::client
+
+  $phabricator_db_name = hiera('phabricator::mysql::database')
+  $phabricator_db_user = hiera('phabricator::mysql::username')
+  $phabricator_db_password = hiera('phabricator::mysql::password')
+  $phabricator_db_password_hashed = hiera('phabricator::mysql::password_hashed')
+
+  ::mysql::db {$phabricator_db_name:
+    user     => $phabricator_db_user,
+    password => $phabricator_db_password_hashed,
+    host     => 'localhost',
+    grant    => ['ALL PRIVILEGES'],
+  }
 }
