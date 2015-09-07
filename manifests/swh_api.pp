fc7f706efbb8317e3b61f029a5cef85327d4ea68
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-07 11:24:24 +0200

    Add worker deploy key to swh_api

--- manifests/swh_api.pp
+++ manifests/swh_api.pp
@@ -3,5 +3,7 @@ class role::swh_api {
   include profile::ssh::server
   include profile::munin::node
   include profile::dar::client
+
   include profile::munin::plugins::rabbitmq
+  include profile::worker::deploy_key
 }
