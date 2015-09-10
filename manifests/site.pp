e7ac331404b60d7e4eabe221520ee02b45044896
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-10 17:34:42 +0200

    Add swh_miracle_worker role to worker08

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,6 +22,10 @@ node 'uffizi.softwareheritage.org' {
   include role::swh_storage
 }
 
+node 'worker08.softwareheritage.org' {
+  include role::swh_miracle_worker
+}
+
 node /worker\d+\.softwareheritage\.org/ {
   include role::swh_worker
 }
