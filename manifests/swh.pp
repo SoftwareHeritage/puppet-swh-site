f138abbdfb9f03a79aac11af1781cdf60f3438d6
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-23 15:30:07 +0200

    Add the softwareheritage repository to puppet

--- manifests/swh.pp
+++ manifests/swh.pp
@@ -4,6 +4,8 @@ class profile::swh {
   $swh_base_directory = hiera('swh::base_directory')
   $swh_conf_directory = hiera('swh::conf_directory')
 
+  $swh_mirror_location = hiera('swh::debian_mirror::location')
+
   file {[
     $swh_base_directory,
     $swh_conf_directory,
@@ -12,6 +14,15 @@ class profile::swh {
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+  }
+
+  include ::apt
+  ::apt::source {'softwareheritage':
+    comment        => 'Software Heritage specific package repository',
+    location       => $swh_mirror_location,
+    release        => $::lsbdistcodename,
+    repos          => 'main'
+    allow_unsigned => true,
   }
 
   include profile::swh::deploy
