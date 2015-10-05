91401986b4a3ea75ad9b1b71869dd0be230f801d
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-10-05 15:04:31 +0200

    munin::node: Add plugin overrides to node config

--- manifests/munin/node.pp
+++ manifests/munin/node.pp
@@ -5,8 +5,20 @@ class profile::munin::node {
   $munin_node_plugins_enable = hiera_array('munin::node::plugins::enable')
 
   class { '::munin::node':
-    allow   => $munin_node_allow,
-    address => ip_for_network('192.168.100.0/24')
+    allow        => $munin_node_allow,
+    address      => ip_for_network('192.168.100.0/24'),
+    masterconfig => [
+      '# The apt plugin doesn\'t graph by default. Let\'s make it.',
+      'apt.graph yes',
+      'apt.graph_category system',
+      'apt.graph_vlabel Total Packages',
+      '',
+      '# Move the libvirt plugins to a spaceless category',
+      'libvirt_blkstat.graph_category virtualization',
+      'libvirt_cputime.graph_category virtualization',
+      'libvirt_ifstat.graph_category virtualization',
+      'libvirt_mem.graph_category virtualization',
+    ],
   }
 
   munin::plugin { $munin_node_plugins_enable:
