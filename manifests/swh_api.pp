f55466fda665eb0ecc891a3a2414fee62f252ec7
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-10-16 15:43:44 +0200

    role::swh_api: deploy a local storage backend

--- manifests/swh_api.pp
+++ manifests/swh_api.pp
@@ -2,5 +2,6 @@ class role::swh_api inherits role::swh_server {
   include profile::network
 
   include profile::munin::plugins::rabbitmq
+  include profile::swh::deploy::storage
   include profile::swh::deploy::webapp
 }
