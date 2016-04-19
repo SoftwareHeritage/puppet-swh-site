e2197388d8a7d201a8d7a49c4078ff3835772e54
Stefano Zacchiroli <zack@upsilon.cc>
2016-04-19 11:08:59 +0200

    remove swh_devel role
    our desktops are *development* desktops by default

--- manifests/swh_desktop.pp
+++ manifests/swh_desktop.pp
@@ -1,3 +1,4 @@
 class role::swh_desktop inherits role::swh_base {
   include profile::desktop
+  include profile::devel
 }
--- manifests/swh_devel.pp
+++
@@ -1,4 +0,0 @@
-class role::swh_desktop inherits role::swh_base {
-  include profile::desktop
-  include profile::devel
-}
