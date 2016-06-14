2083276e2e8b988b828b0769f7ba054116934ba1
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-06-14 12:25:32 +0200

    profile::kafka::server: fully qualify kafka class imports

--- manifests/kafka/server.pp
+++ manifests/kafka/server.pp
@@ -3,9 +3,9 @@
 class profile::kafka::server {
   include ::profile::zookeeper::server
 
-  class {'kafka':}
+  class {'::kafka':}
 
-  class {'kafka::server':
+  class {'::kafka::server':
     log_dirs         => hiera_array('kafka::log_dirs'),
     brokers          => hiera_hash('kafka::brokers'),
     zookeeper_hosts  => hiera_array('kafka::zookeeper::hosts'),
