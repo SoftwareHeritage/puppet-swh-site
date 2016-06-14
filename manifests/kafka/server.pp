7e4f2ac38c385e200d1ac848a30277e0c0f16f7d
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-06-14 12:23:11 +0200

    kafka::server: add kafka classes

--- manifests/kafka/server.pp
+++ manifests/kafka/server.pp
@@ -2,4 +2,13 @@
 
 class profile::kafka::server {
   include ::profile::zookeeper::server
+
+  class {'kafka':}
+
+  class {'kafka::server':
+    log_dirs         => hiera_array('kafka::log_dirs'),
+    brokers          => hiera_hash('kafka::brokers'),
+    zookeeper_hosts  => hiera_array('kafka::zookeeper::hosts'),
+    zookeeper_chroot => hiera('kafka::zookeeper::chroot'),
+  }
 }
