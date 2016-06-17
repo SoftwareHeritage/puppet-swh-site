e941f3a8fcc521df39ca632fe196ee74f07207a6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-06-17 17:17:28 +0200

    Add profile::munin::stats_export to role sysadmin

--- manifests/swh_sysadmin.pp
+++ manifests/swh_sysadmin.pp
@@ -2,6 +2,7 @@ class role::swh_sysadmin inherits role::swh_server {
   include profile::network
 
   include profile::munin::master
+  include profile::munin::stats_export
   #include profile::puppet::agent
   #include profile::puppet::master
   include profile::apache::simple_server
