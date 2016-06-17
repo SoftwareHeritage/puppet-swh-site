b214aeb1cd9e7c093e1b37482ee3b383a876c251
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-06-17 17:34:59 +0200

    Add vhost for stats_export

--- manifests/munin/stats_export.pp
+++ manifests/munin/stats_export.pp
@@ -1,5 +1,12 @@
 # stats_export master class
 class profile::munin::stats_export {
+  $vhost_name = hiera('stats_export::vhost::name')
+  $vhost_docroot = hiera('stats_export::vhost::docroot')
+  $vhost_ssl_protocol = hiera('stats_export::vhost::ssl_protocol')
+  $vhost_ssl_honorcipherorder = hiera('stats_export::vhost::ssl_honorcipherorder')
+  $vhost_ssl_cipher = hiera('stats_export::vhost::ssl_cipher')
+  $vhost_hsts_header = hiera('stats_export::vhost::hsts_header')
+
   $packages = ['python3-click']
 
   package {$packages:
@@ -14,4 +21,47 @@ class profile::munin::stats_export {
     source  => 'puppet:///modules/profile/munin/stats_export/export-rrd',
     require => Package[$packages],
   }
+
+  file {$vhost_docroot:
+    ensure  => directory,
+    owner   => 'munin',
+    group   => 'www-data',
+    mode    => '2755',
+  }
+
+  include ::profile::ssl
+  include ::apache
+
+  ::apache::vhost {"${vhost_name}_non-ssl":
+    servername      => $vhost_name,
+    port            => '80',
+    docroot         => $vhost_docroot,
+    redirect_status => 'permanent',
+    redirect_dest   => "https://${vhost_name}/",
+  }
+
+  $ssl_cert_name = 'star_softwareheritage_org'
+  $ssl_cert = $::profile::ssl::certificate_paths[$ssl_cert_name]
+  $ssl_ca   = $::profile::ssl::ca_paths[$ssl_cert_name]
+  $ssl_key  = $::profile::ssl::private_key_paths[$ssl_cert_name]
+
+  ::apache::vhost {"${vhost_name}_ssl":
+    servername           => $vhost_name,
+    port                 => '443',
+    ssl                  => true,
+    ssl_protocol         => $vhost_ssl_protocol,
+    ssl_honorcipherorder => $vhost_ssl_honorcipherorder,
+    ssl_cipher           => $vhost_ssl_cipher,
+    ssl_cert             => $ssl_cert,
+    ssl_ca               => $ssl_ca,
+    ssl_key              => $ssl_key,
+    headers              => [$vhost_hsts_header],
+    docroot              => $vhost_docroot,
+    require              => [
+        File[$ssl_cert],
+        File[$ssl_ca],
+        File[$ssl_key],
+    ],
+  }
+
 }
