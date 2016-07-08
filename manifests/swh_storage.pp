1ea3af0fc90714415c147ba9fe393819284384e1
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-07-08 14:31:08 +0200

    role swh_storage enhanced with profile::swh::deploy::storage::archiver
    Related T481

--- manifests/swh_storage.pp
+++ manifests/swh_storage.pp
@@ -1,5 +1,5 @@
 class role::swh_storage inherits role::swh_server {
   include profile::puppet::agent
   include profile::swh::deploy::storage
-  include profile::swh::deploy::storage::archiver
+  include profile::swh::deploy::storage_archiver
 }
