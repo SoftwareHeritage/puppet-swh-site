80f0713294a4cebad231b58646f3bbc3a458902b
Stefano Zacchiroli <zack@upsilon.cc>
2016-07-26 14:24:34 +0200

    create new profile::annex_web and add it to pergamon

--- manifests/swh_sysadmin.pp
+++ manifests/swh_sysadmin.pp
@@ -8,4 +8,6 @@ class role::swh_sysadmin inherits role::swh_server {
   include profile::apache::simple_server
   include profile::bind_server
   include profile::munin::plugins::postgresql
+
+  include profile::annex_web
 }
