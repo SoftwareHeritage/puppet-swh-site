364c82ba829c67c14d3995e526197a2cccbd3f44
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-08-19 17:30:00 +0200

    Add deployment of the scheduler services.
    Close T326

---
+++ manifests/swh/deploy/scheduler.pp
@@ -0,0 +1,73 @@
+# Deployment of swh-scheduler related utilities
+class profile::swh::deploy::scheduler {
+  $conf_file = hiera('swh::deploy::scheduler::conf_file')
+  $user = hiera('swh::deploy::storage::user')
+  $group = hiera('swh::deploy::storage::group')
+  $db_host = hiera('swh::deploy::storage::database')
+
+  include ::systemd
+
+  $listener_service_name = 'swh-scheduler-listener'
+  $listener_service_file = "/etc/systemd/system/${listener_service_name}.service"
+  $listener_service_template = "profile/swh/deploy/scheduler/${listener_service_name}.service.erb"
+
+  $runner_service_name = 'swh-scheduler-runner'
+  $runner_service_file = "/etc/systemd/system/${runner_service_name}.service"
+  $runner_service_template = "profile/swh/deploy/scheduler/${runner_service_name}.service.erb"
+
+  $services = [$listener_service_name, $runner_service_name]
+
+  package {'python3-swh.scheduler':
+    ensure => latest,
+    notify => Service[$services],
+  }
+
+  # Template uses variables
+  #  - $database
+  #
+  file {$conf_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => $group,
+    mode    => '0640',
+    content => template('profile/swh/deploy/scheduler/scheduler.ini.erb'),
+    notify  => Service[$services],
+  }
+
+  # Template uses variables
+  #  - $user
+  #  - $group
+  #
+  file {$listener_service_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($listener_service_template),
+    notify  => [
+      Exec['systemd-daemon-reload'],
+      Service[$listener_service_name],
+    ],
+  }
+
+  # Template uses variables
+  #  - $user
+  #  - $group
+  #
+  file {$runner_service_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($runner_service_template),
+    notify  => [
+      Exec['systemd-daemon-reload'],
+      Service[$runner_service_name],
+    ],
+  }
+
+  service {$services:
+    ensure => running,
+    enable => true,
+  }
+}
