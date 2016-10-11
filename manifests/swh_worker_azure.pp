283f86c626786db3c7b38d090bba8764dc80fb5c
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-10-11 11:53:27 +0200

    swh_worker_azure: add apt config

--- manifests/swh_worker_azure.pp
+++ manifests/swh_worker_azure.pp
@@ -1,3 +1,5 @@
-class role::swh_worker_azure inherits role::swh_worker {
+# Configuration for Azure workers
 
+class role::swh_worker_azure inherits role::swh_worker {
+  include ::profile::swh::apt_config::azure
 }
