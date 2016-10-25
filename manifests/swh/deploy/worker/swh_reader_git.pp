96eaa32ebe60b65ea88bc4f5c8571522f603e272
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-10-25 18:53:29 +0200

    swh::deploy::worker::swh_reader_git: Add reader git queue
    Related T595

---
+++ manifests/swh/deploy/worker/swh_reader_git.pp
@@ -0,0 +1,42 @@
+# Deployment for swh-reader-git
+class profile::swh::deploy::worker::swh_reader_git {
+  include ::profile::swh::deploy::loader
+  include ::profile::swh::deploy::worker::swh_storage_archiver_azure;;
+
+  $concurrency = hiera('swh::deploy::worker::swh_reader_git::concurrency')
+  $loglevel = hiera('swh::deploy::worker::swh_reader_git::loglevel')
+  $task_broker = hiera('swh::deploy::worker::swh_reader_git::task_broker')
+
+  $config_file = '/etc/softwareheritage/loader/git-remote-reader.yml'
+  $config = hiera('swh::deploy::worker::swh_reader_git::config')
+
+  $task_modules = ['swh.loader.git.tasks']
+  $task_queues = ['swh_reader_git']
+
+  $packages = ['python3-swh.loader.git']
+
+  package {$packages:
+    ensure => 'installed',
+  }
+
+  ::profile::swh::deploy::worker::instance {'swh_reader_git':
+    ensure       => present,
+    concurrency  => $concurrency,
+    loglevel     => $loglevel,
+    task_broker  => $task_broker,
+    task_modules => $task_modules,
+    task_queues  => $task_queues,
+    require      => [
+      Package[$packages],
+      File[$config_file],
+    ],
+  }
+
+  file {$config_file:
+    ensure  => 'present',
+    owner   => 'swhworker',
+    group   => 'swhworker',
+    mode    => '0644',
+    content => inline_template('<%= @config.to_yaml %>'),
+  }
+}
