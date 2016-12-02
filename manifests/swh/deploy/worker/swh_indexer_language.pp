3442f2479f520ca8a4f405df35854877e186da53
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-12-02 18:30:01 +0100

    swh::deploy::worker::swh_indexer_language: Update manifest
    Related T610

--- manifests/swh/deploy/worker/swh_indexer_language.pp
+++ manifests/swh/deploy/worker/swh_indexer_language.pp
@@ -7,6 +7,12 @@ class profile::swh::deploy::worker::swh_indexer_language {
   $loglevel = hiera('swh::deploy::worker::swh_indexer::language::loglevel')
   $task_broker = hiera('swh::deploy::worker::swh_indexer::language::task_broker')
 
+  $config_file = '/etc/softwareheritage/indexer/language.yml'
+  $config = hiera('swh::deploy::worker::swh_indexer::language::config')
+
+  $objstorage_config = hiera('swh::azure_objstorage::config')
+  $merged_config = merge($config, {'objstorage' => $objstorage_config})
+
   $task_modules = ['swh.indexer.tasks']
   $task_queues = ['swh_indexer_content_language']
 
@@ -20,6 +26,16 @@ class profile::swh::deploy::worker::swh_indexer_language {
     require      => [
       Class['profile::swh::deploy::indexer'],
       Class['profile::swh::deploy::objstorage_cloud'],
+      File[$config_file],
     ],
+  }
+
+  file {$config_file:
+    ensure  => 'present',
+    owner   => 'swhworker',
+    group   => 'swhdev',
+    # Contains passwords
+    mode    => '0640',
+    content => inline_template('<%= @merged_config.to_yaml %>'),
   }
 }
