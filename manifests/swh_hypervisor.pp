8f30ee3486ba395b8780e1ee2e4431ca50f11c66
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-12-06 16:15:23 +0100

    Create new role hypervisor_master

--- manifests/swh_hypervisor.pp
+++ manifests/swh_hypervisor.pp
@@ -1,4 +1,3 @@
 class role::swh_hypervisor inherits role::swh_server {
-  include profile::dar::server
   include profile::puppet::agent
 }
---
+++ manifests/swh_hypervisor_master.pp
@@ -0,0 +1,3 @@
+class role::swh_hypervisor_master inherits role::swh_hypervisor {
+  include profile::dar::server
+}
