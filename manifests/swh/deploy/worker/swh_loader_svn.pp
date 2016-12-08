91a167d82b5fce68ba94d10b7a0ae8716e468ff6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-12-08 18:23:45 +0100

    swh::deploy::worker::swh_loader_svn: Add manifest
    Related T328
    Related T570

---
+++ manifests/swh/deploy/worker/swh_loader_svn.pp
@@ -0,0 +1,39 @@
+# Deployment for swh-loader-svn
+class profile::swh::deploy::worker::swh_loader_svn {
+  $concurrency = hiera('swh::deploy::worker::swh_loader_svn::concurrency')
+  $loglevel = hiera('swh::deploy::worker::swh_loader_svn::loglevel')
+  $task_broker = hiera('swh::deploy::worker::swh_loader_svn::task_broker')
+
+  $config_file = '/etc/softwareheritage/loader/svn.yml'
+  $config = hiera('swh::deploy::worker::swh_loader_svn::config')
+
+  $task_modules = ['swh.loader.svn.tasks']
+  $task_queues = ['swh_loader_svn']
+
+  $packages = ['python3-swh.loader.svn']
+
+  package {$packages:
+    ensure => 'installed',
+  }
+
+  ::profile::swh::deploy::worker::instance {'swh_loader_svn':
+    ensure       => present,
+    concurrency  => $concurrency,
+    loglevel     => $loglevel,
+    task_broker  => $task_broker,
+    task_modules => $task_modules,
+    task_queues  => $task_queues,
+    require      => [
+      Package[$packages],
+      File[$config_file],
+    ],
+  }
+
+  file {$config_file:
+    ensure  => 'present',
+    owner   => 'swhworker',
+    group   => 'swhworker',
+    mode    => '0644',
+    content => inline_template('<%= @config.to_yaml %>'),
+  }
+}
