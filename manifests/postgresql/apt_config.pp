7b5c8b7d7b23aa5a0644c6d413cc76efe9d0c958
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-01-06 14:32:40 +0100

    postgresql::apt_config: add mandatory keyid

--- manifests/postgresql/apt_config.pp
+++ manifests/postgresql/apt_config.pp
@@ -1,8 +1,10 @@
 # PostgreSQL APT configuration
 class profile::postgresql::apt_config {
   $pgdg_mirror = hiera('postgresql::apt_config::pgdg::mirror')
+  $pgdg_keyid = hiera('postgresql::apt_config::pgdg::keyid')
   $pgdg_key = hiera('postgresql::apt_config::pgdg::key')
   $pglogical_mirror = hiera('postgresql::apt_config::pglogical::mirror')
+  $pglogical_keyid = hiera('postgresql::apt_config::pglogical::keyid')
   $pglogical_key = hiera('postgresql::apt_config::pglogical::key')
 
   ::apt::source {'pgdg':
@@ -10,6 +12,7 @@ class profile::postgresql::apt_config {
     release  => "${::lsbdistcodename}-pgdg",
     repos    => 'main',
     key      => {
+      id      => $pgdg_keyid,
       content => $pgdg_key,
     },
   }
@@ -19,6 +22,7 @@ class profile::postgresql::apt_config {
     release  => "${::lsbdistcodename}-2ndquadrant",
     repos    => 'main',
     key      => {
+      id      => $pglogical_keyid,
       content => $pglogical_key,
     },
   }
