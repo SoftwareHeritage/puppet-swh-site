50f5a2f2b7e2a87b4ec64a91beb451b3266915a7
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-01-23 14:00:23 +0100

    swh_api: add profile::redis

--- manifests/swh_api.pp
+++ manifests/swh_api.pp
@@ -2,8 +2,12 @@ class role::swh_api inherits role::swh_server {
   include profile::network
   include profile::puppet::agent
 
+  # Scheduler
   include profile::munin::plugins::rabbitmq
+  include profile::swh::deploy::scheduler
+
+  # Web UI
+  include profile::redis
   include profile::swh::deploy::storage
   include profile::swh::deploy::webapp
-  include profile::swh::deploy::scheduler
 }
