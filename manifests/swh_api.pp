a21a6bb841ca453140b8a59baff510e3e15e3e9a
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-01-31 13:27:01 +0100

    manifests: Move scheduler deployment to swh_scheduler

--- manifests/swh_api.pp
+++ manifests/swh_api.pp
@@ -2,10 +2,6 @@ class role::swh_api inherits role::swh_server {
   include profile::network
   include profile::puppet::agent
 
-  # Scheduler
-  include profile::munin::plugins::rabbitmq
-  include profile::swh::deploy::scheduler
-
   # Web UI
   include profile::redis
   include profile::swh::deploy::storage
