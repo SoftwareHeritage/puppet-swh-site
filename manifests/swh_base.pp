a4f4e68bb3cdec9eb8ffeec87d50e5f332fb8e38
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-02-23 16:06:51 +0100

    swh_base: add unbound and resolv.conf

--- manifests/swh_base.pp
+++ manifests/swh_base.pp
@@ -1,6 +1,8 @@
 class role::swh_base {
   include profile::base
   include profile::ssh::server
+  include profile::unbound
+  include profile::resolv_conf
   include profile::munin::node
 
   include profile::swh
