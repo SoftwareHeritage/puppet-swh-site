deb5e70ecd21931d053f3eade9f09114d8b92518
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-02-24 15:44:37 +0100

    swh_eventlog: add icinga2 agent

--- manifests/swh_eventlog.pp
+++ manifests/swh_eventlog.pp
@@ -1,6 +1,8 @@
 class role::swh_eventlog inherits role::swh_server {
   include profile::puppet::agent
 
+  include profile::icinga2::agent
+
   include profile::kafka::server
   include profile::swh::deploy::storage_listener
 }
