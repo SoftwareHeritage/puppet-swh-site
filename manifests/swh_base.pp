b223513c99eea5347d4b5a5265ab58f52287d3c8
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-03-14 12:31:23 +0100

    swh_base: add profile::systemd_journal

--- manifests/swh_base.pp
+++ manifests/swh_base.pp
@@ -2,6 +2,7 @@ class role::swh_base {
   include profile::base
   include profile::ssh::server
   include profile::unbound
+  include profile::systemd_journal
   include profile::resolv_conf
   include profile::munin::node
 
