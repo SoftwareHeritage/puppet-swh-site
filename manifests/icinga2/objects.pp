89c09222ce8785b93b65442a4da35c580e3f5ead
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-03-16 16:49:54 +0100

    icinga2::objects: add static checks

--- manifests/icinga2/objects.pp
+++ manifests/icinga2/objects.pp
@@ -2,4 +2,5 @@
 class profile::icinga2::objects {
   include profile::icinga2::objects::templates
   include profile::icinga2::objects::common_checks
+  include profile::icinga2::objects::static_checks
 }
---
+++ manifests/icinga2/objects/static_checks.pp
@@ -0,0 +1,24 @@
+# Static checks on the icinga master
+class profile::icinga2::objects::static_checks {
+
+  $checks_file = '/etc/icinga2/conf.d/static-checks.conf'
+
+  ::icinga2::object::host {'www.softwareheritage.org':
+    import => ['generic-host'],
+    target => $checks_file,
+  }
+
+  ::icinga2::object::service {'Software Heritage Homepage':
+    import    => ['generic-service'],
+    host_name => 'www.softwareheritage.org',
+    command   => 'http',
+    vars      => {
+      http_vhost  => 'www.softwareheritage.org',
+      http_uri    => '/',
+      http_ssl    => true,
+      http_sni    => true,
+      http_string => '<title>Software Heritage</title>',
+    },
+    target    => $checks_file,
+  }
+}
