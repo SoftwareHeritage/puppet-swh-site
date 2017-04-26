d87f33b9373b99b54af83191f35856ca87d09591
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-04-26 16:10:12 +0200

    systemd_journal::journalbeat: add new class to configure journalbeat

--- manifests/systemd_journal.pp
+++ manifests/systemd_journal.pp
@@ -1,8 +1,7 @@
 # Manage the configuration of the systemd journal
 class profile::systemd_journal {
 
-  $role = hiera('systemd_journal::role')
-
   include profile::systemd_journal::apt_config
   include profile::systemd_journal::base_config
+  include profile::systemd_journal::journalbeat
 }
---
+++ manifests/systemd_journal/journalbeat.pp
@@ -0,0 +1,78 @@
+# Journalbeat: a systemd journal collection beater for the ELK stack
+class profile::systemd_journal::journalbeat {
+  $package = 'journalbeat'
+  $user = 'journalbeat'
+  $group = 'nogroup'
+  $homedir = '/var/lib/journalbeat'
+  $configdir = '/etc/journalbeat'
+  $configfile = "${configdir}/journalbeat.yml"
+  $service = 'journalbeat'
+  $servicefile = "/etc/systemd/system/${service}.service"
+
+  $logstash_hosts = hiera('systemd_journal::logstash_hosts')
+
+  include ::systemd
+
+  package {$package:
+    ensure => present
+  }
+
+  user {$user:
+    ensure     => present,
+    gid        => $group,
+    groups     => 'systemd-journal',
+    home       => $homedir,
+    managehome => true,
+    system     => true,
+  }
+
+  # Uses variables
+  #  - $user
+  #  - $homedir
+  #  - $configfile
+  #
+  file {$servicefile:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('profile/systemd/journalbeat/journalbeat.service.erb'),
+    require => Package[$package],
+    notify  => [
+      Exec['systemd-daemon-reload'],
+      Service[$service],
+    ],
+  }
+
+  file {$configdir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  # Uses variables
+  #  - $logstash_hosts
+  #
+  file {$configfile:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('profile/systemd/journalbeat/journalbeat.yml.erb'),
+    notify  => [
+      Exec['systemd-daemon-reload'],
+      Service[$service],
+    ],
+  }
+
+  service {$service:
+    ensure  => running,
+    enable  => true,
+    require => [
+      File[$servicefile],
+      File[$configfile],
+      Exec['systemd-daemon-reload'],
+    ],
+  }
+}
