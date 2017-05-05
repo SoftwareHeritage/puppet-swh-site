b6cd4811db1cbfee32be9eec98b3c796c7cf3569
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-05-05 19:48:08 +0200

    icinga2::notifications: add host notifications

--- manifests/icinga2/objects/notifications.pp
+++ manifests/icinga2/objects/notifications.pp
@@ -2,16 +2,17 @@
 class profile::icinga2::objects::notifications {
   $notification_file = '/etc/icinga2/conf.d/notifications.conf'
 
-  $type = 'service'
-  $apply_target = "${type[0].upcase}${type[1,-1]}"
   $means = 'irc'
 
-  ::icinga2::object::notification {"${means}-notify-all-${type}s":
-    import       => ["${means}-${type}-notification"],
-    apply        => true,
-    apply_target => $apply_target,
-    assign       => [true],
-    users        => ['root'],
-    target       => $notification_file,
+  each(['host', 'service']) |$type| {
+    $apply_target = "${type[0].upcase}${type[1,-1]}"
+    ::icinga2::object::notification {"${means}-notify-all-${type}s":
+      import       => ["${means}-${type}-notification"],
+      apply        => true,
+      apply_target => $apply_target,
+      assign       => [true],
+      users        => ['root'],
+      target       => $notification_file,
+    }
   }
 }
