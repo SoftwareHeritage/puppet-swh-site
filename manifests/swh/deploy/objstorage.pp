564baad8d889b70a2712e9521c54fcf250d687f5
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-05-19 18:01:31 +0200

    objstorage: add icinga checks

--- manifests/swh/deploy/objstorage.pp
+++ manifests/swh/deploy/objstorage.pp
@@ -10,7 +10,10 @@ class profile::swh::deploy::objstorage {
 
   $swh_packages = ['python3-swh.objstorage']
 
-  $backend_listen_address = hiera('swh::deploy::objstorage::backend::listen')
+  $backend_listen_host = hiera('swh::deploy::objstorage::backend::listen::host')
+  $backend_listen_port = hiera('swh::deploy::objstorage::backend::listen::port')
+  $backend_listen_address = "${backend_listen_host}:${backend_listen_port}"
+
   $backend_workers = hiera('swh::deploy::objstorage::backend::workers')
   $backend_http_keepalive = hiera('swh::deploy::objstorage::backend::http_keepalive')
   $backend_http_timeout = hiera('swh::deploy::objstorage::backend::http_timeout')
@@ -54,4 +57,36 @@ class profile::swh::deploy::objstorage {
     }
   }
 
+
+  @@::icinga2::object::service {"swh-objstorage api (localhost on ${::fqdn})":
+    service_name     => 'swh-objstorage api (localhost)',
+    import           => ['generic-service'],
+    host_name        => $::fqdn,
+    check_command    => 'http',
+    command_endpoint => $::fqdn,
+    vars             => {
+      http_address => '127.0.0.1',
+      http_port    => $backend_listen_port,
+      http_uri     => '/',
+      http_string  => 'SWH Objstorage API server',
+    },
+    target           => $icinga_checks_file,
+    tag              => 'icinga2::exported',
+  }
+
+  if $backend_listen_host != '127.0.0.1' {
+    @@::icinga2::object::service {"swh-objstorage api (remote on ${::fqdn})":
+      service_name  => 'swh-objstorage api (remote)',
+      import        => ['generic-service'],
+      host_name     => $::fqdn,
+      check_command => 'http',
+      vars          => {
+        http_port   => $backend_listen_port,
+        http_uri    => '/',
+        http_string => 'SWH Objstorage API server',
+      },
+      target        => $icinga_checks_file,
+      tag           => 'icinga2::exported',
+    }
+  }
 }
