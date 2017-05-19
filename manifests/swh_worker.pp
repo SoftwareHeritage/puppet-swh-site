60dc2246b15962a5b95fea0691701c509584fb42
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-05-19 16:13:56 +0200

    swh_worker: stop deploying a storage instance everywhere

--- manifests/swh_worker.pp
+++ manifests/swh_worker.pp
@@ -1,6 +1,5 @@
 class role::swh_worker inherits role::swh_base {
   include profile::puppet::agent
-  include profile::swh::deploy::storage
   include profile::swh::deploy::worker
   include profile::mountpoints
 }
