b559ddb98f4458aeaaba59d918826605e0e33346
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-06-28 14:39:04 +0200

    worker::swh_lister_github: update for new lister

--- manifests/swh/deploy/worker/swh_lister_github.pp
+++ manifests/swh/deploy/worker/swh_lister_github.pp
@@ -4,13 +4,13 @@ class profile::swh::deploy::worker::swh_lister_github {
   $loglevel = hiera('swh::deploy::worker::swh_lister_github::loglevel')
   $task_broker = hiera('swh::deploy::worker::swh_lister_github::task_broker')
 
-  $config_file = '/etc/softwareheritage/lister-github.yml'
+  $config_file = '/etc/softwareheritage/lister-github.com.yml'
   $config = hiera_hash('swh::deploy::worker::swh_lister_github::config')
 
   $task_modules = ['swh.lister.github.tasks']
-  $task_queues = ['swh_lister_github_incremental', 'swh_lister_github_full']
+  $task_queues = ['swh_lister_github_discover', 'swh_lister_github_refresh']
 
-  $packages = ['python3-swh.lister.github']
+  $packages = ['python3-swh.lister']
 
   package {$packages:
     ensure => 'installed',
