33d215cb719c030f0e069c2ac5c7934d54be3206
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-09-22 15:55:07 +0200

    deploy/deposit: Deploy apache in front of swh-deposit

--- manifests/swh/deploy/deposit.pp
+++ manifests/swh/deploy/deposit.pp
@@ -23,6 +23,17 @@ class profile::swh::deploy::deposit {
   $backend_http_timeout = hiera('swh::deploy::deposit::backend::http_timeout')
   $backend_reload_mercy = hiera('swh::deploy::deposit::backend::reload_mercy')
 
+  $vhost_name = hiera('swh::deploy::deposit::vhost::name')
+  $vhost_aliases = hiera('swh::deploy::deposit::vhost::aliases')
+  $vhost_docroot = hiera('swh::deploy::deposit::vhost::docroot')
+  $vhost_basic_auth_file = "${conf_directory}/http_auth"
+  # swh::deploy::deposit::vhost::basic_auth_content in private
+  $vhost_basic_auth_content = hiera('swh::deploy::deposit::vhost::basic_auth_content')
+  $vhost_ssl_protocol = hiera('swh::deploy::deposit::vhost::ssl_protocol')
+  $vhost_ssl_honorcipherorder = hiera('swh::deploy::deposit::vhost::ssl_honorcipherorder')
+  $vhost_ssl_cipher = hiera('swh::deploy::deposit::vhost::ssl_cipher')
+  $locked_endpoints = hiera_array('swh::deploy::deposit::locked_endpoints')
+
   include ::gunicorn
 
   package {$swh_packages:
@@ -72,6 +83,96 @@ class profile::swh::deploy::deposit {
     }
   }
 
+  $endpoint_directories = $locked_endpoints.map |$endpoint| {
+    { path           => "^${endpoint}",
+      provider       => 'locationmatch',
+      auth_type      => 'Basic',
+      auth_name      => 'Software Heritage Deposit',
+      auth_user_file => $vhost_basic_auth_file,
+      auth_require   => 'valid-user',
+    }
+  }
+
+  include ::profile::ssl
+  include ::profile::apache::common
+  include ::apache::mod::proxy
+  include ::apache::mod::headers
+
+  ::apache::vhost {"${vhost_name}_non-ssl":
+    servername      => $vhost_name,
+    serveraliases   => $vhost_aliases,
+    port            => '80',
+    docroot         => $vhost_docroot,
+    redirect_status => 'permanent',
+    redirect_dest   => "https://${vhost_name}/",
+  }
+
+  $ssl_cert_name = 'star_softwareheritage_org'
+  $ssl_cert = $::profile::ssl::certificate_paths[$ssl_cert_name]
+  $ssl_ca   = $::profile::ssl::ca_paths[$ssl_cert_name]
+  $ssl_key  = $::profile::ssl::private_key_paths[$ssl_cert_name]
+
+  ::apache::vhost {"${vhost_name}_ssl":
+    servername           => $vhost_name,
+    serveraliases        => $vhost_aliases,
+    port                 => '443',
+    ssl                  => true,
+    ssl_protocol         => $vhost_ssl_protocol,
+    ssl_honorcipherorder => $vhost_ssl_honorcipherorder,
+    ssl_cipher           => $vhost_ssl_cipher,
+    ssl_cert             => $ssl_cert,
+    ssl_ca               => $ssl_ca,
+    ssl_key              => $ssl_key,
+    docroot              => $vhost_docroot,
+    proxy_pass           => [
+      { path => '/static',
+        url  => '!',
+      },
+      { path => '/robots.txt',
+        url  => '!',
+      },
+      { path => '/favicon.ico',
+        url  => '!',
+      },
+      { path => '/',
+        url  => "http://${backend_listen_address}/",
+      },
+    ],
+    directories          => [
+      { path     => '/1',
+        provider => 'location',
+        allow    => 'from all',
+        satisfy  => 'Any',
+        headers  => ['add Access-Control-Allow-Origin "*"'],
+      },
+      { path    => $static_dir,
+        options => ['-Indexes'],
+      },
+    ] + $endpoint_directories,
+    aliases              => [
+      { alias => '/static',
+        path  => $static_dir,
+      },
+      { alias => '/robots.txt',
+        path  => "${static_dir}/robots.txt",
+      },
+    ],
+    require              => [
+      File[$vhost_basic_auth_file],
+      File[$ssl_cert],
+      File[$ssl_ca],
+      File[$ssl_key],
+    ],
+  }
+
+  file {$vhost_basic_auth_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'www-data',
+    mode    => '0640',
+    content => $vhost_basic_auth_content,
+  }
+
   $icinga_checks_file = '/etc/icinga2/conf.d/exported-checks.conf'
 
   @@::icinga2::object::service {"swh-deposit api (localhost on ${::fqdn})":
@@ -105,4 +206,52 @@ class profile::swh::deploy::deposit {
       tag           => 'icinga2::exported',
     }
   }
+
+  @@::icinga2::object::service {"swh-deposit http redirect on ${::fqdn}":
+    service_name  => 'swh deposit http redirect',
+    import        => ['generic-service'],
+    host_name     => $::fqdn,
+    check_command => 'http',
+    vars          => {
+      http_address => $vhost_name,
+      http_vhost   => $vhost_name,
+      http_uri     => '/',
+    },
+    target        => $icinga_checks_file,
+    tag           => 'icinga2::exported',
+  }
+
+  @@::icinga2::object::service {"swh-deposit https on ${::fqdn}":
+    service_name  => 'swh deposit',
+    import        => ['generic-service'],
+    host_name     => $::fqdn,
+    check_command => 'http',
+    vars          => {
+      http_address    => $vhost_name,
+      http_vhost      => $vhost_name,
+      http_ssl        => true,
+      http_sni        => true,
+      http_uri        => '/',
+      http_onredirect => sticky
+    },
+    target        => $icinga_checks_file,
+    tag           => 'icinga2::exported',
+  }
+
+  @@::icinga2::object::service {"swh-deposit https certificate ${::fqdn}":
+    service_name  => 'swh deposit https certificate',
+    import        => ['generic-service'],
+    host_name     => $::fqdn,
+    check_command => 'http',
+    vars          => {
+      http_address     => $vhost_name,
+      http_vhost       => $vhost_name,
+      http_ssl         => true,
+      http_sni         => true,
+      http_certificate => 60,
+    },
+    target        => $icinga_checks_file,
+    tag           => 'icinga2::exported',
+  }
+
 }
