c5d68a2bc121eea62adcb171a33b2fe280d4590c
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-10-19 15:50:26 +0200

    add icingaweb2 profile to swh_sysadmin

--- manifests/swh_sysadmin.pp
+++ manifests/swh_sysadmin.pp
@@ -5,6 +5,9 @@ class role::swh_sysadmin inherits role::swh_server {
   include profile::munin::stats_export
 
   include profile::puppet::master
+
+  include profile::icinga2::icingaweb2
+
   include profile::apache::simple_server
   include profile::bind_server
   include profile::munin::plugins::postgresql
