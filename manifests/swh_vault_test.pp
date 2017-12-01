574ef98e0c16a496ae6aa694630c252b9d0f9418
Antoine Pietri <antoine.pietri1@gmail.com>
2017-12-01 16:43:22 +0100

    [test] vault: force include swh_vault_cooker in vault_test

--- manifests/swh_vault_test.pp
+++ manifests/swh_vault_test.pp
@@ -2,6 +2,7 @@ class role::swh_vault_test inherits role::swh_server {
   include profile::puppet::agent
 
   include profile::swh::deploy::vault
+  include profile::swh::deploy::worker::swh_vault_cooker
 
   include profile::munin::plugins::postgresql
   include profile::postgresql
