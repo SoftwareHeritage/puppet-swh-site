7028cf390a58410d26d76f0f907529a3c34e99a0
Antoine Pietri <antoine.pietri1@gmail.com>
2017-12-01 17:09:06 +0100

    vault: include generic swh::deploy::worker profile instead of manually specifying the workers to include

--- manifests/swh_vault_test.pp
+++ manifests/swh_vault_test.pp
@@ -2,7 +2,7 @@ class role::swh_vault_test inherits role::swh_server {
   include profile::puppet::agent
 
   include profile::swh::deploy::vault
-  include profile::swh::deploy::worker::swh_vault_cooker
+  include profile::swh::deploy::worker
 
   include profile::munin::plugins::postgresql
   include profile::postgresql
