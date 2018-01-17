c05e0b64acd70b7b146db34218d89faa5433879e
François Tigeot <ftigeot@softwareheritage.org>
2018-01-17 17:08:58 +0100

    swh-profile: Create rsyslog profile

---
+++ manifests/rsyslog.pp
@@ -0,0 +1,10 @@
+# Parameters for rsyslog
+class profile::rsyslog {
+
+    file_line {'rsyslog maxsize':
+	path    => '/etc/logrotate.d/rsyslog',
+	line    => 'maxsize 100M',
+	after => 'weekly',
+    }
+
+}
