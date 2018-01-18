10c8a51dd87bbaae6686056c377ec3b723fc97f9
François Tigeot <ftigeot@softwareheritage.org>
2018-01-18 12:09:58 +0100

    rsyslog: Add some whitespace

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -3,7 +3,7 @@ class profile::rsyslog {
 
     file_line {'rsyslog maxsize':
 	path    => '/etc/logrotate.d/rsyslog',
-	line    => 'maxsize 100M',
+	line    => '	maxsize 100M',
 	after => 'weekly',
     }
 
