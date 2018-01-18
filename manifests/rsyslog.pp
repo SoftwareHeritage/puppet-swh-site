8c7a4af1adde9ceaca65cf6441dee96c4c4726d7
François Tigeot <ftigeot@softwareheritage.org>
2018-01-18 13:22:08 +0100

    rsyslog: Match lines independently of whitespace

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -3,8 +3,9 @@ class profile::rsyslog {
 
     file_line {'rsyslog maxsize':
 	path    => '/etc/logrotate.d/rsyslog',
+	match	=> 'maxsize 100M',
 	line    => '	maxsize 100M',
-	after => 'weekly',
+	after	=> 'weekly',
     }
 
 }
