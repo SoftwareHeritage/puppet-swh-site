e7fbbff85ca9abd0a4569aa5dd619bd83cb4fb7f
Antoine Pietri <antoine.pietri1@gmail.com>
2018-01-30 13:05:47 +0100

    vault: fix orangerie hostname matching

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,7 +34,7 @@ node /^(unibo-test|orangeriedev).(internal.)?softwareheritage.org$/ {
   include role::swh_vault_test
 }
 
-node /^(unibo-test|orangeriedev).(internal.)?softwareheritage.org$/ {
+node /^(unibo-prod|orangerie).(internal.)?softwareheritage.org$/ {
   include role::swh_vault
 }
 
