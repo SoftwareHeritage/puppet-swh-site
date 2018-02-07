b68df447c45845f0750eacd877b98930a5cfee64
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-02-07 16:10:30 +0100

    Add bootstrap-osd key to ceph osds

--- manifests/ceph/osd.pp
+++ manifests/ceph/osd.pp
@@ -1,4 +1,10 @@
 # Ceph OSD profile
 class profile::ceph::osd {
   include profile::ceph::base
+
+  $bootstrap_osd_key = hiera('ceph::key::bootstrap_osd')
+  ::ceph::key {'client.bootstrap-osd':
+    keyring_path => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
+    secret       => $bootstrap_osd_key,
+  }
 }
