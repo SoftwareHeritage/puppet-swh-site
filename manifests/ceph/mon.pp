ccebe468fbe33328a10fbadd9177f612510c0902
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-02-07 15:44:14 +0100

    Make the ceph monitor manifest do things

--- manifests/ceph/mon.pp
+++ manifests/ceph/mon.pp
@@ -1,4 +1,30 @@
 # Ceph Monitor profile
 class profile::ceph::mon {
   include profile::ceph::base
+
+  $mon_key = hiera('ceph::key::mon')
+  $admin_key = hiera('ceph::key::admin')
+  $bootstrap_osd_key = hiera('ceph::key::bootstrap_osd')
+
+  ::ceph::mon {$::hostname:
+    key => $mon_key,
+  }
+
+  ::Ceph::Key {
+    inject => true,
+    inject_as_id => 'mon.',
+    inject_keyring => "/var/lib/ceph/mon/ceph-${::hostname}/keyring",
+  }
+
+  ::ceph::key {'client.admin':
+    secret  => $admin_key,
+    cap_mon => 'allow *',
+    cap_osd => 'allow *',
+    cap_mds => 'allow',
+  }
+
+  ::ceph::key {'client.bootstrap-osd':
+    secret  => $bootstrap_osd_key,
+    cap_mon => 'allow profile bootstrap-osd',
+  }
 }
