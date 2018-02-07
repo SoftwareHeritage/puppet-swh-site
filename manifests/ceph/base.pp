27a15c6d18fc0a51e1ac0da482e7850b199396a9
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-02-07 15:32:05 +0100

    Add base settings for the ceph cluster

--- manifests/ceph/base.pp
+++ manifests/ceph/base.pp
@@ -2,7 +2,17 @@
 class profile::ceph::base {
   $ceph_release = hiera('ceph::release')
 
+  $ceph_fsid = hiera('ceph::fsid')
+  $ceph_mon_initial_members = join(hiera('ceph::mon_initial_members'), ',')
+  $ceph_mon_host = join(hiera('ceph::mon_host'), ',')
+
   class {'::ceph::repo':
     release => $ceph_release,
+  }
+
+  class {'::ceph':
+    fsid                => $ceph_fsid,
+    mon_initial_members => $ceph_mon_initial_members,
+    mon_host            => $ceph_mon_host,
   }
 }
