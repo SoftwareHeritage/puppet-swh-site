ee4b29647cb473151fe445740f297fd3c66f3475
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-02-16 19:22:22 +0100

    deploy/worker: deposit loader: Use specific /tmp folder
    Related T964

--- manifests/swh/deploy/worker/swh_loader_deposit.pp
+++ manifests/swh/deploy/worker/swh_loader_deposit.pp
@@ -12,6 +12,7 @@ class profile::swh::deploy::worker::swh_loader_deposit {
   $task_queues = ['swh_checker_deposit', 'swh_loader_deposit']
 
   $packages = ['python3-swh.deposit.loader']
+  $private_tmp = hiera('swh::deploy::worker::swh_loader_deposit::private_tmp')
 
   $service_name = 'swh_loader_deposit'
 
@@ -28,6 +29,7 @@ class profile::swh::deploy::worker::swh_loader_deposit {
     task_broker  => $task_broker,
     task_modules => $task_modules,
     task_queues  => $task_queues,
+    private_tmp  => $private_tmp,
     require      => [
       Package[$packages],
       File[$config_file],
