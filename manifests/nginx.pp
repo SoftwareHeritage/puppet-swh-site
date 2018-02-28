4d437692b2823c31c163388ce3bf129b549f9704
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-02-28 18:25:54 +0100

    nginx: set names_hash_bucket and max_size

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,9 +4,14 @@ class profile::nginx {
   $accept_mutex = lookup('nginx::accept_mutex')
   $package_name = lookup('nginx::package_name')
 
+  $names_hash_bucket_size = lookup('nginx::names_hash_bucket_size')
+  $names_hash_max_size = lookup('nginx::names_hash_max_size')
+
   class {'::nginx':
-    package_name => $package_name,
-    manage_repo  => false,
-    accept_mutex => $accept_mutex,
+    package_name           => $package_name,
+    manage_repo            => false,
+    accept_mutex           => $accept_mutex,
+    names_hash_bucket_size => $names_hash_bucket_size,
+    names_hash_max_size    => $names_hash_max_size,
   }
 }
