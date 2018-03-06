a962d2e641b86d7155cacae4033f8bdf001d7813
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-03-06 18:52:06 +0100

    Add worker processes to nginx

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -6,6 +6,12 @@ class profile::nginx {
 
   $names_hash_bucket_size = lookup('nginx::names_hash_bucket_size')
   $names_hash_max_size = lookup('nginx::names_hash_max_size')
+  $worker_processes = lookup('nginx::worker_processes')
+  if $worker_processes != 'auto' {
+    $actual_worker_processes = $worker_processes + 0
+  } else {
+    $actual_worker_processes = 'auto'
+  }
 
   class {'::nginx':
     package_name           => $package_name,
@@ -13,5 +19,6 @@ class profile::nginx {
     accept_mutex           => $accept_mutex,
     names_hash_bucket_size => $names_hash_bucket_size,
     names_hash_max_size    => $names_hash_max_size,
+    worker_processes       => $actual_worker_processes,
   }
 }
