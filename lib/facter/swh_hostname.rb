bc2b2adecd58af512e53829af69fff488ef5d44f
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-03-07 14:48:26 +0100

    swh_hostname: fix whitespace

--- lib/facter/swh_hostname.rb
+++ lib/facter/swh_hostname.rb
@@ -4,16 +4,16 @@ require 'socket'
 domain_name = ".internal.softwareheritage.org"
 
 Facter.add(:swh_hostname) do
-	setcode do
-		retval = {}
-		retval["fqdn"] = Facter.value(:fqdn).to_s
+  setcode do
+    retval = {}
+    retval["fqdn"] = Facter.value(:fqdn).to_s
     if retval["fqdn"].end_with?(domain_name)
       retval["short"] = retval["fqdn"].chomp(domain_name)
       retval["internal_fqdn"] = retval["fqdn"]
     else
-		  retval["short"] = Socket.gethostname
-		  retval["internal_fqdn"] = retval["short"] + domain_name
+      retval["short"] = Socket.gethostname
+      retval["internal_fqdn"] = retval["short"] + domain_name
     end
-		retval
-	end
+    retval
+  end
 end
