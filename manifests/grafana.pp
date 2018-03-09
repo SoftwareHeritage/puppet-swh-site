6a427a6ad3805d1ae3d7f3b905f6dcfb03108e2e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-03-09 19:26:51 +0100

    Add grafana database configuration

--- manifests/grafana.pp
+++ manifests/grafana.pp
@@ -1,5 +1,27 @@
 class profile::grafana {
+  $db = lookup('grafana::db::database')
+  $db_username = lookup('grafana::db::username')
+  $db_password = lookup('grafana::db::password')
+
+  $config = lookup('grafana::config')
+
+  include ::postgresql::server
+
+  ::postgresql::server::db {$db:
+    user     => $db_username,
+    password => postgresql_password($db_username, $db_password),
+  }
+
   class {'::grafana':
     install_method => 'repo',
+    cfg            => $config + {
+      database => {
+        type     => 'postgresql',
+        host     => '127.0.0.1:5432',
+        name     => $db,
+        user     => $db_username,
+        password => $db_password
+      }
+    }
   }
 }
