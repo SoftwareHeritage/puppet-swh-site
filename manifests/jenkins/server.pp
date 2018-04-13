ee1b285f407237b97c05259ebbd71313c445ec1e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-04-13 18:03:50 +0200

    Add jenkins user to docker group

--- manifests/jenkins/server.pp
+++ manifests/jenkins/server.pp
@@ -1,8 +1,3 @@
 class profile::jenkins::server {
-  include profile::jenkins::apt_config
-
-  package {'jenkins':
-    ensure  => present,
-    require => Apt::Source['jenkins'],
-  }
+  include profile::jenkins::service
 }
