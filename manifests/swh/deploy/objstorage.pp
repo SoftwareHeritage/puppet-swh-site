3add37f8b4e2047b97b0977e4622afad1c3bd0ca
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-05-12 18:58:33 +0200

    Update objstorage instantiation

--- manifests/swh/deploy/objstorage.pp
+++ manifests/swh/deploy/objstorage.pp
@@ -19,7 +19,7 @@ class profile::swh::deploy::objstorage {
   }
 
   ::profile::swh::deploy::rpc_server {'objstorage':
-    executable => 'swh.objstorage.api.server:make_app_from_configfile()',
+    executable => 'swh.objstorage.api.server:app',
     worker     => 'async',
   }
 }
