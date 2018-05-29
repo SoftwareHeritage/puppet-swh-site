b418a33b8d128bd4b5dc18a4457399a498a53bab
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-05-29 16:03:04 +0200

    deploy/scheduler: Deploy new services
    Related T1031

---
+++ manifests/swh/deploy/scheduler_updater.pp
@@ -0,0 +1,32 @@
+# Deployment of swh-scheduler-updater related utilities
+
+class profile::swh::deploy::scheduler::updater {
+  # Package and backend configuration
+  $scheduler_updater_packages = ['python3-swh.scheduler.updater']
+
+  package {$scheduler_updater_packages:
+    ensure => latest,
+  }
+
+  $backend_conf_dir = lookup('swh::deploy::scheduler::updater::backend::conf_dir')
+  $backend_conf_file = lookup('swh::deploy::scheduler::updater::backend::conf_file')
+  $backend_user = lookup('swh::deploy::scheduler::updater::backend::user')
+  $backend_group = lookup('swh::deploy::scheduler::updater::backend::group')
+  $backend_config = lookup('swh::deploy::scheduler::updater::backend::config')
+
+  file {$backend_conf_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => $backend_group,
+    mode   => '0755',
+  }
+
+  file {$backend_config:
+    ensure  => present,
+    owner   => 'root',
+    group   => $backend_group,
+    mode    => '0640',
+    content => inline_template("<%= @backend_config.to_yaml %>\n"),
+  }
+
+}
