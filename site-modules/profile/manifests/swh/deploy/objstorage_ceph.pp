b278321e87dc8d60a0e3bb4f9ab0d6401d2faa81
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-06-18 20:57:32 +0200

    Add a ceph.yml file with the objstorage config

--- site-modules/profile/manifests/swh/deploy/objstorage_ceph.pp
+++ site-modules/profile/manifests/swh/deploy/objstorage_ceph.pp
@@ -8,4 +8,14 @@ class profile::swh::deploy::objstorage_ceph {
   package {$objstorage_packages:
     ensure => installed,
   }
+
+  $objstorage_config = lookup('swh::deploy::objstorage::ceph::config')
+
+  file {"${profile::swh::deploy::objstorage::conf_directory}/ceph.yml":
+    ensure  => present,
+    owner   => 'root',
+    group   => $profile::swh::deploy::objstorage::group,
+    mode    => '0640',
+    content => inline_yaml($objstorage_config),
+  }
 }
