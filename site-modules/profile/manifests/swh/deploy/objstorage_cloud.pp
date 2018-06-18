4c5c9e113e5d15611b890610d4ed312ac2871f68
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-06-18 20:26:22 +0200

    Remove useless list from objstorage_cloud manifest

--- site-modules/profile/manifests/swh/deploy/objstorage_cloud.pp
+++ site-modules/profile/manifests/swh/deploy/objstorage_cloud.pp
@@ -1,14 +1,6 @@
 # Deployment of the cloud objstorage
 
 class profile::swh::deploy::objstorage_cloud {
-  $pinned_packages = [
-    'python3-cffi',
-    'python3-cryptography',
-    'python3-pkg-resources',
-    'python3-pyasn1',
-    'python3-setuptools',
-  ]
-
   $objstorage_packages = ['python3-swh.objstorage.cloud']
 
   package {$objstorage_packages:
