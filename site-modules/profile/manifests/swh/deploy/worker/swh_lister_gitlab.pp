0ed66cf469a32da14dd8322cdb34d8ced52e97e8
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-07-18 16:08:36 +0200

    Add new gitlab lister manifest
    Related T1137

---
+++ site-modules/profile/manifests/swh/deploy/worker/swh_lister_gitlab.pp
@@ -0,0 +1,36 @@
+# Deployment for swh-lister-gitlab
+class profile::swh::deploy::worker::swh_lister_gitlab {
+  $concurrency = lookup('swh::deploy::worker::swh_lister_gitlab::concurrency')
+  $loglevel = lookup('swh::deploy::worker::swh_lister_gitlab::loglevel')
+  $task_broker = lookup('swh::deploy::worker::swh_lister_gitlab::task_broker')
+
+  $config_file = '/etc/softwareheritage/lister-gitlab.yml'
+  $config = lookup('swh::deploy::worker::swh_lister_gitlab::config', Hash, 'deep')
+
+  $task_modules = ['swh.lister.gitlab.tasks']
+  $task_queues = ['swh_lister_gitlab_discover', 'swh_lister_gitlab_refresh']
+
+  include ::profile::swh::deploy::base_lister
+
+  ::profile::swh::deploy::worker::instance {'swh_lister_gitlab':
+    ensure       => present,
+    concurrency  => $concurrency,
+    loglevel     => $loglevel,
+    task_broker  => $task_broker,
+    task_modules => $task_modules,
+    task_queues  => $task_queues,
+    require      => [
+      Package['python3-swh.lister'],
+      File[$config_file],
+    ],
+  }
+
+  # Contains passwords
+  file {$config_file:
+    ensure  => 'present',
+    owner   => 'swhworker',
+    group   => 'swhdev',
+    mode    => '0640',
+    content => inline_template("<%= @config.to_yaml %>\n"),
+  }
+}
