5c0d5a2f7ba122a37558e5916ef28406ea3f987e
Valentin Lorentz <vlorentz@softwareheritage.org>
2018-11-21 16:50:43 +0100

    Deploy metadata indexers
    Summary: Apply the same changes as 565518b40e7b93738be8f917f1df4b158b09143b, 856c76fae7a575b352b375006fd5e9da88b924f1, ab31b2f255842d324b4fc719d1a573e36c01309d; but on origin_intrinsic_metadata and revision_metadata indexers.
    
    Reviewers: #reviewers, ardumont
    
    Reviewed By: #reviewers, ardumont
    
    Subscribers: ardumont
    
    Maniphest Tasks: T1375, T1376
    
    Differential Revision: https://forge.softwareheritage.org/D691

--- site-modules/profile/manifests/swh/deploy/worker/swh_indexer_origin_intrinsic_metadata.pp
+++ site-modules/profile/manifests/swh/deploy/worker/swh_indexer_origin_intrinsic_metadata.pp
@@ -7,7 +7,9 @@ class profile::swh::deploy::worker::swh_indexer_origin_intrinsic_metadata {
   $loglevel = lookup('swh::deploy::worker::swh_indexer::origin_intrinsic_metadata::loglevel')
   $task_broker = lookup('swh::deploy::worker::swh_indexer::origin_intrinsic_metadata::task_broker')
 
-  $config_file = '/etc/softwareheritage/indexer/origin_intrinsic_metadata.yml'
+  $config_file = lookup('swh::deploy::worker::swh_indexer::origin_intrinsic_metadata::config_file')
+  $config_directory = lookup('swh::conf_directory')
+  $config_path = "${config_directory}/${config_file}"
   $config = lookup('swh::deploy::worker::swh_indexer::origin_intrinsic_metadata::config')
 
   $task_modules = ['swh.indexer.tasks']
@@ -23,11 +25,11 @@ class profile::swh::deploy::worker::swh_indexer_origin_intrinsic_metadata {
     require      => [
       Class['profile::swh::deploy::indexer'],
       Class['profile::swh::deploy::objstorage_cloud'],
-      File[$config_file],
+      File[$config_path],
     ],
   }
 
-  file {$config_file:
+  file {$config_path:
     ensure  => 'present',
     owner   => 'swhworker',
     group   => 'swhdev',
--- site-modules/profile/manifests/swh/deploy/worker/swh_indexer_revision_metadata.pp
+++ site-modules/profile/manifests/swh/deploy/worker/swh_indexer_revision_metadata.pp
@@ -7,7 +7,9 @@ class profile::swh::deploy::worker::swh_indexer_revision_metadata {
   $loglevel = lookup('swh::deploy::worker::swh_indexer::revision_metadata::loglevel')
   $task_broker = lookup('swh::deploy::worker::swh_indexer::revision_metadata::task_broker')
 
-  $config_file = '/etc/softwareheritage/indexer/revision_metadata.yml'
+  $config_file = lookup('swh::deploy::worker::swh_indexer::revision_metadata::config_file')
+  $config_directory = lookup('swh::conf_directory')
+  $config_path = "${config_directory}/${config_file}"
   $config = lookup('swh::deploy::worker::swh_indexer::revision_metadata::config')
 
   $task_modules = ['swh.indexer.tasks']
@@ -23,11 +25,11 @@ class profile::swh::deploy::worker::swh_indexer_revision_metadata {
     require      => [
       Class['profile::swh::deploy::indexer'],
       Class['profile::swh::deploy::objstorage_cloud'],
-      File[$config_file],
+      File[$config_path],
     ],
   }
 
-  file {$config_file:
+  file {$config_path:
     ensure  => 'present',
     owner   => 'swhworker',
     group   => 'swhdev',
