fb1c07b444c78b3691e41545ca1ff7867957044a
Nicolas Dandrimont <nicolas@dandrimont.eu>
2018-12-13 18:18:36 +0100

    Add an icinga check for backup freshness

--- site-modules/profile/manifests/dar/client.pp
+++ site-modules/profile/manifests/dar/client.pp
@@ -18,12 +18,30 @@ class profile::dar::client {
 
   $server_cron = profile::cron_rand(lookup('dar_server::cron', Hash), 'backup_server')
 
+  $dir_for_fetched_backups = lookup('dar_server::backup::storage')
   # Export a remote backup to the backup server
   @@dar::remote_backup { "${dar_remote_hostname}.${dar_backup_name}":
     remote_backup_storage => lookup('dar::backup::storage'),
     remote_backup_host    => $dar_remote_hostname,
     remote_backup_name    => $dar_backup_name,
-    local_backup_storage  => lookup('dar_server::backup::storage'),
+    local_backup_storage  => $dir_for_fetched_backups,
     *                     => $server_cron,
+  }
+
+  # Export an icinga check to verify backup freshness
+  $icinga_checks_file = '/etc/icinga2/conf.d/exported-checks.conf'
+  $checked_directory = "${dir_for_fetched_backups}/${dar_remote_hostname}"
+
+  @@::icinga2::object::service {"backup freshness for ${dar_remote_hostname}":
+    service_name     => 'backup freshness',
+    import           => ['generic-service'],
+    host_name        => $::fqdn,
+    command_endpoint => 'louvre.softwareheritage.org',
+    check_command    => 'check_newest_file_age',
+    vars             => {
+      check_directory => $checked_directory,
+    },
+    target           => $icinga_checks_file,
+    tag              => 'icinga2::exported',
   }
 }
