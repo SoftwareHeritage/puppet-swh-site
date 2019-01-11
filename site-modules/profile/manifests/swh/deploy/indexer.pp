25afa2d589a870a059ccf2a77e2169e2d44ed599
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-01-11 15:35:54 +0100

    base_indexer: Remove unneeded base configuration
    Every indexer override their base configuration file.

--- site-modules/profile/manifests/swh/deploy/indexer.pp
+++ site-modules/profile/manifests/swh/deploy/indexer.pp
@@ -2,17 +2,4 @@
 class profile::swh::deploy::indexer {
   include ::profile::swh::deploy::base_indexer
   include ::profile::swh::deploy::objstorage_cloud
-
-  $config_directory = lookup('swh::deploy::base_indexer::config_directory')
-  $config_file = "${config_directory}/base.yml"
-  $config = lookup('swh::deploy::worker::swh_indexer::base::config')
-
-  file {$config_file:
-    ensure  => 'present',
-    owner   => 'swhworker',
-    group   => 'swhdev',
-    # Contains passwords
-    mode    => '0640',
-    content => inline_template("<%= @config.to_yaml %>\n"),
-  }
 }
