1a125d4dcc117670a8e8b8f2d0566a682499fa98
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-01-11 13:30:45 +0100

    swh_eventlog: Add indexer_journal_client profile
    Related T1466

--- site-modules/role/manifests/swh_eventlog.pp
+++ site-modules/role/manifests/swh_eventlog.pp
@@ -3,4 +3,5 @@ class role::swh_eventlog inherits role::swh_base {
 
   include profile::kafka::broker
   include profile::swh::deploy::storage_listener
+  include profile::swh::deploy::indexer_journal_client
 }
