2db2e07d1f65a2fabc49e391524d241126836462
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-15 18:14:34 +0100

    ctags: Update configuration
    Related T1525

--- site-modules/profile/manifests/swh/deploy/worker/swh_indexer_ctags.pp
+++ site-modules/profile/manifests/swh/deploy/worker/swh_indexer_ctags.pp
@@ -7,9 +7,7 @@ class profile::swh::deploy::worker::swh_indexer_ctags {
   $loglevel = lookup('swh::deploy::worker::swh_indexer::ctags::loglevel')
   $task_broker = lookup('swh::deploy::worker::swh_indexer::ctags::task_broker')
 
-  $config_file = lookup('swh::deploy::worker::swh_indexer::language::config_file')
-  $config_directory = lookup('swh::deploy::base_indexer::config_directory')
-  $config_path = "${config_directory}/${config_file}"
+  $config_file = lookup('swh::deploy::worker::swh_indexer::ctags::config_file')
   $config = lookup('swh::deploy::worker::swh_indexer::ctags::config')
 
   $task_modules = ['swh.indexer.tasks']
@@ -20,7 +18,7 @@ class profile::swh::deploy::worker::swh_indexer_ctags {
     ensure => 'present',
   }
 
-  Package[$::profile::swh::deploy::base_indexer::packages] ~> ::profile::swh::deploy::worker::instance {'swh_indexer_ctags':
+  Package[$::profile::swh::deploy::base_indexer::packages] ~> ::profile::swh::deploy::worker::instance {'indexer_content_ctags':
     ensure       => present,
     concurrency  => $concurrency,
     loglevel     => $loglevel,
@@ -30,12 +28,12 @@ class profile::swh::deploy::worker::swh_indexer_ctags {
     require      => [
       Class['profile::swh::deploy::indexer'],
       Class['profile::swh::deploy::objstorage_cloud'],
-      File[$config_path],
+      File[$config_file],
       Package[$packages],
     ],
   }
 
-  file {$config_path:
+  file {$config_file:
     ensure  => 'present',
     owner   => 'swhworker',
     group   => 'swhdev',
