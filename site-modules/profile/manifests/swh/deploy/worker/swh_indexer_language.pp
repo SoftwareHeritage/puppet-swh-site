2cbbb95a874e4309b4f98cee1d35f640ca3cb36c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-15 18:26:43 +0100

    language: Update configuration
    Related T1525

--- site-modules/profile/manifests/swh/deploy/worker/swh_indexer_language.pp
+++ site-modules/profile/manifests/swh/deploy/worker/swh_indexer_language.pp
@@ -8,14 +8,12 @@ class profile::swh::deploy::worker::swh_indexer_language {
   $task_broker = lookup('swh::deploy::worker::swh_indexer::language::task_broker')
 
   $config_file = lookup('swh::deploy::worker::swh_indexer::language::config_file')
-  $config_directory = lookup('swh::deploy::base_indexer::config_directory')
-  $config_path = "${config_directory}/${config_file}"
   $config = lookup('swh::deploy::worker::swh_indexer::language::config')
 
   $task_modules = ['swh.indexer.tasks']
   $task_queues = ['swh_indexer_content_language']
 
-  Package[$::profile::swh::deploy::base_indexer::packages] ~> ::profile::swh::deploy::worker::instance {'swh_indexer_language':
+  Package[$::profile::swh::deploy::base_indexer::packages] ~> ::profile::swh::deploy::worker::instance {'indexer_content_language':
     ensure       => present,
     concurrency  => $concurrency,
     loglevel     => $loglevel,
@@ -25,11 +23,11 @@ class profile::swh::deploy::worker::swh_indexer_language {
     require      => [
       Class['profile::swh::deploy::indexer'],
       Class['profile::swh::deploy::objstorage_cloud'],
-      File[$config_path],
+      File[$config_file],
     ],
   }
 
-  file {$config_path:
+  file {$config_file:
     ensure  => 'present',
     owner   => 'swhworker',
     group   => 'swhdev',
