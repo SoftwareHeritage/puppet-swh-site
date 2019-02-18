43c26b7fdea18a28f49ef12a5a4a1fd16d112a70
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-18 16:58:56 +0100

    vault: Fix server executable entry point
    Related T1525

--- site-modules/profile/manifests/swh/deploy/vault.pp
+++ site-modules/profile/manifests/swh/deploy/vault.pp
@@ -5,7 +5,7 @@ class profile::swh::deploy::vault {
   Package['python3-swh.vault'] ~> Service['gunicorn-swh-vault']
 
   ::profile::swh::deploy::rpc_server {'vault':
-    executable => 'swh.vault.api.server:make_app_from_configfile()',
+    executable => 'swh.vault.api.wsgi:app',
     worker     => 'async',
   }
 }
