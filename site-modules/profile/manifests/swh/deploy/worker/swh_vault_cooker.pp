4b8719185edaa84cfa294eb9b25dda221cb3be95
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-18 16:34:28 +0100

    vault-cooker: Update configuration
    Related T1525

--- site-modules/profile/manifests/swh/deploy/worker/swh_vault_cooker.pp
+++ site-modules/profile/manifests/swh/deploy/worker/swh_vault_cooker.pp
@@ -7,13 +7,13 @@ class profile::swh::deploy::worker::swh_vault_cooker {
   $loglevel = lookup('swh::deploy::worker::swh_vault_cooker::loglevel')
   $task_broker = lookup('swh::deploy::worker::swh_vault_cooker::task_broker')
 
-  $conf_file = lookup('swh::deploy::worker::swh_vault_cooker::conf_file')
+  $conf_file = lookup('swh::deploy::worker::swh_vault_cooker::config_file')
   $config = lookup('swh::deploy::worker::swh_vault_cooker::config')
 
   $task_modules = ['swh.vault.cooking_tasks']
   $task_queues = ['swh_vault_cooking', 'swh_vault_batch_cooking']
 
-  ::profile::swh::deploy::worker::instance {'swh_vault_cooker':
+  ::profile::swh::deploy::worker::instance {'vault_cooker':
     ensure       => present,
     concurrency  => $concurrency,
     loglevel     => $loglevel,
