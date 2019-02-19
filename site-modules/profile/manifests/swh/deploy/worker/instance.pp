fa887af3cfa1809cb1f499dd7e04bd3cda911983
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-19 18:53:38 +0100

    worker: Remove no longer used worker/<service>.ini config
    Related T1531

--- site-modules/profile/manifests/swh/deploy/worker/instance.pp
+++ site-modules/profile/manifests/swh/deploy/worker/instance.pp
@@ -16,8 +16,6 @@ define profile::swh::deploy::worker::instance (
 
   $service_basename = "swh-worker@${instance_name}"
   $service_name = "${service_basename}.service"
-  $config_directory = '/etc/softwareheritage/worker'
-  $instance_config = "${config_directory}/${instance_name}.ini"
 
   case $ensure {
     'present', 'running': {
@@ -32,32 +30,13 @@ define profile::swh::deploy::worker::instance (
         content  => template('profile/swh/deploy/worker/parameters.conf.erb'),
       }
 
-      # Uses variables
-      # - $task_broker
-      # - $task_modules
-      # - $task_queues
-      # - $task_soft_time_limit
-      file {$instance_config:
-        ensure  => present,
-        owner   => 'swhworker',
-        group   => 'swhdev',
-        # contains a password for the broker
-        mode    => '0640',
-        content => template('profile/swh/deploy/worker/instance_config.ini.erb'),
-      }
       if $ensure == 'running' {
         service {$service_basename:
           ensure  => $ensure,
-          require => [
-            File[$instance_config],
-          ],
         }
       }
     }
     default: {
-      file {$instance_config:
-        ensure => absent,
-      }
       ::systemd::dropin_file {"${service_basename}/parameters.conf":
         ensure   => absent,
         unit     => $service_name,
