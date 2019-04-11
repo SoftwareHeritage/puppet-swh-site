0ba8097ac0040b044ef5117d1cf0eb8dc5296cd0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-04-11 11:14:54 +0200

    Add npm loader configuration
    Related T1632

---
+++ site-modules/profile/manifests/swh/deploy/worker/loader_npm.pp
@@ -0,0 +1,18 @@
+# Deployment for swh-loader-npm
+class profile::swh::deploy::worker::loader_npm {
+  $private_tmp = lookup('swh::deploy::worker::loader_npm::private_tmp')
+
+  $packages = ['python3-swh.loader.npm']
+
+  package {$packages:
+    ensure => 'present',
+  }
+
+  ::profile::swh::deploy::worker::instance {'loader_npm':
+    ensure       => present,
+    private_tmp  => $private_tmp,
+    require      => [
+      Package[$packages],
+    ],
+  }
+}
