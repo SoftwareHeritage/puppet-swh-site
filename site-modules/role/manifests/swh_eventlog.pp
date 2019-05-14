96011d1089478c6fe2461fb3cbe54126fdb4bfb2
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-05-14 17:25:10 +0200

    Finish storage_listener undeployment

--- site-modules/role/manifests/swh_eventlog.pp
+++ site-modules/role/manifests/swh_eventlog.pp
@@ -2,7 +2,6 @@ class role::swh_eventlog inherits role::swh_base {
   include profile::puppet::agent
 
   include profile::kafka::broker
-  include profile::swh::deploy::storage_listener
   include profile::swh::deploy::journal_publisher
   include profile::swh::deploy::indexer_journal_client
 }
