ddf2ded5a201bc313c7d8660d511b2a6b7927fab
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-05-22 09:38:02 +0200

    vault: Remove no longer needed postgres profile
    The db is plugged to prado and no longer running on the vault node
    
    Related T1716

--- site-modules/role/manifests/swh_vault.pp
+++ site-modules/role/manifests/swh_vault.pp
@@ -2,8 +2,5 @@ class role::swh_vault inherits role::swh_server {
   include profile::puppet::agent
   include profile::swh::deploy::vault
 
-  include profile::munin::plugins::postgresql
-  include profile::postgresql
-
   include profile::swh::deploy::objstorage
 }
