6f7fc6fe41f303ef9e113c3896e3c827c9dff595
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-06-05 14:44:44 +0200

    pgbouncer: Adapt listen_addr, admin_users config appropriately
    Related T1784

--- site-modules/profile/manifests/pgbouncer.pp
+++ site-modules/profile/manifests/pgbouncer.pp
@@ -4,8 +4,18 @@ class profile::pgbouncer {
   $userlist = lookup('pgbouncer::userlist')
   $databases = lookup('pgbouncer::databases')
 
+  # Need format manipulation (expected format in pgbouncer class is {key,
+  # value} with no nested data)
+  $listen_addr = join($config_params['listen_addr'], ',')
+  $admin_users = join($config_params['admin_users'], ',')
+
+  $expected_config_params = merge($config_params, {
+    listen_addr => $listen_addr,
+    admin_users => $admin_users,
+  })
+
   class {'::pgbouncer':
-    config_params => $config_params,
+    config_params => $expected_config_params,
     userlist      => $userlist,
     databases     => $databases,
   }
