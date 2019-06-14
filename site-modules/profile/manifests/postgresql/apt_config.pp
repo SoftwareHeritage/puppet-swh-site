5569db2b8b16c7c132a2583fbbe83a26b8d370db
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-06-14 10:38:34 +0200

    Remove pglogical apt config

--- site-modules/profile/manifests/postgresql/apt_config.pp
+++ site-modules/profile/manifests/postgresql/apt_config.pp
@@ -3,9 +3,6 @@ class profile::postgresql::apt_config {
   $pgdg_mirror = lookup('postgresql::apt_config::pgdg::mirror')
   $pgdg_keyid = lookup('postgresql::apt_config::pgdg::keyid')
   $pgdg_key = lookup('postgresql::apt_config::pgdg::key')
-  $pglogical_mirror = lookup('postgresql::apt_config::pglogical::mirror')
-  $pglogical_keyid = lookup('postgresql::apt_config::pglogical::keyid')
-  $pglogical_key = lookup('postgresql::apt_config::pglogical::key')
 
   ::apt::source {'pgdg':
     location => $pgdg_mirror,
@@ -18,12 +15,6 @@ class profile::postgresql::apt_config {
   }
 
   ::apt::source {'pglogical':
-    location => $pglogical_mirror,
-    release  => "${::lsbdistcodename}-2ndquadrant",
-    repos    => 'main',
-    key      => {
-      id      => $pglogical_keyid,
-      content => $pglogical_key,
-    },
+    ensure => 'absent',
   }
 }
