f5d0ddfd7c75d9857cc597cbca94add73fde9e85
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-07-26 17:45:02 +0200

    swh_hostname: Deal with all swh domains
    Related T1876

--- site-modules/profile/lib/facter/swh_hostname.rb
+++ site-modules/profile/lib/facter/swh_hostname.rb
@@ -1,19 +1,28 @@
 require 'puppet'
 require 'socket'
 
-domain_name = ".internal.softwareheritage.org"
+domain_names = [
+  ".internal.softwareheritage.org"
+  ".internal.staging.swh.network"
+]
+
+default_domain_name = domain_names[0]
 
 Facter.add(:swh_hostname) do
   setcode do
     retval = {}
     retval["fqdn"] = Facter.value(:fqdn).to_s
-    if retval["fqdn"].end_with?(domain_name)
-      retval["short"] = retval["fqdn"].chomp(domain_name)
-      retval["internal_fqdn"] = retval["fqdn"]
-    else
-      retval["short"] = Socket.gethostname
-      retval["internal_fqdn"] = retval["short"] + domain_name
+    domain_names.each do |domain|
+      if retval["fqdn"].end_with?(domain)
+        retval["short"] = retval["fqdn"].chomp(domain)
+        retval["internal_fqdn"] = retval["fqdn"]
+        return retval
+      end
     end
+
+    # fallback
+    retval["short"] = Socket.gethostname
+    retval["internal_fqdn"] = retval["short"] + default_domain_name
     retval
   end
 end
