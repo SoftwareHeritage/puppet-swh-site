877b4ed3a9b940522b34f1760bb321877c138c76
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-07-30 17:50:48 +0200

    swh_hostname: Fix fqdn computation
    Prior to this commit, the fqdn computation stanza was broken. A return
    statement (~ side-effect) within a "foreach" instruction cannot work in ruby.
    
    Use functional approach (filter and select the first result if any). If a
    domain is filtered, so great we do as before, use the fqdn as `internal_fqdn`.
    Otherwise, fallback as before to the default domain (.internal.s.o as
    `internal_fqdn`).
    
    Related D1779

--- site-modules/profile/lib/facter/swh_hostname.rb
+++ site-modules/profile/lib/facter/swh_hostname.rb
@@ -12,17 +12,18 @@ Facter.add(:swh_hostname) do
   setcode do
     retval = {}
     retval["fqdn"] = Facter.value(:fqdn).to_s
-    domain_names.each do |domain|
-      if retval["fqdn"].end_with?(domain)
-        retval["short"] = retval["fqdn"].chomp(domain)
-        retval["internal_fqdn"] = retval["fqdn"]
-        return retval
-      end
-    end
 
-    # fallback
-    retval["short"] = Socket.gethostname
-    retval["internal_fqdn"] = retval["short"] + default_domain_name
+    filtered_domain = domain_names.select { |domain_name|
+      retval["fqdn"].end_with?(domain_name)
+    }.first
+
+    if filtered_domain
+      retval["short"] = retval["fqdn"].chomp(filtered_domain)
+      retval["internal_fqdn"] = retval["fqdn"]
+    else
+      retval["short"] = Socket.gethostname
+      retval["internal_fqdn"] = retval["short"] + default_domain_name
+    end
     retval
   end
 end
