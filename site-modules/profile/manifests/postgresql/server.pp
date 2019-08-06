d64b18d73f67f545cf455a5ce328be348b48f72a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-06 19:13:17 +0200

    postgresql/server: Adapt postgres setup

--- site-modules/profile/manifests/postgresql/server.pp
+++ site-modules/profile/manifests/postgresql/server.pp
@@ -7,13 +7,15 @@ class profile::postgresql::server {
   }
 
   $postgres_pass = lookup('swh::deploy::db::postgres::password')
+  $server_port = lookup('postgres::server::port')
+  $server_addresses = lookup('postgres::server::listen_addresses')
 
   class { 'postgresql::server':
-    ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
     ipv4acls                   => ['hostssl all guest 192.168.128.0/24 cert'],
     postgres_password          => $postgres_pass,
-    port                       => 5433,
+    port                       => $server_port,
+    listen_addresses           => $server_addresses,
   }
 
   $guest = 'guest'
@@ -34,6 +36,12 @@ class profile::postgresql::server {
       owner    => $db_user
     }
 
+    # guest user has read access on tables
+    postgresql::server::database_grant { $db_name:
+      privilege   => 'connect',
+      db          => $db_name,
+      role        => $guest,
+    }
     # guest user has read access on tables
     postgresql::server::table_grant { $db_name:
       privilege   => 'select',
