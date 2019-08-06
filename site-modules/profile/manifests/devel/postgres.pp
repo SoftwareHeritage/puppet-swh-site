57177ae9b4e0304ccdb7ef0ea2aedc911e9af372
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-06 17:57:28 +0200

    pg_service.conf/pgpass.conf: Generate service connection file
    Related T1234

--- site-modules/profile/manifests/devel/postgres.pp
+++ site-modules/profile/manifests/devel/postgres.pp
@@ -5,9 +5,21 @@ class profile::devel::postgres {
     ensure => present,
   }
 
+  $dbs = lookup('swh::dbs::all', Array, 'deep')
+
   file { '/etc/postgresql-common/pg_service.conf':
     ensure  => file,
     content => template('profile/postgres/pg_service.conf.erb'),
     require => Package[$packages],
   }
+
+  # Users who wants this need to install themselves
+  # ln -s /etc/postgresql-common/pgpass.conf ~/.pgpass
+  file { '/etc/postgresql-common/pgpass.conf':
+    ensure  => file,
+    mode   => '0600',
+    content => template('profile/postgres/pgpass.conf.erb'),
+    require => Package[$packages],
+  }
+
 }
