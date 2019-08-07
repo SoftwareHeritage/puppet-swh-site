596fa56ef41578f75e667517f2ed813c06f22119
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-07 11:59:36 +0200

    devel/postgres: Install the ~/.pgpass ~/.pg_service.conf locally
    As we cannot use the global one for everyone (pgpass needs to be user readonly
    accessible if we don't want warnings...)

--- site-modules/profile/manifests/devel/postgres.pp
+++ site-modules/profile/manifests/devel/postgres.pp
@@ -5,21 +5,41 @@ class profile::devel::postgres {
     ensure => present,
   }
 
-  $dbs = lookup('swh::dbs::all', Array, 'deep')
+  $dbs = lookup('swh::postgres::service::dbs', Array, 'deep')
 
+  # globally installed
   file { '/etc/postgresql-common/pg_service.conf':
     ensure  => file,
     content => template('profile/postgres/pg_service.conf.erb'),
     require => Package[$packages],
   }
 
-  # Users who wants this need to install themselves
-  # ln -s /etc/postgresql-common/pgpass.conf ~/.pgpass
   file { '/etc/postgresql-common/pgpass.conf':
     ensure  => file,
     mode   => '0600',
     content => template('profile/postgres/pgpass.conf.erb'),
     require => Package[$packages],
+  }
+
+  # Explicitly install the configuration files per user's home
+  # TL;DR the pgpass must be readonly per user so we can't use the global one
+  # FIXME: might as well remove the global one
+  $users = lookup('swh::postgres::service::users', Array, 'deep')
+  each ($users) | $user | {
+    file {"/home/${user}/.pg_service.conf":
+      ensure  => file,
+      content => template('profile/postgres/pg_service.conf.erb'),
+      user => $user,
+      group => $user,
+      mode => '0400',
+    }
+    file {"/home/${user}/.pgpass":
+      ensure  => file,
+      content => template('profile/postgres/pgpass.conf.erb'),
+      user => $user,
+      group => $user,
+      mode => '0400',
+    }
   }
 
 }
