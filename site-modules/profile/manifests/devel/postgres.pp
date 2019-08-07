7a03b05447750f346993b2cd29abefc3d12f9402
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-07 16:34:09 +0200

    devel/postgres: Remove the globally defined one
    This does not work as it must be user readable only.
    Instead generate the files for a requisite list of users.

--- site-modules/profile/manifests/devel/postgres.pp
+++ site-modules/profile/manifests/devel/postgres.pp
@@ -7,33 +7,24 @@ class profile::devel::postgres {
 
   $dbs = lookup('swh::postgres::service::dbs', Array, 'deep')
 
-  # globally installed
-  file { '/etc/postgresql-common/pg_service.conf':
-    ensure  => file,
-    content => template('profile/postgres/pg_service.conf.erb'),
-    require => Package[$packages],
-  }
-
-  file { '/etc/postgresql-common/pgpass.conf':
-    ensure  => file,
-    mode   => '0600',
-    content => template('profile/postgres/pgpass.conf.erb'),
-    require => Package[$packages],
-  }
-
   # Explicitly install the configuration files per user's home
   # TL;DR the pgpass must be readonly per user so we can't use the global one
-  # FIXME: might as well remove the global one
   $users = lookup('swh::postgres::service::users', Array, 'deep')
   each ($users) | $user | {
-    file {"/home/${user}/.pg_service.conf":
+    if $user == 'root' {
+      $home = '/root'
+    } else {
+      $home = "/home/${user}"
+    }
+
+    file {"/${home}/.pg_service.conf":
       ensure  => file,
       content => template('profile/postgres/pg_service.conf.erb'),
       user => $user,
       group => $user,
       mode => '0400',
     }
-    file {"/home/${user}/.pgpass":
+    file {"/${user}/.pgpass":
       ensure  => file,
       content => template('profile/postgres/pgpass.conf.erb'),
       user => $user,
