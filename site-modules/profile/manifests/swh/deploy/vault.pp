42d51e3a77a5f0218c2b331c775f8cc1ec6f56a0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-08 20:16:48 +0200

    vault: Build the vault_cache directory

--- site-modules/profile/manifests/swh/deploy/vault.pp
+++ site-modules/profile/manifests/swh/deploy/vault.pp
@@ -4,6 +4,14 @@ class profile::swh::deploy::vault {
   include ::profile::swh::deploy::base_vault
   Package['python3-swh.vault'] ~> Service['gunicorn-swh-vault']
 
+  $cache_directory = lookup('swh::deploy::vault::cache')
+  file {$cache_directory:
+    ensure => directory,
+    owner  => 'root',
+    group  => $group,
+    mode   => '0755',
+  }
+
   ::profile::swh::deploy::rpc_server {'vault':
     executable => 'swh.vault.api.wsgi:app',
     worker     => 'async',
