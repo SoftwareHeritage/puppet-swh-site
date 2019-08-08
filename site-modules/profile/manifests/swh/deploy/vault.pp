d8bd0a600195843e93ca69ae2afdb8f1477598a6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-08-08 20:21:56 +0200

    vault: Use vault's defined group for the conf/cache directories

--- site-modules/profile/manifests/swh/deploy/vault.pp
+++ site-modules/profile/manifests/swh/deploy/vault.pp
@@ -4,6 +4,7 @@ class profile::swh::deploy::vault {
   include ::profile::swh::deploy::base_vault
   Package['python3-swh.vault'] ~> Service['gunicorn-swh-vault']
 
+  $group = lookup('swh::deploy::vault::group')
   $cache_directory = lookup('swh::deploy::vault::cache')
   file {$cache_directory:
     ensure => directory,
