e567862b64e44f0f9908386d1160f6349968cd2a
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-10-07 17:10:34 +0200

    Disable /etc/resolv.conf modification by isc-dhcp-client

--- site-modules/profile/manifests/resolv_conf.pp
+++ site-modules/profile/manifests/resolv_conf.pp
@@ -7,4 +7,19 @@ class profile::resolv_conf {
     nameservers => $nameservers,
     searchpath  => $search_domains,
   }
+
+  file {['/etc/dhcp', '/etc/dhcp/dhclient-enter-hooks.d']:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  file {'/etc/dhcp/dhclient-enter-hooks.d/noresolvconf':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/profile/resolv_conf/dhcp-noresolvconf',
+  }
 }
