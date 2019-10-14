f02171ac60a41c94a362157d07e115d2af87eca2
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-10-14 18:51:25 +0200

    Add proxmox VE 5.x apt key

--- site-modules/profile/manifests/proxmox/apt_keys.pp
+++ site-modules/profile/manifests/proxmox/apt_keys.pp
@@ -1,12 +1,18 @@
 # Proxmox APT keys configuration
 class profile::proxmox::apt_keys {
-  $proxmox_key = lookup('proxmox::apt_config::key::6_x')
+  $proxmox_keys = {
+    '5.x' => lookup('proxmox::apt_config::key::5_x'),
+    '6.x' => lookup('proxmox::apt_config::key::6_x'),
+  }
 
-  file {'/etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.asc':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => $proxmox_key,
-  } -> Apt::Source <| tag == 'proxmox' |>
+  $proxmox_keys.each |$release, $content| {
+    file {"/etc/apt/trusted.gpg.d/proxmox-ve-release-${release}.asc":
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => $content,
+      tag     => 'proxmox',
+    } -> Apt::Source <| tag == 'proxmox' |>
+  }
 }
