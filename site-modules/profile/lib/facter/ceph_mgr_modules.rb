756549a25954e974b51cff165e086a3e32fb48df
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-10-14 18:35:14 +0200

    Make ceph_mgr_modules fact more robust

--- site-modules/profile/lib/facter/ceph_mgr_modules.rb
+++ site-modules/profile/lib/facter/ceph_mgr_modules.rb
@@ -3,7 +3,12 @@ require 'json'
 Facter.add('ceph_mgr_modules') do
   setcode do
     if File.exist?('/var/lib/ceph/mgr')
-      JSON.parse(Facter::Core::Execution.execute('ceph mgr module ls'))
+      data = Facter::Core::Execution.execute('ceph mgr module ls').strip
+      if data
+        JSON.parse(data)
+      else
+        {}
+      end
     end
   end
 end
