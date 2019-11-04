22baa347dbd669838a9e4379c81c990e3cf45dad
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-11-04 15:31:59 +0100

    Parse elasticsearch version number to generate apt source

--- site-modules/profile/manifests/elasticsearch.pp
+++ site-modules/profile/manifests/elasticsearch.pp
@@ -20,14 +20,21 @@ class profile::elasticsearch {
   $key =     lookup('elastic::apt_config::key')
   $version = lookup('elastic::elk_version')
 
-  apt::source { 'elastic-6.x':
-    location => 'https://artifacts.elastic.co/packages/6.x/apt',
+  $parsed_version = split($version, '[.]')
+  $major_version = $parsed_version[0]
+
+  apt::source { 'elasticsearch':
+    location => "https://artifacts.elastic.co/packages/${major_version}.x/apt",
     release  => 'stable',
     repos    => 'main',
     key      => {
       id      => $keyid,
       content => $key,
     },
+  }
+
+  apt::source {'elastic-6.x':
+    ensure => absent,
   }
 
   package { 'elasticsearch':
