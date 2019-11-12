a3fcd89fab9d1305c42355c92e7a6b03fa756844
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-11-12 15:40:36 +0100

    loader_debian: Add missing runtime dependency
    Added here instead of the python3-swh.loader.core runtime dependency.

--- site-modules/profile/manifests/swh/deploy/worker/loader_debian.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_debian.pp
@@ -2,10 +2,15 @@
 class profile::swh::deploy::worker::loader_debian {
   include ::profile::swh::deploy::worker::loader_package
 
+  package {'dpkg-dev':
+    ensure => 'present',
+  }
+
   ::profile::swh::deploy::worker::instance {'loader_debian':
     ensure       => present,
     require      => [
       Package[$packages],
+      Package['dpkg-dev'],
     ],
   }
 }
