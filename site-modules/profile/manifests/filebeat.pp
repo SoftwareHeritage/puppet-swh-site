a77fc921f4352922ce30f6e314fdd472126b15e2
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-11-25 14:39:01 +0100

    filebeat: Use refactored elastic.co apt config

--- site-modules/profile/manifests/filebeat.pp
+++ site-modules/profile/manifests/filebeat.pp
@@ -1,24 +1,22 @@
 # Filebeat apache log shipper profile
 
 class profile::filebeat {
+  include ::profile::elastic::apt_config
 
-  # Filebeat official package installation instructions:
-  # https://www.elastic.co/guide/en/beats/filebeat/current/setup-repositories.html
-  $keyid =  lookup('elastic::apt_config::keyid')
-  $key =    lookup('elastic::apt_config::key')
-
-  apt::source { 'elastic-6.x':
-    location => 'https://artifacts.elastic.co/packages/6.x/apt',
-    release  => 'stable',
-    repos    => 'main',
-    key      => {
-      id      => $keyid,
-      content => $key,
-    },
-  }
+  $version = lookup('elastic::elk_version')
 
   package { 'filebeat':
-    ensure => '6.3.2',
+    ensure => $version,
   }
 
+  apt::pin { 'filebeat':
+    packages => 'filebeat',
+    version  => $version,
+    priority => 1001,
+  }
+
+  service { 'filebeat':
+    ensure => running,
+    enable => true,
+  }
 }
