06bcf4c6819ebf4d8218c0321763e10ed1b4a0d6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-11-26 17:16:47 +0100

    Drop scheduler updater references
    Related to D2354
    Related to D2358

--- site-modules/role/manifests/swh_scheduler.pp
+++ site-modules/role/manifests/swh_scheduler.pp
@@ -4,6 +4,4 @@ class role::swh_scheduler inherits role::swh_server {
   # Scheduler
   include profile::rabbitmq
   include profile::swh::deploy::scheduler
-  include profile::swh::deploy::scheduler_updater_consumer
-  include profile::swh::deploy::scheduler_updater_writer
 }
