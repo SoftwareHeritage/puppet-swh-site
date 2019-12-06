c319f6c248ed174e6374541a602c9a29f80f3435
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-12-06 14:44:03 +0100

    Set sentry admin email address

--- site-modules/profile/manifests/sentry.pp
+++ site-modules/profile/manifests/sentry.pp
@@ -33,10 +33,11 @@ class profile::sentry {
   }
 
   # variables for config.yml
-  $secret_key = lookup('sentry::secret_key')
-  $vhost_name = lookup('sentry::vhost::name')
-  $mail_host  = lookup('sentry::mail::host')
-  $mail_from  = lookup('sentry::mail::from')
+  $admin_email = lookup('sentry::admin_email')
+  $secret_key  = lookup('sentry::secret_key')
+  $vhost_name  = lookup('sentry::vhost::name')
+  $mail_host   = lookup('sentry::mail::host')
+  $mail_from   = lookup('sentry::mail::from')
 
   file {$config_yml:
     ensure => present,
