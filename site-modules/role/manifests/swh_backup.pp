9477931db45555f6586fdc49fb6c5f60d3d22ad6
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-01-03 18:11:33 +0100

    Drop rsnapshot configuration

--- site-modules/role/manifests/swh_backup.pp
+++ site-modules/role/manifests/swh_backup.pp
@@ -3,5 +3,4 @@ class role::swh_backup inherits role::swh_server {
   include profile::swh::deploy::objstorage
   include profile::swh::deploy::objstorage_ceph
   include profile::megacli
-  include profile::rsnapshot::master
 }
