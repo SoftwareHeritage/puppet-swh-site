c4d9b5d8e9d8b904f304e89fd9e05af67416b79b
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-01-03 19:22:12 +0100

    Use proper syntax for exported resource collection

--- site-modules/profile/manifests/borg/repository_server.pp
+++ site-modules/profile/manifests/borg/repository_server.pp
@@ -1,4 +1,4 @@
 # Centralized borg repository server
 class profile::borg::repository_server {
-  Profile::Borg::Repository <| tag == $::swh_hostname['fqdn'] |>
+  Profile::Borg::Repository <<| tag == $::swh_hostname['internal_fqdn'] |>>
 }
