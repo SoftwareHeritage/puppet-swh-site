effa756055976f05e76d49fa28ccbc587282edfc
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-01-08 13:31:46 +0100

    Cleanup dar setup

--- site-modules/profile/manifests/dar/client.pp
+++ site-modules/profile/manifests/dar/client.pp
@@ -1,54 +1,28 @@
 class profile::dar::client {
-  include ::dar
+  $directory = lookup('backups::legacy_storage')
 
-  $backup_enable = lookup('backups::enable')
+  file {$directory:
+    ensure  => 'absent',
+    purge   => true,
+    recurse => true,
+  }
 
-  if $backup_enable {
-    $dar_remote_hostname = $::swh_hostname['short']
-    $dar_backup_name = $::hostname
+  exec {'sed -e /dar\./d -e /swh-dar-backup/d -i /var/spool/cron/crontabs/root':
+    path   => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
+    onlyif => 'grep -q swh-dar-backup /var/spool/cron/crontabs/root',
+  }
 
-    $backup_cron = profile::cron_rand(lookup('backups::cron', Hash), 'backup')
+  file {'/usr/local/bin/swh-dar-backup':
+    ensure => absent,
+  }
 
-    dar::backup { $dar_backup_name:
-      backup_storage   => lookup('backups::storage'),
-      keep_backups     => lookup('backups::num_backups'),
-      backup_base      => lookup('backups::base'),
-      backup_selection => lookup('backups::select'),
-      backup_exclusion => lookup('backups::exclude', Array, 'unique'),
-      backup_options   => lookup('backups::options'),
-      *                => $backup_cron,
-    }
+  file {'/var/log/dar':
+    ensure => absent,
+    purge   => true,
+    recurse => true,
+  }
 
-    $server_cron = profile::cron_rand(lookup('dar_server::cron', Hash), 'backup_server')
-
-    $dir_for_fetched_backups = lookup('dar_server::backup::storage')
-    $central_backup_host = lookup('dar_server::central_host')
-
-    # Export a remote backup to the backup server
-    @@dar::remote_backup { "${dar_remote_hostname}.${dar_backup_name}":
-      remote_backup_storage => lookup('backups::storage'),
-      remote_backup_host    => $dar_remote_hostname,
-      remote_backup_name    => $dar_backup_name,
-      local_backup_storage  => $dir_for_fetched_backups,
-      *                     => $server_cron,
-    }
-
-    # Export an icinga check to verify backup freshness
-    $icinga_checks_file = lookup('icinga2::exported_checks::filename')
-    $checked_directory = "${dir_for_fetched_backups}/${dar_remote_hostname}"
-
-    @@::icinga2::object::service {"backup freshness for ${dar_remote_hostname}":
-      service_name     => 'backup freshness',
-      import           => ['generic-service'],
-      host_name        => $::fqdn,
-      command_endpoint => $central_backup_host,
-      check_command    => 'check_newest_file_age',
-      check_interval   => '10h',
-      vars             => {
-        check_directory => $checked_directory,
-      },
-      target           => $icinga_checks_file,
-      tag              => 'icinga2::exported',
-    }
+  file {'/etc/logrotate.d/swh-dar':
+    ensure => absent,
   }
 }
--- site-modules/profile/manifests/dar/server.pp
+++ site-modules/profile/manifests/dar/server.pp
@@ -1,3 +1,10 @@
 class profile::dar::server {
-  include ::dar::backup_server
+  file {'/usr/local/bin/swh-dar-copy-remote-backup':
+    ensure => 'absent',
+  }
+
+  exec {'sed -e /dar_remote_backup/d -e /swh-dar-copy-remote-backup/d -i /var/spool/cron/crontabs/root':
+    path   => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
+    onlyif => 'grep -q swh-dar-copy-remote-backup /var/spool/cron/crontabs/root',
+  }
 }
