2a20635416937dd3ff7f1d87997c609dcfedcb7e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-01-09 12:21:22 +0100

    Export jenkins metrics to prometheus

--- site-modules/profile/manifests/jenkins/reverse_proxy.pp
+++ site-modules/profile/manifests/jenkins/reverse_proxy.pp
@@ -1,10 +1,21 @@
 class profile::jenkins::reverse_proxy {
+  $jenkins_vhost_name = lookup('jenkins::vhost::name')
   ::profile::reverse_proxy {'jenkins':
     default_proxy_pass_opts => {
       keywords => ['nocanon'],
     },
     extra_apache_opts       => {
       allow_encoded_slashes => 'nodecode',
+    },
+  }
+
+  profile::prometheus::export_scrape_config {"jenkins_${jenkins_vhost_name}":
+    job          => "jenkins",
+    target       => "${jenkins_vhost_name}:443",
+    scheme       => "https",
+    metrics_path => '/prometheus',
+    labels       => {
+      instance => $jenkins_vhost_name,
     },
   }
 }
