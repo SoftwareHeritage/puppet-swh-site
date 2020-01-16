a53d811eea82edf965b23a7f5b1a7bc15646ec4e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-01-16 17:09:56 +0100

    Add postgresql settings for keycloak

--- site-modules/profile/manifests/keycloak/primary.pp
+++ site-modules/profile/manifests/keycloak/primary.pp
@@ -1,5 +1,21 @@
 # Definition for the primary keycloak server
 
 class profile::keycloak::primary {
-  class {'::keycloak':}
+  $postgres_host = lookup('keycloak::postgres::host')
+  $postgres_port = lookup('keycloak::postgres::port')
+  $postgres_dbname = lookup('keycloak::postgres::dbname')
+  $postgres_user = lookup('keycloak::postgres::user')
+  $postgres_password = lookup('keycloak::postgres::password')
+
+  class {'::keycloak':
+    # Database settings
+    datasource_driver    => 'postgresql',
+    datasource_host      => $postgres_host,
+    datasource_port      => $postgres_port,
+    datasource_dbname    => $postgres_dbname,
+    datasource_username  => $postgres_user,
+    datasource_password  => $postgres_password,
+    # Don't manage the PostgreSQL database
+    manage_datasource    => false,
+  }
 }
