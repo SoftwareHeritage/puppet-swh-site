5771627e1dd010255ec9ae336afbf1d4593fe8c1
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-27 07:58:54 +0100

    workers: Rename loader_functional into loader_nixguix
    Related to D2875

---
+++ site-modules/profile/manifests/swh/deploy/worker/loader_nixguix.pp
@@ -0,0 +1,12 @@
+# Deployment for loader-nixguix
+class profile::swh::deploy::worker::loader_nixguix {
+  include ::profile::swh::deploy::worker::base_loader_archive
+
+  $private_tmp = lookup('swh::deploy::worker::loader_nixguix::private_tmp')
+
+  ::profile::swh::deploy::worker::instance {'loader_nixguix':
+    ensure       => present,
+    private_tmp  => $private_tmp,
+    sentry_name  => 'loader_core',
+  }
+}
