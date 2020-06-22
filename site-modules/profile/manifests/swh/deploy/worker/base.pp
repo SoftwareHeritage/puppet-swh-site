83ce6ce3482ef2496aa3f262acb626dce40ed45f
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-06-22 18:26:21 +0200

    Set the celery worker hostname to the internal fqdn instead of the short hostname

--- site-modules/profile/manifests/swh/deploy/worker/base.pp
+++ site-modules/profile/manifests/swh/deploy/worker/base.pp
@@ -5,13 +5,15 @@ class profile::swh::deploy::worker::base {
   $systemd_unit_name = 'swh-worker.service'
   $systemd_slice_name = 'system-swh\x2dworker.slice'
 
+  $celery_hostname = $swh_hostname['internal_fqdn']
+
   package {'python3-swh.scheduler':
     ensure => installed,
   }
 
   ::systemd::unit_file {$systemd_template_unit_name:
     ensure => 'present',
-    source => "puppet:///modules/profile/swh/deploy/worker/${systemd_template_unit_name}",
+    content => template("profile/swh/deploy/worker/${systemd_template_unit_name}.erb"),
   }
 
   ::systemd::unit_file {$systemd_unit_name:
