9fe3a5a2bafb07c0fdf84e8a34c62d653f13c7fd
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-06-22 18:19:32 +0200

    Move cleanup-workers-tmp to swh::deploy::worker::base

--- site-modules/profile/manifests/swh/deploy/worker.pp
+++ site-modules/profile/manifests/swh/deploy/worker.pp
@@ -6,11 +6,4 @@ class profile::swh::deploy::worker {
     $classname = "::profile::swh::deploy::worker::${instance}"
     include $classname
   }
-
-  profile::cron::d {'cleanup-workers-tmp':
-    command => 'find /tmp -depth -mindepth 3 -maxdepth 3 -type d -ctime +2 -exec rm -rf {} \+',
-    target  => 'swh-worker',
-    minute  => 'fqdn_rand',
-    hour    => 'fqdn_rand/2',
-  }
 }
--- site-modules/profile/manifests/swh/deploy/worker/base.pp
+++ site-modules/profile/manifests/swh/deploy/worker/base.pp
@@ -26,4 +26,11 @@ class profile::swh::deploy::worker::base {
     ensure => 'present',
     source => "puppet:///modules/profile/swh/deploy/worker/${systemd_slice_name}",
   }
+
+  profile::cron::d {'cleanup-workers-tmp':
+    command => 'find /tmp -depth -mindepth 3 -maxdepth 3 -type d -ctime +2 -exec rm -rf {} \+',
+    target  => 'swh-worker',
+    minute  => 'fqdn_rand',
+    hour    => 'fqdn_rand/2',
+  }
 }
