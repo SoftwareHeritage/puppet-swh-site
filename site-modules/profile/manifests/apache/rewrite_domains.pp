316f153bd7484db96e98a39605e918302d923de4
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-07-16 19:29:53 +0200

    Move rewrite domains to Let's Encrypt
    Related to T979

--- site-modules/profile/manifests/apache/rewrite_domains.pp
+++ site-modules/profile/manifests/apache/rewrite_domains.pp
@@ -2,8 +2,6 @@
 class profile::apache::rewrite_domains {
   include ::profile::apache::common
 
-  include ::profile::ssl
-
   $ssl_protocol = lookup('apache::ssl_protocol')
   $ssl_honorcipherorder = lookup('apache::ssl_honorcipherorder')
   $ssl_cipher = lookup('apache::ssl_cipher')
@@ -11,11 +9,10 @@ class profile::apache::rewrite_domains {
 
   $rewrite_domains = lookup('apache::rewrite_domains', Hash, 'deep')
   each($rewrite_domains) |$name, $data| {
-    $ssl_cert_name = $data['ssl_cert_name']
 
-    $ssl_cert = $::profile::ssl::certificate_paths[$ssl_cert_name]
-    $ssl_chain   = $::profile::ssl::chain_paths[$ssl_cert_name]
-    $ssl_key  = $::profile::ssl::private_key_paths[$ssl_cert_name]
+
+    ::profile::letsencrypt::certificate {$name:}
+    $cert_paths = ::profile::letsencrypt::certificate_paths($name)
 
     ::apache::vhost {"${name}_non-ssl":
       servername      => $name,
@@ -33,9 +30,9 @@ class profile::apache::rewrite_domains {
       ssl_protocol         => $ssl_protocol,
       ssl_honorcipherorder => $ssl_honorcipherorder,
       ssl_cipher           => $ssl_cipher,
-      ssl_cert             => $ssl_cert,
-      ssl_chain            => $ssl_chain,
-      ssl_key              => $ssl_key,
+      ssl_cert             => $cert_paths['cert'],
+      ssl_chain            => $cert_paths['chain'],
+      ssl_key              => $cert_paths['privkey'],
       headers              => [$hsts_header],
       docroot              => '/var/www',
       rewrites             => [
