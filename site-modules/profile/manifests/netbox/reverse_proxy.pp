07afbf78d9cd85f9cc0a5b1ea123508de3d65d48
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2020-10-02 10:02:35 +0200

    netbox: fix apache configuration

--- site-modules/profile/manifests/netbox/reverse_proxy.pp
+++ site-modules/profile/manifests/netbox/reverse_proxy.pp
@@ -1,15 +1,26 @@
 # Reverseproxy for netbox
 class profile::netbox::reverse_proxy {
+  $install_path = $::profile::netbox::install_path
+  $static_path = "${install_path}/netbox/static"
+
   ::profile::reverse_proxy {'netbox':
-    extra_proxy_pass  => [{ patch => '/static', url => '!'}],
     extra_apache_opts => {
       proxy_preserve_host => true,
       aliases             => [
-        { aliasmatch => '^/static',
-          path       => "%{lookup('profile::netbox::install_path')}",
+        { alias => '/static',
+          path  => $static_path,
         },
       ],
-    },
+      directories         => [ {
+        path       => '/static',
+        provider   => 'location',
+        proxy_pass => [ { url => '!' } ],
+      }, {
+        path     => $static_path,
+        provider => 'directory',
+        options  => ['Indexes','FollowSymLinks','MultiViews'],
+      }
+    ]    },
     icinga_check_uri  => '/login',
   }
 }
