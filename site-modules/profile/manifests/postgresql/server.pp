8590a5dc4379d75164a5ce5f7cd4037defdda040
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2020-10-27 17:48:55 +0100

    refactor the postgresql declaration to configure the main cluster instance
    All the databases are currently configured on the same cluster as the puppetlabs-postgresql module doesn't seem to allow several cluster declarations
    
    Related to 2736

--- site-modules/profile/manifests/postgresql/server.pp
+++ site-modules/profile/manifests/postgresql/server.pp
@@ -1,30 +1,41 @@
+# Install and configure a postgresql server
 class profile::postgresql::server {
-  class { 'postgresql::globals':
-    encoding            => 'UTF-8',
-    locale              => 'en_US.UTF-8',
-    manage_package_repo => true,
-    version             => '11',
-  }
+
+  $swh_base_directory = lookup('swh::base_directory')
 
   $postgres_pass = lookup('swh::deploy::db::postgres::password')
-  $server_port = lookup('postgres::server::port')
-  $server_addresses = lookup('postgres::server::listen_addresses').join(',')
+  $listen_addresses = lookup('swh::postgresql::listen_addresses').join(',')
+
   # allow access through credentials
-  $network_access = lookup('postgres::server::network_access').map | $nwk | {
+  $network_accesses = lookup('swh::postgresql::network_accesses').map | $nwk | {
       "host all all ${nwk} md5"
   }
+  $postgres_version = lookup('swh::postgresql::version')
+  $postgres_port = lookup('swh::postgresql::port')
+  $postgres_datadir = lookup('swh::postgresql::datadir')
 
-  class { 'postgresql::server':
-    ip_mask_allow_all_users    => '0.0.0.0/0',
-    ipv4acls                   => $network_access,
-    postgres_password          => $postgres_pass,
-    port                       => $server_port,
-    listen_addresses           => [$server_addresses],
+  file { [ "${swh_base_directory}/postgresql",
+      "${swh_base_directory}/postgresql/${postgres_version}" ] :
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0655',
+  }
+  -> class { 'postgresql::server':
+    ip_mask_allow_all_users => '0.0.0.0/0',
+    ipv4acls                => $network_accesses,
+    postgres_password       => $postgres_pass,
+    port                    => $postgres_port,
+    listen_addresses        => [$listen_addresses],
+    datadir                 => $postgres_datadir,
+    needs_initdb            => true, # Needed because managed_repo is false and data_dir is redefined by us ¯\_(ツ)_/¯
+    require                 => Class['profile::postgresql::apt_config']
   }
 
   $guest = 'guest'
   postgresql::server::role { $guest:
     password_hash => postgresql_password($guest, 'guest'),
+    require       => Class['postgresql::server']
   }
 
   $dbs = lookup('swh::dbs')
@@ -37,14 +48,16 @@ class profile::postgresql::server {
     postgresql::server::db { $db_name:
       user     => $db_user,
       password => $db_pass,
-      owner    => $db_user
+      owner    => $db_user,
+      require  => Class['postgresql::server']
     }
 
     # guest user has read access on tables
     postgresql::server::database_grant { $db_name:
-      privilege   => 'connect',
-      db          => $db_name,
-      role        => $guest,
+      privilege => 'connect',
+      db        => $db_name,
+      role      => $guest,
+      require   => Postgresql::Server::Db[$db_name]
     }
   }
 }
