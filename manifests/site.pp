2830795f6f7aec6168bb88327c5ea20132819965
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2020-11-18 21:27:58 +0100

    Move archive storage to a new server

--- manifests/site.pp
+++ manifests/site.pp
@@ -66,7 +66,7 @@ node /^(unibo-prod|vangogh).(euwest.azure.)?(internal.)?softwareheritage.org$/ {
   include role::swh_vault
 }
 
-node /^(uffizi|saam)\.(internal\.)?softwareheritage\.org$/ {
+node /^saam\.(internal\.)?softwareheritage\.org$/ {
   include role::swh_storage_baremetal
 }
 
