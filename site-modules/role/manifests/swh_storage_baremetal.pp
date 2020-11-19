e00340c5a9428a3456df3ec1b4cee15cfa382534
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-11-19 14:59:33 +0100

    Add mountpoints for saam

--- site-modules/role/manifests/swh_storage_baremetal.pp
+++ site-modules/role/manifests/swh_storage_baremetal.pp
@@ -2,4 +2,5 @@ class role::swh_storage_baremetal inherits role::swh_storage {
   include profile::dar::server
   include profile::megacli
   include profile::multipath
+  include profile::mountpoints
 }
