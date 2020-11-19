b68a7b0f45e12c7daef635b8211287356e0b86c6
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-11-19 13:07:25 +0100

    Delay zfs-import-cache after multipath setup

--- site-modules/profile/manifests/multipath.pp
+++ site-modules/profile/manifests/multipath.pp
@@ -9,5 +9,11 @@ class profile::multipath {
   package {$multipath_packages:
     ensure => present
   }
+
+  systemd::dropin_file {'zfs-import-cache.service.d/multipath.conf':
+    ensure   => 'present',
+    unit     => 'zfs-import-cache.service',
+    filename => 'multipath.conf',
+    content  => "# Managed by puppet (module profile::multipath)\n# Set up zfs after multipath\n[Unit]\nAfter=multipathd.socket\n",
   }
 }
