37a6bc671d490dd18f050a82025d6226a6db1cec
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-18 14:03:54 +0100

    hedgedoc: Install a more recent yarn version
    This allows the bin/setup from hedgedoc to work
    
    Related to T2827

--- site-modules/profile/manifests/hedgedoc.pp
+++ site-modules/profile/manifests/hedgedoc.pp
@@ -2,9 +2,27 @@
 class profile::hedgedoc {
 
   $packages = [
-    'npm', 'yarnpkg', 'node-gyp'
+    'npm', 'yarn', 'node-gyp'
   ]
+
+  $keyid = lookup('yarn::apt_config::keyid')
+  $key =   lookup('yarn::apt_config::key')
+
+  apt::source { 'yarn':
+    location => "https://dl.yarnpkg.com/debian/",
+    release  => 'stable',
+    repos    => 'main',
+    key      => {
+      id      => $keyid,
+      content => $key,
+    },
+  }
 
   ensure_packages ( $packages )
 
+  # packages { $packages:
+  #   ensure => present,
+  #   require => Apt::source['yarn'],
+  # }
 }
+s
