fba1ecdcf5bd5d302f5c034a4509af5951eec4ee
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-22 17:26:11 +0100

    Rename bardo fqdn appropriately
    It's an admin node.
    
    This also removes the letsencrypt certificate which is unused for the moment.
    
    Related to T2827

--- manifests/site.pp
+++ manifests/site.pp
@@ -133,7 +133,7 @@ node /^db\d\.internal\.staging\.swh\.network$/ {
   include profile::postgresql::client
 }
 
-node "bardo.softwareheritage.org" {
+node "bardo.internal.admin.swh.network" {
   include role::swh_hedgedoc
 }
 
