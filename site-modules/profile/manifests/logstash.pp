2cf48d29a4649b17bff444b659edf955d8f67d6d
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2021-01-29 16:56:13 +0100

    logstash: fix first puppet run and configuration updates
    - ensure java is installed before trying to install logstash as
      the postinstall script is failing if not
    - add missing dependencies and notification between the logstash
      package, the configuration and the service
    - Restart the service in case of notification change
    - code format
    
    Related to T2787

--- site-modules/profile/manifests/logstash.pp
+++ site-modules/profile/manifests/logstash.pp
@@ -1,13 +1,16 @@
+# Install and configure logstash
 class profile::logstash {
+  include ::java
   include ::profile::elastic::apt_config
 
-  $version = sprintf("1:%s-1", lookup('elastic::elk_version'))
+  $version = sprintf('1:%s-1', lookup('elastic::elk_version'))
 
   $elasticsearch_hosts = lookup('logstash::elasticsearch::hosts')
   $listen_address = ip_for_network(lookup('kibana::listen_network'))
 
   package { 'logstash':
-    ensure => $version,
+    ensure  => $version,
+    require => Class['java'],
   }
 
   apt::pin { 'logstash':
@@ -17,23 +20,34 @@ class profile::logstash {
   }
 
   file { '/etc/logstash/conf.d/input.conf':
-    ensure => 'file',
+    ensure  => 'file',
     content => template('profile/logstash/input.conf.erb'),
+    require => Package['logstash'],
+    notify  => Service['logstash'],
   }
 
   file { '/etc/logstash/conf.d/output.conf':
-    ensure => 'file',
+    ensure  => 'file',
     content => template('profile/logstash/output.conf.erb'),
+    require => Package['logstash'],
+    notify  => Service['logstash'],
   }
 
   file { '/etc/logstash/conf.d/filter.conf':
-    ensure => 'file',
+    ensure  => 'file',
     content => template('profile/logstash/filter.conf.erb'),
+    require => Package['logstash'],
+    notify  => Service['logstash'],
   }
 
   service { 'logstash':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['logstash'],
+      File['/etc/logstash/conf.d/input.conf'],
+      File['/etc/logstash/conf.d/output.conf'],
+      File['/etc/logstash/conf.d/filter.conf']
+    ],
   }
 
 }
