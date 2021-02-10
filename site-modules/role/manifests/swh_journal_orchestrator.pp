66a84f07fca1388a0aa0c528c726f9657c8db3ef
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-02-10 17:03:23 +0100

    Move indexer_journal_client to scheduler instances
    No need for those journal client to be on kafka nodes.
    
    In effect, this moves the service from:
    - prod: getty -> saatchi
    - staging: journal0 -> scheduler0

--- site-modules/role/manifests/swh_journal_orchestrator.pp
+++ site-modules/role/manifests/swh_journal_orchestrator.pp
@@ -1,5 +1,4 @@
 class role::swh_journal_orchestrator inherits role::swh_base {
   include profile::kafka
   include profile::kafka::prometheus_consumer_group_exporter
-  include profile::swh::deploy::indexer_journal_client
 }
--- site-modules/role/manifests/swh_scheduler_with_journal_client.pp
+++ site-modules/role/manifests/swh_scheduler_with_journal_client.pp
@@ -1,4 +1,5 @@
 # Install the swh-scheduler stack + the scheduler's journal client
 class role::swh_scheduler_with_journal_client inherits role::swh_scheduler {
   include profile::swh::deploy::scheduler::journal_client
+  include profile::swh::deploy::indexer_journal_client
 }
