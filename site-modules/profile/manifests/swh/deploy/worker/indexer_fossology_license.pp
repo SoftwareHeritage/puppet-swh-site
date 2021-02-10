0b625c4effa77aaadb784aef38719ceb21677249
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-02-10 13:46:43 +0100

    indexer-fossology-license: Fix missing source to install deps
    Now that fossology-nomossa got ported for buster, it should be clear we expect the swh
    repository as source to install that dpendency.
    
    Related to T3039

--- site-modules/profile/manifests/swh/deploy/worker/indexer_fossology_license.pp
+++ site-modules/profile/manifests/swh/deploy/worker/indexer_fossology_license.pp
@@ -3,8 +3,10 @@
 class profile::swh::deploy::worker::indexer_fossology_license {
   include ::profile::swh::deploy::indexer
   $packages = ['fossology-nomossa']
+
   package {$packages:
-    ensure => 'present',
+    ensure  => 'present',
+    require => Apt::Source['softwareheritage'],
   }
 
   Package[$::profile::swh::deploy::base_indexer::packages] ~> ::profile::swh::deploy::worker::instance {'indexer_fossology_license':
