6f18a7797f1c950dd2cf1cbf307baecf75f8f5db
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2021-02-11 11:11:33 +0100

    webapp: use swh-search as main search engine
    - Deploy and configure a swh-search environment on moma
    - Explicit the elasticsearch configurations for the
    scheduler/storage and the webapps searches as they are not stored
    on the same clusters
    - Simplify the overrides between staging/moma/webapp1
    
    Related to T2182

--- site-modules/role/manifests/swh_rp_webapps.pp
+++ site-modules/role/manifests/swh_rp_webapps.pp
@@ -2,4 +2,5 @@ class role::swh_rp_webapps inherits role::swh_rp_webapp {
   # Extra deposit and storage services
   include profile::swh::deploy::deposit
   include profile::swh::deploy::storage
+  include profile::swh::deploy::search
 }
