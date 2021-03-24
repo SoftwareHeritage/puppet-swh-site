385a9100ff2a02d74203aafaba55106ec44afb2a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-03-24 09:47:40 +0100

    Deploy memcached on deposit instance
    Related to T3166

--- site-modules/role/manifests/swh_deposit.pp
+++ site-modules/role/manifests/swh_deposit.pp
@@ -1,5 +1,6 @@
 # Deposit api without reverse proxy
 class role::swh_deposit inherits role::swh_server {
+  include profile::memcached
   # Web UI
   include profile::swh::deploy::deposit
   # Apache logs
