c6ec3347ba9ce8727b5a32f814c3d56847ce1d14
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-04-16 19:01:13 +0200

    Make the new end to end checks collected
    Related to T2117

--- site-modules/profile/manifests/swh/deploy/webapp/icinga_checks.pp
+++ site-modules/profile/manifests/swh/deploy/webapp/icinga_checks.pp
@@ -108,6 +108,7 @@ class profile::swh::deploy::webapp::icinga_checks {
       origin_url    => $entry['origin'],
       origin_type   => $entry['type'],
       environment   => $environment,
+      tag           => 'icinga2::exported',
     }
   }
 }
