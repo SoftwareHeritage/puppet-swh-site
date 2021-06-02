1823f83e4ea49db35809f542cf4ccd9a6f44d385
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-06-02 17:31:06 +0200

    Dedicate a loader_oneshot service for temporary use
    This will be used for the first sourceforge ingestion for example.
    
    Related to T3350

---
+++ site-modules/profile/manifests/swh/deploy/worker/loader_oneshot.pp
@@ -0,0 +1,14 @@
+# Deployment for oneshot loader
+class profile::swh::deploy::worker::loader_oneshot {
+  include ::profile::swh::deploy::base_loader_git
+  include ::profile::swh::deploy::base_loader_mercurial
+  include ::profile::swh::deploy::base_loader_svn
+
+  $packages = $::profile::swh::deploy::base_loader_git::packages + $::profile::swh::deploy::base_loader_mercurial::packages + $::profile::swh::deploy::base_loader_svn::packages
+
+  ::profile::swh::deploy::worker::instance {'loader_oneshot':
+    ensure  => present,
+    require => Package[$packages],
+  }
+
+}
