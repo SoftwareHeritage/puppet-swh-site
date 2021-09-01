9d72822b1fc6e01f572469098547397d045041ef
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-09-01 16:48:00 +0200

    Enable vault cookers to access swh-graph if configuration requires it
    If the 'graph' key is detected in the configuration, this installs the extra dependency
    for the graph.
    
    Related to T3518

--- site-modules/profile/manifests/swh/deploy/worker/vault_cooker.pp
+++ site-modules/profile/manifests/swh/deploy/worker/vault_cooker.pp
@@ -1,13 +1,25 @@
 # Deployment of a vault cooker
-
 class profile::swh::deploy::worker::vault_cooker {
   include ::profile::swh::deploy::base_vault
 
-  ::profile::swh::deploy::worker::instance {'vault_cooker':
+  $instance_name = 'vault_cooker'
+
+  $config = lookup("swh::deploy::worker::${instance_name}::config", Hash, 'deep')
+  if $config['graph'] {
+      $extra_packages = [
+        "python3-swh.graph.client",
+      ]
+      package {$extra_packages:
+        ensure => 'present',
+      }
+  }
+
+  ::profile::swh::deploy::worker::instance {$instance_name:
     ensure       => present,
     sentry_name  => 'vault',
     require      => [
-      Package[$packages],
+      Package[$extra_packages],
+      Package[$::profile::swh::deploy::base_vault::packages],
     ],
   }
 }
