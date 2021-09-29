a50d9f0a9e3d423ddb292fc1ea3e0e809cd76630
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-09-29 10:15:52 +0200

    loader_opam: Improve manage opam shared state services
    ExecStart does not support somewhat convoluted bash command, so wrap those in a
    dedicated script installed in /usr/local/bin/ instead.
    
    This clarifies the reading of the manifest and push the update logic within the
    boundaries of the script in question.
    
    Related to T3590

--- site-modules/profile/manifests/swh/deploy/worker/loader_opam.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_opam.pp
@@ -22,6 +22,7 @@ class profile::swh::deploy::worker::loader_opam {
   }
 
   $opam_instances = lookup('swh::deploy::worker::opam::instances')
+  $template_path = "profile/swh/deploy/loader_opam"
 
   $opam_root = lookup('swh::deploy::worker::opam::root_directory')
   file {$opam_root:
@@ -32,41 +33,35 @@ class profile::swh::deploy::worker::loader_opam {
     mode   => '0644',
   }
 
-  $binary_cmd = "/usr/bin/opam"
+  $opam_manage_shared_state = "opam-manage-shared-state"
+  $opam_manage_state_script = "/usr/local/bin/${opam_manage_shared_state}.sh"
+  file {$opam_manage_state_script:
+    ensure => 'file',
+    owner  => $user,
+    group  => $group,
+    mode   => '0755',
+    content => template("${template_path}/${opam_manage_shared_state}.sh.erb"),
+  }
 
   each ( $opam_instances ) | $instance, $instance_url | {
-    $opam_manage_shared_state = "opam-manage-shared-state"
     $opam_manage_service_name = "${$opam_manage_shared_state}-${instance}"
     $opam_manage_shared_state_timer_name = "${opam_manage_service_name}.timer"
 
-    # update the instance repository if present
-    $update_command = "${binary_cmd} repo --all --root ${opam_root} | grep ${instance_url} && ${binary_cmd} update --root ${opam_root}"
-    # common suffix command for the opam commands
-    $command_suffix = "--root ${opam_root} ${instance} ${instance_url}"
-    if $instance == "opam" {
-      # Install the default rootdir for the main opam instance
-      $install_command = "${binary_cmd} init --reinit --bare --no-setup ${command_suffix}"
-    } else {
-      # Other instances will just be added to the main opam root directory
-      $install_command = "${binary_cmd} repository add ${command_suffix}"
-    }
-
-    # Either update the instance if present or install/add that new instance
-    $opam_command = "( ${update_command} ) || ${install_command}"
-
-    # Template uses variables
+    # Templates uses variables
     #  - $user
     #  - $group
     #  - $opam_root
     #  - $opam_manage_service_name
     #  - $command
+
     ::systemd::timer { $opam_manage_shared_state_timer_name:
-      timer_content    => template("profile/swh/deploy/loader_opam/${opam_manage_shared_state}.timer.erb"),
-      service_content  => template("profile/swh/deploy/loader_opam/${opam_manage_shared_state}.service.erb"),
+      timer_content    => template("${template_path}/${opam_manage_shared_state}.timer.erb"),
+      service_content  => template("${template_path}/${opam_manage_shared_state}.service.erb"),
       enable           => true,
       require          => [
         Package[$packages],
         File[$opam_root],
+        File[$opam_manage_state_script],
       ],
     }
   }
