9acf27203f768b17b068854a6a742b078e30d4ad
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-10-15 10:14:41 +0200

    Activate sentry for counter journal client
    Related to T3662

--- site-modules/profile/manifests/swh/deploy/counters/journal_client.pp
+++ site-modules/profile/manifests/swh/deploy/counters/journal_client.pp
@@ -11,6 +11,9 @@ class profile::swh::deploy::counters::journal_client {
 
   $service_name = 'swh-counters-journal-client'
   $unit_name = "${service_name}.service"
+  $sentry_dsn = lookup("swh::deploy::counters::sentry_dsn", Optional[String], 'first', undef)
+  $sentry_environment = lookup("swh::deploy::counters::sentry_environment", Optional[String], 'first', undef)
+  $sentry_swh_package = lookup("swh::deploy::counters::sentry_swh_package", Optional[String], 'first', undef)
 
   file {$config_file:
     ensure  => present,
@@ -24,7 +27,9 @@ class profile::swh::deploy::counters::journal_client {
   # Template uses variables
   #  - $user
   #  - $group
-  #
+  #  - $sentry_dsn
+  #  - $sentry_environment
+  #  - $sentry_swh_package
   ::systemd::unit_file {$unit_name:
     ensure  => present,
     content => template("profile/swh/deploy/journal/${unit_name}.erb"),
