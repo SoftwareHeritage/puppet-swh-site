7a87a7a09bc5f5227acb5e3dceb7b338fcf039cc
Nicolas Dandrimont <nicolas@dandrimont.eu>
2021-11-18 12:16:40 +0100

    Stop freezing elasticsearch indexes.
    Elasticsearch 7.14 has deprecated index freezing, thanks to improvements
    in heap memory usage[1]. So we can stop freezing indexes and just close
    them, which should also reduce issues with logstash on restarting
    servers.
    
    [1] https://www.elastic.co/blog/significantly-decrease-your-elasticsearch-heap-memory-usage

--- site-modules/profile/files/elasticsearch/elasticsearch_close_index.py
+++ site-modules/profile/files/elasticsearch/elasticsearch_close_index.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
 
-import click
 import datetime
+
+import click
 import iso8601
 
 import elasticsearch
+
 
 @click.command()
 @click.option('--host', '-h', 'hosts',
               multiple=True,
               help="Elasticsearch node instances")
 @click.option('--timeout', '-t', default=1200)
-@click.option('--freeze-after-days', '-f', default=7)
 @click.option('--close-after-days', '-c', default=30)
-def main(hosts, timeout, freeze_after_days, close_after_days):
-    """Janitor script to manage (freeze or close) indices when respective date threshold are
+def main(hosts, timeout, close_after_days):
+    """Janitor script to close indices when date thresholds are
        exceeded.
 
     """
     today = datetime.date.today()
-    days = lambda n: datetime.timedelta(days=n)
+    close_after_days = datetime.timedelta(days=close_after_days)
 
     if not hosts:
         raise ValueError("Provide a list of elasticsearch nodes")
 
     es = elasticsearch.Elasticsearch(hosts=hosts, timeout=timeout)
 
-    for l in sorted(es.cat.indices(h='i,sth,status').splitlines()):
-        i, throttled, status = l.split()
-        throttled = throttled == 'true'
-        if throttled and status != 'open':
-            continue
+    for line in sorted(es.cat.indices(h='i,status').splitlines()):
+        i, status = line.split()
         # ignore dot-prefixed indexes (e.g. kibana settings)
         if i.startswith('.'):
             continue
@@ -39,15 +37,8 @@ def main(hosts, timeout, freeze_after_days, close_after_days):
             continue
         date = date.replace('.', '-')
         date = iso8601.parse_date(date).date()
-        info = es.indices.get(i)[i]
-        shards = int(info['settings']['index']['number_of_shards'])
 
-        if not throttled and date < today - days(freeze_after_days):
-            print('freezing', i)
-            es.indices.freeze(i, wait_for_active_shards=shards)
-            status = 'open'
-
-        if status == 'open' and date < today - days(close_after_days):
+        if status == 'open' and date < today - close_after_days:
             print('closing', i)
             es.indices.close(i)
 
