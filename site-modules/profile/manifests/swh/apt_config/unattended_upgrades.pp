c55b7755aef491c9d77d4b15a945bf65c5718525
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-11-24 11:43:32 +0100

    Fix typo in unattended upgrades variable
    Related to T3749

--- site-modules/profile/manifests/swh/apt_config/unattended_upgrades.pp
+++ site-modules/profile/manifests/swh/apt_config/unattended_upgrades.pp
@@ -1,6 +1,6 @@
 # Unattended upgrades configuration
 class profile::swh::apt_config::unattended_upgrades {
-  $origins = lookup('swh::apt_config::unattended_upgraes::origins')
+  $origins = lookup('swh::apt_config::unattended_upgrades::origins')
 
   class {'::unattended_upgrades':
     mail    => {
