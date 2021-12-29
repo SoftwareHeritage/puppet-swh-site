8ac5e537bbe825d6c542bf6a44305b7f69263e37
Nicolas Dandrimont <nicolas@dandrimont.eu>
2021-12-29 10:13:21 +0100

    docker_compose: use the docker::compose class instead of the package
    The Debian version of docker-compose is too old for recent versions of
    sentry, so we need to download it from elsewhere. Use the
    puppetlabs-docker module to do so.

--- site-modules/profile/manifests/docker_compose.pp
+++ site-modules/profile/manifests/docker_compose.pp
@@ -1,6 +1,12 @@
 # Install docker-compose
 class profile::docker_compose {
   package {'docker-compose':
-    ensure => present,
+    ensure => absent,
+  }
+
+  class {'docker::compose':
+    ensure      => 'present',
+    version     => lookup('docker::compose::version'),
+    curl_ensure => false,
   }
 }
