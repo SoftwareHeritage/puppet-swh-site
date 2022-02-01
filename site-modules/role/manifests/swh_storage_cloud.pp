14a9832c5a830a9d55250c16f1fde4c880e09d3e
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-02-01 12:16:57 +0100

    storage01.euwest.azure: Only keep the gunicorn-swh-storage service
    The other services installed are unused. It still requires the objstorage-cloud profile
    though since that storage services uses the multiplexer objstorage (which includes the
    azure blobstorage access).
    
    Related to T3901

--- site-modules/role/manifests/swh_storage_cloud.pp
+++ site-modules/role/manifests/swh_storage_cloud.pp
@@ -1,3 +1,4 @@
-class role::swh_storage_cloud inherits role::swh_base_storage {
+class role::swh_storage_cloud inherits role::swh_base {
+  include profile::swh::deploy::storage
   include ::profile::swh::deploy::objstorage_cloud
 }
