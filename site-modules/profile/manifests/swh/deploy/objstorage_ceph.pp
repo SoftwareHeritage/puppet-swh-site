4c78439e31d33c27721c1b8a6c59d44a7a201936
Nicolas Dandrimont <nicolas@dandrimont.eu>
2022-03-04 18:17:27 +0100

    Neutralize objstorage_ceph profile

--- site-modules/profile/manifests/swh/deploy/objstorage_ceph.pp
+++ site-modules/profile/manifests/swh/deploy/objstorage_ceph.pp
@@ -1,21 +1,8 @@
 # Deployment of the ceph objstorage
 
 class profile::swh::deploy::objstorage_ceph {
-  include profile::ceph::base
-
-  $objstorage_packages = ['python3-swh.objstorage.rados']
-
-  package {$objstorage_packages:
-    ensure => installed,
-  }
-
-  $objstorage_config = lookup('swh::deploy::objstorage::ceph::config')
 
   file {"${profile::swh::deploy::objstorage::conf_directory}/ceph.yml":
-    ensure  => present,
-    owner   => 'root',
-    group   => $profile::swh::deploy::objstorage::group,
-    mode    => '0640',
-    content => inline_yaml($objstorage_config),
+    ensure  => absent,
   }
 }
