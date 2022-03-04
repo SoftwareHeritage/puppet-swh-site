e49f01366b802880bdbbb37cde19e21e45a71078
Nicolas Dandrimont <nicolas@dandrimont.eu>
2022-03-04 19:33:45 +0100

    Add explicit /srv/storage/space/swh-deposit rw mountpoint to moma

--- site-modules/role/manifests/swh_rp_webapps.pp
+++ site-modules/role/manifests/swh_rp_webapps.pp
@@ -7,4 +7,7 @@ class role::swh_rp_webapps inherits role::swh_rp_webapp {
   include profile::swh::deploy::deposit
   include profile::swh::deploy::objstorage_cloud
   include profile::swh::deploy::objstorage
+
+  # needed for the deposit backend
+  include profile::mountpoints
 }
