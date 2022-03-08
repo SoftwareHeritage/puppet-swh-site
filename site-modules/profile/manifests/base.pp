98be7078a2c353a02157966a5f555ca32e2d66c4
Nicolas Dandrimont <nicolas@dandrimont.eu>
2022-03-08 17:27:49 +0100

    Split smtp configuration into separate module

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -4,36 +4,7 @@ class profile::base {
     servers => lookup('ntp::servers'),
   }
 
-  $relay_destinations = lookup('smtp::relay_destinations', Array, 'unique').reduce({}) |$ret, $value| {
-    $ret + {$value['destination'] => $value['route']}
-  }
-
-  $virtual_aliases = lookup('smtp::virtual_aliases', Array, 'unique').reduce({}) |$ret, $value| {
-    $ret + {$value['destination'] => $value['alias']}
-  }
-
-  class { '::postfix':
-    relayhost          => lookup('smtp::relayhost'),
-    mydestination      => lookup('smtp::mydestination', Array, 'unique'),
-    mynetworks         => lookup('smtp::mynetworks', Array, 'unique'),
-    relay_destinations => $relay_destinations,
-    virtual_aliases    => $virtual_aliases,
-  }
-
-  exec {'newaliases':
-    path        => ['/usr/bin', '/usr/sbin'],
-    refreshonly => true,
-    require     => Package['postfix'],
-  }
-
-  $mail_aliases = lookup('smtp::mail_aliases', Array, 'unique')
-  each($mail_aliases) |$alias| {
-    mailalias {$alias['user']:
-      ensure    => present,
-      recipient => $alias['aliases'],
-      notify    => Exec['newaliases'],
-    }
-  }
+  include profile::smtp
 
   class { '::locales':
     default_locale => lookup('locales::default_locale'),
