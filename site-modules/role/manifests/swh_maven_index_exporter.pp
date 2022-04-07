0d248255f01e3b279aae5b645e1b0fb027f31853
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-04-07 18:08:20 +0200

    staging: Deploy apache service to serve extracted export.fld files
    One per maven repository listed.
    
    Related to T3746

--- site-modules/role/manifests/swh_maven_index_exporter.pp
+++ site-modules/role/manifests/swh_maven_index_exporter.pp
@@ -1,5 +1,6 @@
 # SWH maven index exporter service
 class role::swh_maven_index_exporter inherits role::swh_base {
   include profile::docker
+  include profile::apache::simple_server
   include profile::maven_index_exporter
 }
