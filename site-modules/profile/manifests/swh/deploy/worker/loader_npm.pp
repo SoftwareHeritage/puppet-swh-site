4dfc96eb381b20ae815a4a73cd91fcfaa6a0f6f2
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-04-08 17:18:08 +0200

    loader_{npm,pypi}: Clean up definitions
    They used to have dedicated package, it's no longer the case. I'll clean up manually
    those instead on round-tripping commits in swh-site.

--- site-modules/profile/manifests/swh/deploy/worker/loader_npm.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_npm.pp
@@ -1,19 +1,11 @@
 # Deployment for swh-loader-npm
 class profile::swh::deploy::worker::loader_npm {
+  include ::profile::swh::deploy::worker::loader_package
   $private_tmp = lookup('swh::deploy::worker::loader_npm::private_tmp')
-
-  $packages = ['python3-swh.loader.npm']
-
-  package {$packages:
-    ensure => 'present',
-  }
 
   ::profile::swh::deploy::worker::instance {'loader_npm':
     ensure      => present,
     private_tmp => $private_tmp,
     sentry_name => 'loader_core',
-    require     => [
-      Package[$packages],
-    ],
   }
 }
--- site-modules/profile/manifests/swh/deploy/worker/loader_pypi.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_pypi.pp
@@ -1,19 +1,11 @@
 # Deployment for swh-loader-pypi
 class profile::swh::deploy::worker::loader_pypi {
+  include ::profile::swh::deploy::worker::loader_package
   $private_tmp = lookup('swh::deploy::worker::loader_pypi::private_tmp')
-
-  $packages = ['python3-swh.loader.pypi']
-
-  package {$packages:
-    ensure => 'present',
-  }
 
   ::profile::swh::deploy::worker::instance {'loader_pypi':
     ensure      => present,
     private_tmp => $private_tmp,
     sentry_name => 'loader_core',
-    require     => [
-      Package[$packages],
-    ],
   }
 }
