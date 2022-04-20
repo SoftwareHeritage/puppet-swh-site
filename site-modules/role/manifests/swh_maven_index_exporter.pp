3701ea9d12c5b205fcc83d776dccd108fc196a2b
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-04-20 17:10:31 +0200

    Use profile::zfs::docker for maven exporter node
    That setup got done manually initially so now we can use the declared puppet version
    instead.

--- site-modules/role/manifests/swh_maven_index_exporter.pp
+++ site-modules/role/manifests/swh_maven_index_exporter.pp
@@ -1,6 +1,7 @@
 # SWH maven index exporter service
 class role::swh_maven_index_exporter inherits role::swh_base {
   include profile::docker
+  include profile::zfs::docker
   include profile::apache::simple_server
   include profile::maven_index_exporter
 }
