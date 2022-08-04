aa64d3ee19d2ee4e9134f247b8f9856126849a1b
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-08-04 12:14:32 +0200

    scrubber: Make service parametric on the db instance to scrub
    This allows to scrub multiple db instances.
    For production environment, this means:
    - primary db (belvedere)
    - secondary db (somerset)

--- site-modules/profile/manifests/swh/deploy/scrubber/checker/postgres.pp
+++ site-modules/profile/manifests/swh/deploy/scrubber/checker/postgres.pp
@@ -6,8 +6,6 @@ class profile::swh::deploy::scrubber::checker::postgres {
   $sentry_swh_package = lookup("swh::deploy::scrubber::sentry_swh_package")
 
   $config_dir = lookup('swh::deploy::scrubber::checker::postgres::conf_directory')
-  $config_file = lookup('swh::deploy::scrubber::checker::postgres::conf_file')
-  $config_dict = lookup('swh::deploy::scrubber::checker::postgres::config')
   $user = lookup('swh::deploy::scrubber::checker::postgres::user')
   $group = lookup('swh::deploy::scrubber::checker::postgres::group')
 
@@ -21,15 +19,9 @@ class profile::swh::deploy::scrubber::checker::postgres {
     ensure  => 'directory',
     owner   => $user,
     group   => $group,
-  }
-
-  file {$config_file:
-    ensure  => present,
-    owner   => $user,
-    group   => $group,
-    mode    => '0640',
-    content => inline_template("<%= @config_dict.to_yaml %>\n"),
-    require => File[$config_dir]
+    purge   => true,
+    force   => true,
+    recurse => true,
   }
 
   $systemd_slice_name = "swh-scrubber.slice"
@@ -46,7 +38,6 @@ class profile::swh::deploy::scrubber::checker::postgres {
   # - $sentry_dsn
   # - $sentry_environment
   # - $sentry_swh_package
-  # - $config_file
   ::systemd::unit_file {$template_unit_name:
     ensure  => present,
     content => template("profile/swh/deploy/scrubber/${template_unit_name}.erb"),
@@ -58,32 +49,68 @@ class profile::swh::deploy::scrubber::checker::postgres {
     ]
   }
 
+  $config_per_dbs_to_scrub = lookup('swh::deploy::scrubber::checker::postgres::config_per_db')
+
+  # As many services as there are dbs to scrub
+  $config_per_dbs_to_scrub.each | $db_name, $cfg | {
+    $config_file = "${config_dir}/${db_name}.yaml"
+    $config_dict = $cfg['config']
+    file {$config_file:
+      ensure  => present,
+      owner   => $user,
+      group   => $group,
+      mode    => '0640',
+      content => inline_yaml($config_dict),
+      require => File[$config_dir]
+    }
+
+    $object_types.each | $object_type | {
+      $ranges.each | $range_index, $range | {
+        $ranges_list = $range.split(':')
+        $start_object = $ranges_list[0]
+        $end_object = $ranges_list[1]
+        $service_name = "${template_name}@${db_name}-${object_type}-${range_index}.service"
+
+        $parameters_conf_name = "${service_name}.d/parameters.conf"
+        # Template uses:
+        # - $object_type
+        # - $start_object
+        # - $end_object
+        # - $config_file
+        ::systemd::dropin_file {$parameters_conf_name:
+          ensure   => present,
+          unit     => $service_name,
+          filename => 'parameters.conf',
+          content  => template("profile/swh/deploy/scrubber/parameters.conf.erb"),
+        }
+
+        service {$service_name:
+          ensure  => running,
+          enable  => true,
+          require => [
+            ::Systemd::Unit_file[$template_unit_name],
+            ::Systemd::Dropin_File[$parameters_conf_name],
+          ],
+        }
+      }
+    }
+  }
+
+  # clean up old resources
   $object_types.each | $object_type | {
     $ranges.each | $range_index, $range | {
-      $ranges_list = $range.split(':')
-      $start_object = $ranges_list[0]
-      $end_object = $ranges_list[1]
-      $service_name = "${template_name}@${object_type}-${range_index}.service"
+      $old_svc_name = "${template_name}@${object_type}-${range_index}.service"
+      $old_params_confname = "${old_svc_name}.d/parameters.conf"
 
-      $parameters_conf_name = "${service_name}.d/parameters.conf"
-      # Template uses:
-      # - $object_type
-      # - $start_object
-      # - $end_object
-      ::systemd::dropin_file {$parameters_conf_name:
-        ensure   => present,
-        unit     => $service_name,
+      ::systemd::dropin_file {$old_params_confname:
+        ensure   => absent,
+        unit     => $old_svc_name,
         filename => 'parameters.conf',
-        content  => template("profile/swh/deploy/scrubber/parameters.conf.erb"),
       }
 
-      service {$service_name:
-        ensure  => running,
+      service {$old_svc_name:
+        ensure  => stopped,
         enable  => false,
-        require => [
-          ::Systemd::Unit_file[$template_unit_name],
-          ::Systemd::Dropin_File[$parameters_conf_name],
-        ],
       }
     }
   }
