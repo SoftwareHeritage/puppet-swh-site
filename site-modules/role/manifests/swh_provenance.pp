506929c1bb6d67023de452e80c5a06f884c1e0f2
Nicolas Dandrimont <nicolas@dandrimont.eu>
2022-12-20 17:39:41 +0100

    Drop postgresql server profiles from swh_provenance
    We have multiple postgresql server instances on this server now, and our
    puppet setup is very far from being able to handle that.

--- site-modules/role/manifests/swh_provenance.pp
+++ site-modules/role/manifests/swh_provenance.pp
@@ -4,12 +4,6 @@ class role::swh_provenance inherits role::swh_server {
   include profile::rabbitmq
 
   include profile::postgresql
-  include profile::postgresql::server
-  include profile::pgbouncer
   include profile::postgresql::client
   include profile::prometheus::sql
-
-  include profile::docker
 }
-
-
