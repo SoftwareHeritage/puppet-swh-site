f83085be37d09e60cca8a643da49429c4e3e23c2
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-04-03 16:10:30 +0200

    Decommission high priority service already running in elastic worker
    They are currently running in the elastic infra with the proper sentry setup to dispatch
    errors (if any).
    
    Refs. swh/meta#4949

--- site-modules/profile/manifests/swh/deploy/worker/loader_high_priority.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_high_priority.pp
@@ -1,19 +1,6 @@
 # Deployment for high priority loader
 class profile::swh::deploy::worker::loader_high_priority {
-  include ::profile::swh::deploy::base_loader_git
-  include ::profile::swh::deploy::base_loader_mercurial
-  include ::profile::swh::deploy::base_loader_svn
-
-  $packages = $::profile::swh::deploy::base_loader_git::packages + $::profile::swh::deploy::base_loader_mercurial::packages + $::profile::swh::deploy::base_loader_svn::packages
-
   ::profile::swh::deploy::worker::instance {'loader_high_priority':
-    ensure            => present,
-    send_task_events  => true,
-    require           => Package[$packages],
-    extra_config      => $::profile::swh::deploy::base_loader_git::extra_config,
-    # We need to work on the sentry scaffolding for workers with miscellaneous task types
-    # https://forge.softwareheritage.org/T4513
-    sentry_setup      => false,
+    ensure => absent,
   }
-
 }
