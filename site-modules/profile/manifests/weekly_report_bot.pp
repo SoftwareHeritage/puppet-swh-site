a6ea68cdc24629c2850ae372604d3630dadf4195
Nicolas Dandrimont <nicolas@dandrimont.eu>
2023-07-18 14:22:52 +0200

    Remove the weekly report bot

--- site-modules/profile/manifests/weekly_report_bot.pp
+++ site-modules/profile/manifests/weekly_report_bot.pp
@@ -2,21 +2,7 @@
 
 class profile::weekly_report_bot {
   $command = '/usr/local/bin/weekly-report-bot'
-
-  $weekly_report_user = lookup('weekly_report_bot::user')
-  $weekly_report_cron = lookup('weekly_report_bot::cron')
-
   file {$command:
-    ensure => present,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/profile/weekly_report_bot/weekly-report-bot',
-  }
-
-  profile::cron::d {'weekly-report-bot':
-    command => $command,
-    user    => $weekly_report_user,
-    *       => $weekly_report_cron,
+    ensure => absent,
   }
 }
