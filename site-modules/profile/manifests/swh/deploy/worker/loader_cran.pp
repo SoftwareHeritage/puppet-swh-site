0f5e52c8b33be9f4e6a8e5fa1f1e3294bd55fadd
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-09-04 17:50:45 +0200

    Drop loader cran references
    After the 'puppet agent -t' which disabled and uninstalled it. We can now drop the
    remaining references.
    
    Refs. swh/infra/sysadm-environment#5029

--- site-modules/profile/manifests/swh/deploy/worker/loader_cran.pp
+++
@@ -1,9 +0,0 @@
-# Deployment for loader-cran
-class profile::swh::deploy::worker::loader_cran {
-  include ::profile::swh::deploy::worker::base_loader_archive
-
-  ::profile::swh::deploy::worker::instance {'loader_cran':
-    sentry_name => 'loader_core',
-    ensure      => absent,
-  }
-}
