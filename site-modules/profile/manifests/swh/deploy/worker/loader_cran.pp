5057a50533a8290f7f7a78a4e8699b6f0c884c71
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-09-04 17:27:17 +0200

    Drop cran loader from static workers
    They got moved to the elastic workers.
    
    Refs. swh/infra/sysadm-environment#5029

--- site-modules/profile/manifests/swh/deploy/worker/loader_cran.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_cran.pp
@@ -2,12 +2,8 @@
 class profile::swh::deploy::worker::loader_cran {
   include ::profile::swh::deploy::worker::base_loader_archive
 
-  $private_tmp = lookup('swh::deploy::worker::loader_cran::private_tmp')
-
   ::profile::swh::deploy::worker::instance {'loader_cran':
-    ensure      => present,
-    private_tmp => $private_tmp,
     sentry_name => 'loader_core',
-    require     => Class['profile::swh::deploy::worker::base_loader_archive'],
+    ensure      => absent,
   }
 }
