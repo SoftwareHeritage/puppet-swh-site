832bbc0d347000629a472820ab8ee76aeb03713d
Guillaume Samson <guillaume.samson@softwareheritage.org>
2023-10-11 16:34:56 +0200

    docker: Define storage-driver from facts

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -1,8 +1,18 @@
 # Deploy the Docker daemon
 class profile::docker {
+  if $facts['mountpoints']['/var/lib/docker'] {
+    if $facts['mountpoints']['/var/lib/docker']['filesystem'] == 'zfs' {
+      $storage_driver = 'zfs'
+    }
+  }
+  else {
+    $storage_driver = undef
+  }
+
   class {'::docker':
-    dns        => lookup('dns::local_nameservers'),
-    log_driver => 'journald',
+    dns            => lookup('dns::local_nameservers'),
+    log_driver     => 'journald',
+    storage_driver => $storage_driver,
   }
 
   group {'docker':
