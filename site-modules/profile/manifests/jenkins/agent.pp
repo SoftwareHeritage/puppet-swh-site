021ca41b1b4a3cfd1b9406681b43ef97534e6d88
Nicolas Dandrimont <nicolas@dandrimont.eu>
2023-10-16 18:26:14 +0200

    jenkins: Move agent to modern jnlp calling convention

--- site-modules/profile/manifests/jenkins/agent.pp
+++ site-modules/profile/manifests/jenkins/agent.pp
@@ -2,9 +2,11 @@ class profile::jenkins::agent {
   include profile::jenkins::base
 
   $jenkins_agent_jar_url = lookup('jenkins::agent::jar_url')
-  $jenkins_url = lookup('jenkins::backend::url')
+  $jenkins_url = lookup('jenkins::url')
   $jenkins_agent_name = lookup('jenkins::agent::name')
   $jenkins_jnlp_token = lookup('jenkins::agent::jnlp::token')
+
+  $jnlp_url = "${jenkins_url}/computer/${jenkins_agent_name}/jenkins-agent.jnlp"
 
   $workdir = '/var/lib/jenkins/agent-workdir'
   file {$workdir:
@@ -19,16 +21,20 @@ class profile::jenkins::agent {
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
+    notify => Service['jenkins-agent'],
+  }
+
+  $jnlp_secret_file = '/usr/share/jenkins/jnlp-secret'
+  file {$jnlp_secret_file:
+    content => "${jenkins_jnlp_token}\n",
+    mode    => '0600',
+    owner   => 'jenkins',
+    group   => 'jenkins',
   }
 
   $environment_file = '/etc/default/jenkins-agent'
   file {$environment_file:
-    # Contains credentials
-    mode    => '0600',
-    owner   => 'root',
-    group   => 'root',
-    content => template('profile/jenkins/agent/jenkins-agent.defaults.erb'),
-    notify  => Service['jenkins-agent'],
+    ensure => absent,
   }
 
   ::systemd::unit_file {'jenkins-agent.service':
