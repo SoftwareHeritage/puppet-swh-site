0f2312096cc14b38fffccac6e36ea552a26068ea
Nicolas Dandrimont <nicolas@dandrimont.eu>
2023-10-18 12:00:15 +0200

    ensure Jenkins group can write to the docker socket

--- site-modules/profile/manifests/jenkins/agent/docker.pp
+++ site-modules/profile/manifests/jenkins/agent/docker.pp
@@ -4,4 +4,10 @@ class profile::jenkins::agent::docker {
   ::docker::system_user {'jenkins':
     tag => 'reload_jenkins',
   }
+
+  ::systemd::dropin_file {"docker-jenkins-access.conf":
+    ensure  => 'present',
+    unit    => 'docker.socket',
+    content => "[Socket]\nExecStartPost=setfacl -m g:jenkins:rw /run/docker.sock\n",
+  }
 }
