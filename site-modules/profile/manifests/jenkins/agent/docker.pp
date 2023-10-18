e193377c4f5e3d41757cb93468cadd22659621d4
Nicolas Dandrimont <nicolas@dandrimont.eu>
2023-10-18 12:57:06 +0200

    Create a jenkins-docker user to run jenkins jobs
    Instead of relying on the (randomly set) uid of the jenkins system user
    to match that of the container, create a user with a known high uid and
    gid, which we can map through to the docker container and hopefully use
    for jobs that need access to shared volumes.

--- site-modules/profile/manifests/jenkins/agent/docker.pp
+++ site-modules/profile/manifests/jenkins/agent/docker.pp
@@ -1,13 +1,34 @@
 class profile::jenkins::agent::docker {
   include profile::docker
 
+  $jenkins_docker_uid = lookup('jenkins::agent::docker::uid')
+  $jenkins_docker_gid = lookup('jenkins::agent::docker::gid')
+
+  group {'jenkins-docker':
+    gid    => $jenkins_docker_gid,
+    system => true,
+  }
+
+  user {'jenkins-docker':
+    uid    => $jenkins_docker_uid,
+    gid    => $jenkins_docker_gid,
+    system => true,
+  }
+
   ::docker::system_user {'jenkins':
     tag => 'reload_jenkins',
   }
 
+  $socket_override = join([
+    '[Socket]',
+    'ExecStartPost=setfacl -m g:jenkins:rw /run/docker.sock',
+    'ExecStartPost=setfacl -m g:jenkins-docker:rw /run/docker.sock',
+    '',
+  ], "\n")
+
   ::systemd::dropin_file {"docker-jenkins-access.conf":
     ensure  => 'present',
     unit    => 'docker.socket',
-    content => "[Socket]\nExecStartPost=setfacl -m g:jenkins:rw /run/docker.sock\n",
+    content => $socket_override,
   }
 }
