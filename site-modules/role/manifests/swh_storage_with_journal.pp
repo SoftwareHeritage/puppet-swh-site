556a86eb3c38e2e4d9f53991ad828c8440121b01
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-11-22 15:01:03 +0100

    storage1.staging: Adapt to be a rancher node
    Refs. swh/infra/sysadm-environment#4780
    
    Refs. swh/infra/sysadm-environment#5164

--- site-modules/role/manifests/swh_storage_with_journal.pp
+++ site-modules/role/manifests/swh_storage_with_journal.pp
@@ -1,8 +1,19 @@
-class role::swh_storage_with_journal inherits role::swh_base_storage {
+class role::swh_storage_with_journal inherits role::swh_server {
   include profile::postgresql::client
   include profile::swh::deploy::journal::backfill
 
   # journal
   include profile::zookeeper
   include profile::kafka::broker
+
+  # (temporary) storage/objstorage related
+  # this will be migrated away
+  include profile::swh::deploy::storage
+  include profile::swh::deploy::objstorage
+
+  # Make this node a rancher agent too
+  include profile::zfs::kubelet
+  include profile::zfs::rancher
+  include profile::mountpoints
+  include profile::kubernetes
 }
