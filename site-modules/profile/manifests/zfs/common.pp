7060be15523128ffc35dee9163c88e9bf11aaad9
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-11-23 13:44:59 +0100

    Allow zpool configuration to be undefined for specific nodes
    This does not impact existing nodes with the configuration already set (be it manually
    or through puppet).
    
    This will allow to configure storage1.staging as a rancher agent.
    
    Refs. swh/infra/sysadm-environment#5164

--- site-modules/profile/manifests/zfs/common.pp
+++ site-modules/profile/manifests/zfs/common.pp
@@ -1,9 +1,18 @@
 class profile::zfs::common {
-  $zpool_configuration = lookup('zfs::common::zpool_configuration')
+  # Allow zpool configuration to be undefined (that allows to bypass the manual
+  # configuration on some nodes e.g. storage1.staging)
+  $zpool_configuration = lookup('zfs::common::zpool_configuration', {default_value => {}})
 
-  # zpool create -f data /dev/vdb
-  zpool { 'data':
-    ensure => 'present',
-    *      => $zpool_configuration,
+  if $zpool_configuration {
+    # When provided, let's create the pool of data out of it
+    # zpool create -f data /dev/vdb
+    zpool { 'data':
+      ensure => 'present',
+      *      => $zpool_configuration,
+    }
+  } else {  # Otherwise, do nothing but ensure it's already configured
+    zpool { 'data':
+      ensure => 'present',
+    }
   }
 }
