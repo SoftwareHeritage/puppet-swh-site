edadb65cb235adbccf793ea40c7e2afda1243c6e
Guillaume Samson <guillaume.samson@softwareheritage.org>
2023-12-07 09:30:05 +0100

    staging/static-checks: Add webhooks journal client lag check

--- site-modules/profile/manifests/icinga2/objects/static_checks.pp
+++ site-modules/profile/manifests/icinga2/objects/static_checks.pp
@@ -202,6 +202,7 @@ class profile::icinga2::objects::static_checks {
   ['swh.scheduler.journal_client',
    'swh.search.journal_client-v0.11',
    'swh.search.journal_client.indexed-v0.11',
+   'swh-archive-stg-webhooks',
    'swh.counters.journal_client'].each |$consumer_group| {
     ::icinga2::object::service {"Kafka ${consumer_group} lag in staging":
       check_command => 'check_prometheus_metric',
