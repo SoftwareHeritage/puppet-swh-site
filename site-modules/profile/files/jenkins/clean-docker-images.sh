cb555ea8c0f101f6047fc95c062376faaeb9c89e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2023-12-11 16:47:55 +0100

    clean-docker-images: do more cleaning...
    The main functional change is guarding the docker image pull with a ||
    true to avoid the script crashing when that fails (and therefore the
    cleanup not happening)

--- site-modules/profile/files/jenkins/clean-docker-images.sh
+++ site-modules/profile/files/jenkins/clean-docker-images.sh
@@ -21,38 +21,51 @@ clean_shared_cachedir () {
   find "$dir/yarn/v6" -mindepth 1 -maxdepth 1 -type d -ctime +$threshold -exec rm -r {} \+
 
   # cleanup old Cypress versions
-  (cd $dir/Cypress; ls -1 | sort -V | head -n -1 | xargs -r rm -r)
+  (cd "$dir/Cypress"; ls -1 | sort -V | head -n -1 | xargs -r rm -r)
 
   # cleanup old pre-commit repositories
   find "$dir/pre-commit" -mindepth 1 -maxdepth 1 -type d -ctime +$threshold -exec rm -r {} \+
 }
 
-# Update tagged docker images
+prune_docker () {
+  docker system prune --filter 'label!=keep' --volumes --force
+
+  # Somehow docker system prune --volumes is missing some dangling volumes...
+  docker volume list --filter dangling=true --format json | jq -r 'select(.Labels!="keep=true")|.Name' | xargs -r docker volume rm
+}
+
+# Prune dangling layers and volumes once
+prune_docker
+
+# Update tagged docker images; do not croak if pulling fails
 docker image ls \
     | tail -n +2 \
-    | awk '{if ($2 != "<none>") { print $1":"$2 }}' \
-    | xargs -r -n1 docker image pull
+    | awk '{if ($1 !~ /^app-manager|swh\/|(swh-jenkins(-test)?\/|softwareheritage\/)/ && $2 != "<none>") { print $1":"$2 }}' \
+    | xargs -r -n1 docker image pull \
+  || true
 
 # To avoid timezone shift shenanigans (when triggered around midnight)
 today=$(date --date '13:00' +%Y%m%d)
 yesterday=$(date --date 'yesterday 13:00' +%Y%m%d)
 
-# Drop specific softwareheritage docker images (which accumulates over time)
-# except for the last 2 days. If triggered around midnight, that could drop
-# everything in the end, so let's stay safe and keep only 2 days.
-# We also keep the latest tag
+# Drop specific softwareheritage docker images (which accumulate over time)
+# except for the last 2 days, and the latest tag
 docker image ls \
-    | grep -E "(softwareheritage/(base|web|replayer)|container-registry.softwareheritage.org/swh/infra/ci-cd)" \
-    | grep -v $today \
-    | grep -v $yesterday \
-    | grep -v "latest" \
-    | awk '{if ($2 == "<none>") { print $3 } else { print $1":"$2 }}' \
+    | awk '{
+        if ($1 ~ /^(softwareheritage|container-registry.softwareheritage.org|swh-jenkins(-test)?)\// && $2 !~ '"/$today|$yesterday|latest/"') {
+                if ($2 == "<none>") {
+                        print $3
+                } else {
+                        print $1":"$2
+                }
+        }
+      }' \
     | xargs -r docker rmi
 
-docker system prune --filter 'label!=keep' --volumes --force
+# Prune more dangling layers
+prune_docker
 
 if [ -d /var/lib/docker/volumes/shared-jenkins-cachedir/_data ]; then
 	# clean up cachedir data older than 30 days
 	clean_shared_cachedir /var/lib/docker/volumes/shared-jenkins-cachedir/_data 30
 fi
-
