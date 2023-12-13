70db27a581995271e149757ee5870daa08264702
Guillaume Samson <guillaume.samson@softwareheritage.org>
2023-12-13 10:19:12 +0100

    kubernetes: Define sysctl inotify.max_user_instances to 8192

--- site-modules/profile/manifests/kubernetes.pp
+++ site-modules/profile/manifests/kubernetes.pp
@@ -1,5 +1,8 @@
 # Handle specific kubernetes setup
 class profile::kubernetes {
+  $max_map_count = lookup("kubernetes::max_map_count")
+  $inotify_max_user_instances = lookup("kubernetes::inotify_max_user_instances")
   # needed for specific workload like elasticsearch
-  sysctl { 'vm.max_map_count': value => '2097152' }
+  sysctl { 'vm.max_map_count': value => $max_map_count }
+  sysctl { 'fs.inotify.max_user_instances': value => $inotify_max_user_instances }
 }
