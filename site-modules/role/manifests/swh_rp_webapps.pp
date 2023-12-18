5c8c0535207dd27dd09dcc79e8dd672ba79ad827
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-12-18 15:12:26 +0100

    production/moma: Decommission objstorage service
    It's been migrated to the dynamic infra.
    
    Refs. swh/infra/sysadm-environment#5192

--- site-modules/role/manifests/swh_rp_webapps.pp
+++ site-modules/role/manifests/swh_rp_webapps.pp
@@ -6,7 +6,6 @@
 class role::swh_rp_webapps inherits role::swh_rp_webapp {
   include profile::swh::deploy::deposit
   include profile::swh::deploy::objstorage_cloud
-  include profile::swh::deploy::objstorage
 
   # needed for the deposit backend
   include profile::mountpoints
