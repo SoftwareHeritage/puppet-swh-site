9c1131d2d48d6efea3ceb30911e09c6a07273c79
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-01-09 11:18:51 +0100

    Decommission storage1.staging as a kafka node

--- manifests/site.pp
+++ manifests/site.pp
@@ -172,7 +172,7 @@ node 'gateway.internal.staging.swh.network' {
 }
 
 node /^storage\d\.internal\.staging\.swh\.network$/ {
-  include role::swh_rancher_agent_with_journal
+  include role::rancher_node
 }
 
 node /^worker\d\.internal\.staging\.swh\.network$/ {
