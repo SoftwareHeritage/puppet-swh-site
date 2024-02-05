f0c554b3dc1338b44bae725587847054bcd719d0
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-02-05 17:20:32 +0100

    banco: Allow inotify max user watches configuration
    It prevented banco from properly working as a rancher agent once the postgresql backup
    started. This defaults to the future default value of the debian 12 suite.
    
    Refs. swh/infra/sysadm-environment#5234

--- site-modules/profile/manifests/kubernetes.pp
+++ site-modules/profile/manifests/kubernetes.pp
@@ -2,7 +2,9 @@
 class profile::kubernetes {
   $max_map_count = lookup("kubernetes::max_map_count")
   $inotify_max_user_instances = lookup("kubernetes::inotify_max_user_instances")
-  # needed for specific workload like elasticsearch
+  $inotify_max_user_watches = lookup("kubernetes::inotify_max_user_watches")
+  # needed for specific workload (e.g. elasticsearch, rancher, ...)
   sysctl { 'vm.max_map_count': value => $max_map_count }
   sysctl { 'fs.inotify.max_user_instances': value => $inotify_max_user_instances }
+  sysctl { 'fs.inotify.max_user_watches': value => $inotify_max_user_watches }
 }
