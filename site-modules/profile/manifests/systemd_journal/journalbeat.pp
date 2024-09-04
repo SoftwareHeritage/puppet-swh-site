d3eab3e40b7304585a3a197b33e4986b50995e1e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-09-04 14:03:21 +0200

    Clean up journalbeat

--- site-modules/profile/manifests/systemd_journal/journalbeat.pp
+++ site-modules/profile/manifests/systemd_journal/journalbeat.pp
@@ -1,70 +1,33 @@
-# Journalbeat: a systemd journal collection beater for the ELK stack
+# Clean up journalbeat
 class profile::systemd_journal::journalbeat {
   $package = 'journalbeat'
   $configdir = '/etc/journalbeat'
-  $configfile = "${configdir}/journalbeat.yml"
   $service = 'journalbeat'
-  $default_elk_version = lookup('elastic::elk_version')
-  $version = lookup('elastic::beat_version', { default_value => $default_elk_version })
 
-  $logstash_hosts = lookup('systemd_journal::logstash_hosts')
-
-  include ::profile::elastic::apt_config
-
+  service {$service:
+    ensure => stopped,
+    enable => false,
+  }
   ::apt::pin {'journalbeat':
-    explanation => 'Use the elk stack version for journalbeat',
-    packages    => ['journalbeat'],
-    version     => $version,
-    priority    => 1001,
+    ensure => absent,
   }
-  -> package {$package:
-    ensure => $version,
+  package {$package:
+    ensure => absent,
   }
-  -> file {'/var/lib/journalbeat':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+  file {'/var/lib/journalbeat':
+    ensure  => absent,
+    force   => true,
+    recurse => true,
   }
 
   ::systemd::dropin_file { "${service}.conf":
-    ensure  => present,
     unit    => "${service}.service",
-    content => template('profile/systemd_journal/journalbeat/journalbeat.conf.erb'),
-  }
-  ~> service {$service:
-    ensure    => running,
-    enable    => true,
-    require   => [
-      Package[$package],
-      File[$configfile],
-      ::Systemd::Dropin_file["${service}.conf"],
-    ],
-    subscribe => [
-      Package[$package],
-      File[$configfile],
-      ::Systemd::Dropin_file["${service}.conf"],
-    ],
+    ensure  => absent,
   }
 
-  # Uses variables
-  #  - $logstash_hosts
-  #
-  file {$configfile:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('profile/systemd_journal/journalbeat/journalbeat.yml.erb'),
-    require => [Package[$package]],
-    notify  => [Service[$service]],
-  }
-
-  profile::cron::d {'logrotate-journal':
-    target  => 'logrotate-journal',
-    command => 'chronic sh -c "/usr/lib/nagios/plugins/swh/check_journal && journalctl --vacuum-time=\'7 days\'"',
-    user    => 'root',
-    minute  => 'fqdn_rand',
-    hour    => 'fqdn_rand',
+  file {$configdir:
+    ensure  => absent,
+    force   => true,
+    recurse => true,
   }
 }
