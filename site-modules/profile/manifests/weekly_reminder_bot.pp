2ac18820fcd8f39eee9107c833e36cb396aa1e65
Guillaume Samson <guillaume.samson@softwareheritage.org>
2024-10-24 16:45:06 +0200

    weekly-bot: Add hedgedoc email authentication
    Related to swh/infra/sysadm-environment#5461

--- site-modules/profile/manifests/weekly_reminder_bot.pp
+++ site-modules/profile/manifests/weekly_reminder_bot.pp
@@ -3,6 +3,7 @@ class profile::weekly_reminder_bot {
   $command = '/usr/local/bin/weekly-reminder-bot'
 
   $weekly_planning_user = lookup('weekly_reminder_bot::user')
+  $weekly_bot_password = lookup('hedgedoc::weekly-bot::password')
 
   ['weekly-planning-bot',
    'weekly-management-bot',
@@ -11,20 +12,20 @@ class profile::weekly_reminder_bot {
     $weekly_planning_cron = lookup("weekly_reminder_bot::${bot}::cron")
 
     file {$command:
-      ensure => present,
-      mode   => '0755',
-      owner  => 'root',
-      group  => 'root',
-      source => "puppet:///modules/profile/weekly_reminder_bot/${bot}",
+      ensure  => present,
+      mode    => '0750',
+      owner   => 'root',
+      group   => 'root',
+      content => template("profile/weekly_reminder_bot/${bot}.erb"),
     }
 
-    #profile::cron::d {$bot:
-    #  command => "chronic ${command}",
-    #  user    => $weekly_planning_user,
-    #  *       => $weekly_planning_cron,
-    #  require => [
-    #    File[$command],
-    #  ],
-    #}
+    profile::cron::d {$bot:
+      command => "chronic ${command}",
+      user    => $weekly_planning_user,
+      *       => $weekly_planning_cron,
+      require => [
+        File[$command],
+      ],
+    }
   }
 }
