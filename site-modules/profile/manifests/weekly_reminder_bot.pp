8472399a49dd0448016ac4b32fbd8d15367cf3ad
Guillaume Samson <guillaume.samson@softwareheritage.org>
2024-10-25 15:06:28 +0200

    weekly-bot: Deploy email reminder script and config files
    Related to swh/infra/sysadm-environment#5461

--- site-modules/profile/manifests/weekly_reminder_bot.pp
+++ site-modules/profile/manifests/weekly_reminder_bot.pp
@@ -1,31 +1,69 @@
 # A bot creating a thread for people to prepare their weekly meetings
 class profile::weekly_reminder_bot {
-  $command = '/usr/local/bin/weekly-reminder-bot'
-
-  $weekly_planning_user = lookup('weekly_reminder_bot::user')
+  $weekly_bot_user = lookup('weekly_reminder_bot::user')
   $weekly_bot_password = lookup('hedgedoc::weekly-bot::password')
+  $weekly_bot_config_dir = lookup('weekly_reminder_bot::config_dir')
+  $command = '/usr/local/bin/weekly-bot'
 
-  ['weekly-planning-bot',
-   'weekly-management-bot',
+  file { $weekly_bot_config_dir:
+    ensure => directory,
+    mode   => '0750',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${weekly_bot_config_dir}/default":
+    ensure => present,
+    mode   => '0640',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/profile/weekly_reminder_bot/default',
+    require => [
+      File[$weekly_bot_config_dir],
+    ],
+  }
+
+  file { $command:
+    ensure  => present,
+    mode    => '0750',
+    owner   => 'root',
+    group   => 'root',
+    content => template('profile/weekly_reminder_bot/weekly-bot.erb'),
+  }
+
+  ['planning',
+    'management',
   ].each |$bot| {
-    $command = "/usr/local/bin/$bot";
-    $weekly_planning_cron = lookup("weekly_reminder_bot::${bot}::cron")
+    $weekly_bot_cron = lookup("weekly_reminder_bot::${bot}::cron")
 
-    file {$command:
+    profile::cron::d { "Weekly ${bot} reminder":
+      command => "chronic ${command} ${bot}",
+      user    => $weekly_bot_user,
+      *       => $weekly_bot_cron,
+      require => [
+        File[$command,],
+      ],
+    }
+
+    file { "${weekly_bot_config_dir}/${bot}":
       ensure  => present,
       mode    => '0750',
       owner   => 'root',
       group   => 'root',
-      content => template("profile/weekly_reminder_bot/${bot}.erb"),
-    }
-
-    profile::cron::d {$bot:
-      command => "chronic ${command}",
-      user    => $weekly_planning_user,
-      *       => $weekly_planning_cron,
+      source  => "puppet:///modules/profile/weekly_reminder_bot/${bot}",
       require => [
-        File[$command],
+        File[$weekly_bot_config_dir],
       ],
+    }
+  }
+
+  ['weekly-planning-bot',
+    'weekly-management-bot',
+  ].each |$bot| {
+    $deleted = "/usr/local/bin/${bot}"
+
+    file { $deleted:
+      ensure => absent,
     }
   }
 }
