50bd5082a7f73084ca0a8e2e592c3f707961700e
Guillaume Samson <guillaume.samson@softwareheritage.org>
2024-11-20 13:48:33 +0100

    elasticsearch: Add curator cron task

--- site-modules/profile/manifests/elasticsearch/indices_curator.pp
+++ site-modules/profile/manifests/elasticsearch/indices_curator.pp
@@ -3,6 +3,9 @@ class profile::elasticsearch::indices_curator {
   $esnodes = lookup('elasticsearch::hosts')
   $indices = lookup('elasticsearch::curator::indices')
   $retention = lookup('elasticsearch::curator::retention')
+  $curator_config = lookup('elasticsearch::curator::config')
+  $curator_delete = lookup('elasticsearch::curator::delete')
+  $curator_logs = lookup('elasticsearch::curator::logs')
 
   exec { 'create curator venv':
     command => '/usr/bin/python3 -m venv /opt/curatorVenv',
@@ -16,7 +19,7 @@ class profile::elasticsearch::indices_curator {
     unless  => '/bin/sh -c "/opt/curatorVenv/bin/pip list | grep -qF elasticsearch-curator && /opt/curatorVenv/bin/pip list -o | grep -qvF elasticsearch-curator"',
   }
 
-  file { '/etc/default/curator_config.yaml':
+  file { $curator_config:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
@@ -24,11 +27,28 @@ class profile::elasticsearch::indices_curator {
     content => template('profile/elasticsearch/curator_config.erb'),
   }
 
-  file { '/etc/default/curator_action_delete_indices':
+  file { $curator_delete:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0640',
     content => template('profile/elasticsearch/curator_action_delete_indices.erb'),
+  }
+
+  file { $curator_logs:
+    ensure  => 'directory',
+    purge   => true,
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  profile::cron::d { 'elasticsearch-delete-indices':
+    target  => 'elasticsearch',
+    command => "chronic /opt/curatorVenv/bin/curator --config ${curator_config} ${curator_delete} --logfile ${curator_logs}/delete-indices-week-$(date +%W).log",
+    user    => 'root',
+    minute  => 'fqdn_rand',
+    hour    => 'fqdn_rand',
   }
 }
