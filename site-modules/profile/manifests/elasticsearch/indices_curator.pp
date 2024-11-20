cda3d07baed6471007033628d59fa3e5d3b5869b
Guillaume Samson <guillaume.samson@softwareheritage.org>
2024-11-20 12:08:55 +0100

    elasticsearch: Add curator configuration

--- site-modules/profile/manifests/elasticsearch/indices_curator.pp
+++ site-modules/profile/manifests/elasticsearch/indices_curator.pp
@@ -1,5 +1,6 @@
 class profile::elasticsearch::indices_curator {
   ensure_packages (['python3-venv'])
+  $esnodes = lookup('elasticsearch::hosts')
 
   exec { 'create curator venv':
     command => '/usr/bin/python3 -m venv /opt/curatorVenv',
@@ -11,5 +12,13 @@ class profile::elasticsearch::indices_curator {
     command => '/opt/curatorVenv/bin/pip install --upgrade elasticsearch-curator',
     # Only run the command if elasticsearch-curator is not installed, or if it's outdated
     unless  => '/bin/sh -c "/opt/curatorVenv/bin/pip list | grep -qF elasticsearch-curator && /opt/curatorVenv/bin/pip list -o | grep -qvF elasticsearch-curator"',
+  }
+
+  file { '/etc/default/curator_config.yaml':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('profile/elasticsearch/curator_config.erb'),
   }
 }
