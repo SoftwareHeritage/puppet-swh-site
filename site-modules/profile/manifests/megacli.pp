3d45abf64fe55e094b1a04a05ac57f8a7cc08b3c
Nicolas Dandrimont <nicolas@dandrimont.eu>
2025-01-22 11:01:58 +0100

    hwraid-levert has not been updated for bookworm, pin the repository to bullseye
    (Looks like megacli still works...)
    
    Closes swh/infra/sysadm-environment#5550

--- site-modules/profile/manifests/megacli.pp
+++ site-modules/profile/manifests/megacli.pp
@@ -6,9 +6,11 @@ class profile::megacli {
   $keyid =  lookup('hwraid_levert::apt_config::keyid')
   $key =    lookup('hwraid_levert::apt_config::key')
 
+  $levert_distcodename = if versioncmp($::lsbmajdistrelease, '11') >= 0 { "bullseye" } else { $::lsbdistcodename }
+
   apt::source { 'hwraid_levert':
     location => 'http://hwraid.le-vert.net/debian',
-    release  => $::lsbdistcodename,
+    release  => $levert_distcodename,
     repos    => 'main',
     key      => {
       id      => $keyid,
