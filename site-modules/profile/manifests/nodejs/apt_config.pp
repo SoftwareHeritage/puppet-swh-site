32c1105b25cb19ea802dd22e605380c43af88ae8
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-03-06 15:38:34 +0100

    Allow hedgedoc to run with compatible node/yarn versions
    Since the migration to bookworm, it comes with a node 18 version which is incompatible
    with the yarn coming from bookworm so pinning to a most recent version still compatible.
    
    Refs. swh/infra/sysadm-environment#5415

--- site-modules/profile/manifests/nodejs/apt_config.pp
+++ site-modules/profile/manifests/nodejs/apt_config.pp
@@ -4,6 +4,15 @@ class profile::nodejs::apt_config {
   $key = lookup('nodejs::apt_config::key')
   $version = lookup('nodejs::version')
 
+  if $facts['lsbdistcodename'] == 'bookworm' {
+    ::apt::pin {'nodejs':
+      explanation => 'Pin nodejs to nodejs_16 source',
+      priority    => 990,
+      version     => "16.*",
+      packages    => "nodejs",
+    }
+  }
+
   ::apt::source {'nodejs':
     location => "https://deb.nodesource.com/node_${version}",
     release  => "${::lsbdistcodename}",
