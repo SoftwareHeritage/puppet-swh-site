5c0a42d540cea8538d78224fdad9cec2341c4e5d
Guillaume Samson <guillaume.samson@softwareheritage.org>
2025-08-13 11:55:59 +0200

    filebeat: Update filebeat journald configuration
    Related to swh/infra/sysadm-environment#5687

--- site-modules/profile/manifests/systemd_journal/filebeat.pp
+++ site-modules/profile/manifests/systemd_journal/filebeat.pp
@@ -2,10 +2,18 @@
 class profile::systemd_journal::filebeat {
   include profile::filebeat
 
-  $input_configuration = [{
+  $base_configuration = [{
       'type' => 'journald',
       'id'   => 'journald',
   }]
+  $drop_event_configuration = lookup('filebeat::journald::drop_event', Hash, 'first', {})
+
+  if $drop_event_configuration == {} {
+    $input_configuration = $base_configuration
+  } else {
+    $input_configuration = $base_configuration.map |$hash| {
+    deep_merge($hash, { 'processors' => ['drop_event' => $drop_event_configuration] }) }
+  }
 
   file { "${profile::filebeat::config_directory}/inputs.d/journald.yml":
     owner   => 'root',
