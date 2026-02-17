fddac78d645b7d3aa0b0d83958c0bf5453f2b67a
Théophile Bastian <contact@tobast.fr>
2026-02-17 17:59:31 +0100

    Revert "Fix pergamon bootstrap"
    This reverts commit 85ee054d95d2e88ffea2703b1d42657a2d86abcb.
    
    This might prevent production from being applied. Reverted to
    investigate/fix tomorrow.

--- manifests/pergamon_bootstrap.pp
+++
@@ -1,7 +0,0 @@
-node 'pergamon.softwareheritage.org' {
-  include profile::static_hostnames
-  include profile::base
-  include profile::ssh::server
-  include profile::swh
-  include profile::puppet
-}
