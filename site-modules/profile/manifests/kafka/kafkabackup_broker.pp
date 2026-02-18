d985b8d6487cc54c66b9585a1d99adc2d7ee88fb
Théophile Bastian <contact@tobast.fr>
2026-02-18 14:12:32 +0100

    Kafkabackup-broker: add profile

---
+++ site-modules/profile/manifests/kafka/kafkabackup_broker.pp
@@ -0,0 +1,73 @@
+# Kafkabackup, intended to run on Kafka brokers
+
+class profile::kafka::kafkabackup_broker {
+  # Create user and group
+  $user = 'kafkabackup'
+  $group = 'kafkabackup'
+  $home = '/var/lib/kafkabackup'
+  $venv_path = "$home/venv"
+  $pip_pkg = "git+https://gitlab.softwareheritage.org/swh/infra/swh-kafkabackup"
+  $ssh_authorized_keys = lookup('swh::kafkabackup::ssh_pubkeys')
+
+  group {$group:
+    system => true,
+  }
+  -> user {$user:
+    system     => true,
+    gid        => $group,
+    groups     => ['kafka'],
+    shell      => '/usr/bin/bash',
+    home       => $home,
+  }
+  -> file { $home:
+    ensure  => 'directory',
+    mode    => "0755",
+    owner   => $user,
+    group   => $group,
+  }
+
+  # Configure ssh access
+  if($ssh_authorized_keys) {
+    file { "$home/.ssh":
+      ensure  => 'directory',
+      mode    => "0750",
+      owner   => $user,
+      group   => $group,
+      require => [User[$user], File[$home]],
+    }
+    each($ssh_authorized_keys) |$keyname, $key| {
+      ssh_authorized_key { "$user $keyname":
+        ensure  => 'present',
+        user    => $user,
+        key     => $key['key'],
+        type    => $key['type'],
+        require => File["$home/.ssh"],
+      }
+    }
+  }
+
+  # Configure virtualenv
+  ensure_packages ('python3-venv')
+
+  exec { 'kafkabackup_venv':
+    command => "python3 -m venv '$venv_path'",
+    path    => '/usr/bin',
+    creates => "$venv_path",
+    user    => "$user",
+    require => [User[$user], File[$home], Package['python3-venv']],
+  }
+  -> exec { 'kafkabackup_pip_install':
+    command => "'$venv_path/bin/pip' install '$pip_pkg'",
+    path    => '/usr/bin',
+    creates => "$venv_path/bin/kafkabackup-broker",
+    user    => "$user",
+  }
+
+  # Sudo configuration
+  ::sudo::conf { 'kafkabackup':
+    ensure   => present,
+    content  => "$user  ALL=(root) NOPASSWD: /sbin/zfs list, /sbin/zfs snapshot data/kafka@*, /sbin/zfs destroy data/kafka@*, /usr/bin/mount -t zfs -o ro data/kafka@*, /usr/bin/umount /tmp/kafkabackup-*, /usr/bin/umount -t zfs data/kafka@*",
+    priority => 50,
+  }
+}
+
--- site-modules/role/manifests/swh_kafka_broker.pp
+++ site-modules/role/manifests/swh_kafka_broker.pp
@@ -2,4 +2,5 @@ class role::swh_kafka_broker inherits role::swh_base {
   include profile::zfs::kafka
   include profile::zookeeper
   include profile::kafka::broker
+  include profile::kafka::kafkabackup_broker
 }
