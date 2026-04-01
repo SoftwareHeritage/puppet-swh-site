10ee92fbd930c30b967f833ec9c3752b6f22ae94
Théophile Bastian <contact@tobast.fr>
2026-04-01 11:56:59 +0200

    Kafkabackup: make conditionally enabled
    For now, we want to test it in staging only. This commit might be
    reverted later on when there is no need for it anymore.

--- site-modules/role/manifests/swh_kafka_broker.pp
+++ site-modules/role/manifests/swh_kafka_broker.pp
@@ -2,5 +2,8 @@ class role::swh_kafka_broker inherits role::swh_base {
   include profile::zfs::kafka
   include profile::zookeeper
   include profile::kafka::broker
-  include profile::kafka::kafkabackup_broker
+
+  if lookup("swh::kafkabackup::enable") == true {
+    include profile::kafka::kafkabackup_broker
+  }
 }
