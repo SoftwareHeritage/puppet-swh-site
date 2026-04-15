73f852480c8ad574a68a4e6d919311859585d1b9
Théophile Bastian <contact@tobast.fr>
2026-04-15 10:13:11 +0200

    Add docker on maxxi, as requested by rdicosmo
    NOTE: the `docker` zfs dataset was created by hand, as the pool there is
    named `ssd` instead of `data`.

--- site-modules/role/manifests/swh_graph_compression.pp
+++ site-modules/role/manifests/swh_graph_compression.pp
@@ -1,4 +1,10 @@
 # SWH graph compression server
 class role::swh_graph_compression inherits role::swh_base {
   include profile::zfs
+
+  # For manually run experiments on maxxi
+  # NOTE: ZFS dataset was created by hand (the pool on maxxi is named `ssd` and
+  # not `data`)
+  include profile::docker
+  include profile::docker_compose
 }
