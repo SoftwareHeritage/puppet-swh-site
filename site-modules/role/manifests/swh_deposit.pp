7122bced3cf6125a1538985de7766d0c459036fb
Nicolas Dandrimont <nicolas@dandrimont.eu>
2026-07-09 11:36:14 +0200

    Drop deployment of deposit

--- site-modules/role/manifests/swh_deposit.pp
+++
@@ -1,8 +0,0 @@
-# Deposit api without reverse proxy
-class role::swh_deposit inherits role::swh_server {
-  include profile::memcached
-  # Web UI
-  include profile::swh::deploy::deposit
-  # Apache logs
-  include profile::filebeat
-}
