32d620b069e77e52a22279a5c2f9906ab7504967
Nicolas Dandrimont <nicolas@dandrimont.eu>
2026-07-09 11:29:47 +0200

    Drop scheduler deployment manifests

--- site-modules/profile/manifests/swh/deploy/scheduler/listener.pp
+++
@@ -1,7 +0,0 @@
-# scheduler listener
-class profile::swh::deploy::scheduler::listener {
-  profile::swh::deploy::scheduler::service {"listener":
-    service_description => 'celery event listener',
-    service_args        => ["start-listener"]
-  }
-}
