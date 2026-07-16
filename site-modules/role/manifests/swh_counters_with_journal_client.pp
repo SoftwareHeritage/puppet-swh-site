43198353a50efebe73803c8768165badd1e33cf2
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:41:35 +0200

    Drop counters deployment replated configurations
    Migrated to kubernetes.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/role/manifests/swh_counters_with_journal_client.pp
+++
@@ -1,6 +0,0 @@
-# swh counters with redis backend and the counters journal client
-class role::swh_counters_with_journal_client inherits role::swh_server {
-  include profile::swh::deploy::counters::rpc
-  include profile::swh::deploy::counters::redis
-  include profile::swh::deploy::counters::journal_client
-}
