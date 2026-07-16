9bff71c4b04152ca8e4a7b2b0f54677a119e6266
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:16:09 +0200

    Drop loader-high-priority deployment
    Migrated to kubernetes.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/profile/manifests/swh/deploy/worker/loader_high_priority.pp
+++
@@ -1,6 +0,0 @@
-# Deployment for high priority loader
-class profile::swh::deploy::worker::loader_high_priority {
-  ::profile::swh::deploy::worker::instance {'loader_high_priority':
-    ensure => absent,
-  }
-}
