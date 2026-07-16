a9ab5cc444f840e7a26c850267e937245011ed16
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 17:30:12 +0200

    Drop unused swh_webapp role
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/role/manifests/swh_webapp.pp
+++
@@ -1,9 +0,0 @@
-# - Webapp api without reverse proxy
-# - storage
-# - search
-class role::swh_webapp inherits role::swh_server {
-  include profile::memcached
-  include profile::swh::deploy::search
-  include profile::swh::deploy::webapp
-  include profile::filebeat
-}
