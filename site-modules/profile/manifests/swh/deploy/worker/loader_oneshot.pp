414ffe369da83f86563b0a1906503f2943a8b3a5
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:11:03 +0200

    Drop loader-mercurial deployment
    Migrated to kubernetes.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/profile/manifests/swh/deploy/worker/loader_oneshot.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_oneshot.pp
@@ -1,9 +1,8 @@
 # Deployment for oneshot loader
 class profile::swh::deploy::worker::loader_oneshot {
   include ::profile::swh::deploy::base_loader_git
-  include ::profile::swh::deploy::base_loader_mercurial
 
-  $packages = $::profile::swh::deploy::base_loader_git::packages + $::profile::swh::deploy::base_loader_mercurial::packages
+  $packages = $::profile::swh::deploy::base_loader_git::packages
 
   ::profile::swh::deploy::worker::instance {'loader_oneshot':
     ensure       => present,
