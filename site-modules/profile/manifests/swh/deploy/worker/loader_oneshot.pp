9ef150b0fd7f3f9f65243291cd531ee49c78d2e0
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:09:50 +0200

    Drop loader-svn deployment
    Migrated to kubernetes.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/profile/manifests/swh/deploy/worker/loader_oneshot.pp
+++ site-modules/profile/manifests/swh/deploy/worker/loader_oneshot.pp
@@ -2,9 +2,8 @@
 class profile::swh::deploy::worker::loader_oneshot {
   include ::profile::swh::deploy::base_loader_git
   include ::profile::swh::deploy::base_loader_mercurial
-  include ::profile::swh::deploy::base_loader_svn
 
-  $packages = $::profile::swh::deploy::base_loader_git::packages + $::profile::swh::deploy::base_loader_mercurial::packages + $::profile::swh::deploy::base_loader_svn::packages
+  $packages = $::profile::swh::deploy::base_loader_git::packages + $::profile::swh::deploy::base_loader_mercurial::packages
 
   ::profile::swh::deploy::worker::instance {'loader_oneshot':
     ensure       => present,
