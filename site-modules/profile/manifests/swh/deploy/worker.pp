4d97991ee8bd0ee483ecf6cafb207c82f2eae86b
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:30:19 +0200

    Drop swh::deploy::worker profile no longer used
    It was used by worker like indexer, cooker, loader which are no longer present.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/profile/manifests/swh/deploy/worker.pp
+++
@@ -1,9 +0,0 @@
-# Worker deployment
-class profile::swh::deploy::worker {
-  $instances = lookup('swh::deploy::worker::instances')
-
-  each($instances) |$instance| {
-    $classname = "::profile::swh::deploy::worker::${instance}"
-    include $classname
-  }
-}
