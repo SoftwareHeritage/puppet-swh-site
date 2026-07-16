315464f4629367722d091fa900e58f5fd0bc7b33
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-07-16 09:26:35 +0200

    Drop deposit deployment (loader & checker)
    Migrated to kubernetes.
    
    Refs. swh/infra/sysadm-environment#5814

--- site-modules/profile/manifests/swh/deploy/worker/loader_package.pp
+++
@@ -1,8 +0,0 @@
-# Base loader package configuration
-class profile::swh::deploy::worker::loader_package {
-  $packages = ['python3-swh.loader.core']
-
-  package {$packages:
-    ensure => 'present',
-  }
-}
