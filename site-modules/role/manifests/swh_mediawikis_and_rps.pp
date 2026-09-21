e67b334bd35ef55557b0d130890c6f2175ad6be0
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-09-21 15:33:57 +0200

    Add keycloak-test reverse proxy instance to tate
    Refs. swh/infra/ci-cd/swh-charts#7

--- site-modules/role/manifests/swh_mediawikis_and_rps.pp
+++ site-modules/role/manifests/swh_mediawikis_and_rps.pp
@@ -7,4 +7,12 @@ class role::swh_mediawikis_and_rps inherits role::swh_server {
   # Reverse proxies
   include profile::jenkins::reverse_proxy
   include profile::keycloak::reverse_proxy
+
+  profile::reverse_proxy {'keycloak-test':
+    extra_apache_opts => {
+      proxy_preserve_host => true,
+    },
+    icinga_check_uri  => '/',
+  }
+
 }
